ec34550e7e6bfe9748ac375ed3d5d63d882f6182
Martin <m>
2020-09-01 11:31:57 -0300

    Agrego assets de Azure

---
+++ SustitucionMOAModel/Dto/InfoProveedorDataAgroDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SustitucionMOAModel.Dto
+{
+    public class InfoProveedorDataAgroDto
+    {
+        public string ProveedorCBU { get; set; }
+        public string ProveedorClasificacion { get; set; }
+        public string ProveedorSISACodCategoria { get; set; }
+    }
+}
