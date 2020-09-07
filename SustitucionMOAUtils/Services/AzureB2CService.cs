67fb8841bc0df846675043374b2129fc040e3b91
Martin <m>
2020-09-07 11:28:03 -0300

    Refactor registracion

--- SustitucionMOAUtils/Services/AzureB2CService.cs
+++ SustitucionMOAUtils/Services/AzureB2CService.cs
@@ -25,67 +25,46 @@ namespace SustitucionMOAUtils.Services
 
         public Usuario LoguearUsuario(string mail, string CUIT, string granosFlag)
         {
-            Usuario usuario = new Usuario();
+            Usuario usuario = new Usuario { Mail = mail, CUITRegistro = CUIT };
 
+            if (ExisteUsuario(usuario))
+            {
+                usuario = BuscarUsuarioPorMail(mail);
+            }
+            else
+            {
+                RegistrarUsuario(mail, CUIT, granosFlag, ref usuario);
+            }
+
+            return usuario;
+        }
+
+        private Usuario RegistrarUsuario(string mail, string CUIT, string granosFlag, ref Usuario usuario)
+        {
             switch (granosFlag.ToLower())
             {
                 case "granos":
                     UsuarioGranos usuarioGranos = new UsuarioGranos { Mail = mail, CUITRegistro = CUIT };
 
-                    if (!ExisteUsuario(usuarioGranos))
-                    {
-                        RegistrarUsuarioGranos(ref usuarioGranos);
-                    }
-                    else
-                    {
-                        usuarioGranos = BuscarUsuarioGranos(usuarioGranos);
-                    }
-
+                    RegistrarUsuarioGranos(ref usuarioGranos);
                     usuario = usuarioGranos;
                     break;
 
                 case "no granos":
-                    UsuarioNoGranos usuarioNoGranos = new UsuarioNoGranos { Mail = mail, CUITRegistro = CUIT };
+                    Usuario usuarioNoGranos = new Usuario { Mail = mail, CUITRegistro = CUIT };
 
-                    if (!ExisteUsuario(usuarioNoGranos))
-                    {
-                        Rol rolUsuarioNoImplementado = ObtenerRolPorCodigo("NOIMP");
-
-                        usuarioNoGranos.Roles = new List<Rol>();
-                        usuarioNoGranos.Proveedores = new List<Proveedor>();
-
-                        usuarioNoGranos.Roles.Add(rolUsuarioNoImplementado);
-                        usuarioNoGranos.TipoUsuario = ObtenerTipoPorNombreCorto("NG");
-
-                        RegistrarUsuarioGenerico(usuarioNoGranos);
-                        usuario = usuarioNoGranos;
-
-                    }
-                    else
-                    {
-                        usuario = BuscarUsuarioPorMail(mail);
-                    }
+                    usuarioNoGranos.TipoUsuario = ObtenerTipoPorNombreCorto("NG");
+                    RegistrarUsuarioGenerico(ref usuarioNoGranos);
+                    usuario = usuarioNoGranos;
+             
                     break;
 
                 case "ambos":
                     Usuario usuarioAmbos = new Usuario { Mail = mail, CUITRegistro = CUIT };
 
-                    if (!ExisteUsuario(usuarioAmbos))
-                    {
-                        Rol rolUsuarioNoImplementado = ObtenerRolPorCodigo("NOIMP");
+                    usuarioAmbos.TipoUsuario = ObtenerTipoPorNombreCorto("A");
 
-                        usuarioAmbos.Roles = new List<Rol>();
-                        usuarioAmbos.Proveedores = new List<Proveedor>();
-
-                        usuarioAmbos.Roles.Add(rolUsuarioNoImplementado);
-                        usuarioAmbos.TipoUsuario = ObtenerTipoPorNombreCorto("A");
-
-                        RegistrarUsuarioGenerico(usuarioAmbos);
-                    }
-                    else
-                    {
-                        usuarioAmbos = BuscarUsuarioPorMail(mail);
-                    }
+                    RegistrarUsuarioGenerico(ref usuarioAmbos);
 
                     usuario = usuarioAmbos;
                     break;
@@ -93,22 +72,9 @@ namespace SustitucionMOAUtils.Services
                 case "corredor":
                     Usuario usuarioCorredor = new Usuario { Mail = mail, CUITRegistro = CUIT };
 
-                    if (!ExisteUsuario(usuarioCorredor))
-                    {
-                        Rol rolUsuarioNoImplementado = ObtenerRolPorCodigo("NOIMP");
+                    usuarioCorredor.TipoUsuario = ObtenerTipoPorNombreCorto("CORR");
 
-                        usuarioCorredor.Roles = new List<Rol>();
-                        usuarioCorredor.Proveedores = new List<Proveedor>();
-
-                        usuarioCorredor.Roles.Add(rolUsuarioNoImplementado);
-                        usuarioCorredor.TipoUsuario = ObtenerTipoPorNombreCorto("CORR");
-
-                        RegistrarUsuarioGenerico(usuarioCorredor);
-                    }
-                    else
-                    {
-                        usuarioCorredor = BuscarUsuarioPorMail(mail);
-                    }
+                    RegistrarUsuarioGenerico(ref usuarioCorredor);
 
                     usuario = usuarioCorredor;
                     break;
@@ -116,33 +82,15 @@ namespace SustitucionMOAUtils.Services
                 case "cliente":
                     Usuario usuarioCliente = new Usuario { Mail = mail, CUITRegistro = CUIT };
 
-                    if (!ExisteUsuario(usuarioCliente))
-                    {
-                        Rol rolUsuarioNoImplementado = ObtenerRolPorCodigo("NOIMP");
+                    usuarioCliente.TipoUsuario = ObtenerTipoPorNombreCorto("CLI");
 
-                        usuarioCliente.Roles = new List<Rol>();
-                        usuarioCliente.Proveedores = new List<Proveedor>();
-
-                        usuarioCliente.Roles.Add(rolUsuarioNoImplementado);
-                        usuarioCliente.TipoUsuario = ObtenerTipoPorNombreCorto("CLI");
-
-                        RegistrarUsuarioGenerico(usuarioCliente);
-                    }
-                    else
-                    {
-                        usuarioCliente = BuscarUsuarioPorMail(mail);
-                    }
-
+                    RegistrarUsuarioGenerico(ref usuarioCliente);
+ 
                     usuario = usuarioCliente;
                     break;
             }
 
             return usuario;
-        }
-
-        private UsuarioGranos BuscarUsuarioGranos(UsuarioGranos usuarioGranos)
-        {
-            return repositorio.Obtener<UsuarioGranos>(u => u.Mail == usuarioGranos.Mail);
         }
 
         private Usuario BuscarUsuarioPorMail(string mail)
@@ -176,22 +124,25 @@ namespace SustitucionMOAUtils.Services
             };
 
             return ValidarCUITProveedor(ref usuario, proveedor);
-       
-
-            //usuario.Proveedores.Add(proveedor);
-
-            //usuario.Habilitado = true;
-
-            //repositorio.Agregar(usuario);
-            //return repositorio.GuardarCambios() == 1;
         }
 
-        public bool RegistrarUsuarioGenerico(Usuario usuario)
+        public bool RegistrarUsuarioGenerico(ref Usuario usuario)
         {
-            Proveedor proveedor = new Proveedor();
-            proveedor.CUIT = usuario.CUITRegistro;
-            proveedor.EstadoAprobacion = EstadoAprobacion.AunNoImplementado;
-            proveedor.Observaciones = "El tipo de usuario seleccionado aún no ha sido implementado. Contactese con su comercial.";
+            Rol rolUsuarioNoImplementado = ObtenerRolPorCodigo("NOIMP");
+
+            usuario.Roles = new List<Rol>
+            {
+                rolUsuarioNoImplementado
+            };
+
+            usuario.Proveedores = new List<Proveedor>();
+
+            Proveedor proveedor = new Proveedor
+            {
+                CUIT = usuario.CUITRegistro,
+                EstadoAprobacion = EstadoAprobacion.AunNoImplementado,
+                Observaciones = "El tipo de usuario seleccionado aún no ha sido implementado. Contactese con su comercial."
+            };
 
             usuario.Proveedores.Add(proveedor);
 
