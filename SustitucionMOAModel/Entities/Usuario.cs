ee6ffceb8a39877a1cf9186281646248074e9650
Martin <m>
2020-09-09 15:13:29 -0300

    Fix codigo proveedor

--- SustitucionMOAModel/Entities/Usuario.cs
+++ SustitucionMOAModel/Entities/Usuario.cs
@@ -66,7 +66,7 @@ namespace SustitucionMOAModel.Entities
         {
             if (Proveedores.Count >= 1)
             {
-                if (!string.IsNullOrEmpty(ObtenerProveedorActual().RazonSocial))
+                if (!string.IsNullOrEmpty(ObtenerProveedorActual().CodigoProveedor))
                     return ObtenerProveedorActual().CodigoProveedor;
                 else
                     return "-";
