f647d3a5242b8baf5575b012c314f1ae85597c43
Martin <m>
2020-09-21 15:22:34 -0300

    Fix web config

--- SustitucionMOA/app/pesificacion/pesificacion.service.ts
+++ SustitucionMOA/app/pesificacion/pesificacion.service.ts
@@ -52,7 +52,7 @@ export class PesificacionService extends BaseService {
     public getContratos(): Observable<any> {
         return this.http
             .get('/api/pesificacion/GetContratos')
-            .pipe(timeoutWith(30000, observableThrowError(new Error("Se exedio el tiempo de espera, por favor intentelo mas tarde"))))
-            .pipe(map(this.extractData));
+            .timeoutWith(30000, Observable.throw(new Error("Se exedio el tiempo de espera, por favor intentelo mas tarde")))
+            .map(this.extractData);
     }
 }
