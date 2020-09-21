71dbe99c8578b6c449385ecbce6060dea8293f10
Martin <m>
2020-09-21 16:47:47 -0300

    Revert "Muevo files de azure"
    This reverts commit 04c176bb1374ef2172928fb3fd16d4588bfaeb35.

--- SustitucionMOA/Azure/iniciodesesion.js
+++
@@ -1,9 +0,0 @@
-$("document").ready(function () {
-  $("label[for='email']").html("Ingrese su e-mail");
-
-  $("#email").attr("placeholder", "E-mail");
-
-  $(".intro").html(
-    "Información de operaciones y entregas para proveedores de Materias Primas"
-  );
-});
