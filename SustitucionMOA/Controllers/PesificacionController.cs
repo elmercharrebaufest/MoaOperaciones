49c9d486ae6318ae92ce8f3f965f38870162ac75
Martin <m>
2020-11-11 11:28:25 -0300

    Cambio logueo de error de pesificacion

--- SustitucionMOA/Controllers/PesificacionController.cs
+++ SustitucionMOA/Controllers/PesificacionController.cs
@@ -25,7 +25,7 @@ namespace SustitucionMOA.Controllers
             }
             catch (Exception e)
             {
-                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e.Message);
+                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
                 return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
             }
         }
