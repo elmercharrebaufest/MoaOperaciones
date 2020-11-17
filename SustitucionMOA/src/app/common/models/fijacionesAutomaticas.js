cb4eb27add0dfaabcf0a12f35497efde8c707661
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2020-11-17 16:39:47 -0300

    Se retiro el isGranosSelected en layout para Mis vendedores y vendedores Pendientes.

---
+++ SustitucionMOA/src/app/common/models/fijacionesAutomaticas.js
@@ -0,0 +1,8 @@
+var FijacionesAutomaticas = /** @class */ (function () {
+    function FijacionesAutomaticas() {
+        //this.TipoNegocioId = 3;
+    }
+    return FijacionesAutomaticas;
+}());
+export { FijacionesAutomaticas };
+//# sourceMappingURL=fijacionesAutomaticas.js.map
