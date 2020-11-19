39d76b520f9cfef00a86738869907c9647c37afb
eugenio martin <emartin@baufest.com>
2020-11-19 13:08:08 -0300

    rollback columna prueba

--- SustitucionMOADatabase/DBO/Tables/Proveedor.sql
+++ SustitucionMOADatabase/DBO/Tables/Proveedor.sql
@@ -14,7 +14,6 @@ CREATE TABLE [dbo].[Proveedor](
     [FechaSolicitud] DATETIME NULL, 
     [Comercial] VARCHAR(200) NULL, 
     [IdProveedorCorredor] INT NULL, 
-    [prueba] NCHAR(10) NULL, 
     CONSTRAINT [PK_dbo.Proveedor] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
