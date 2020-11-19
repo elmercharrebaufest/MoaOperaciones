07bf0e1d7ad8723d13dce6124254e50c371bae78
eugenio martin <emartin@baufest.com>
2020-11-19 16:22:59 -0300

    ARMOA003-205

--- SustitucionMOAUtils/Services/DataAgroService.cs
+++ SustitucionMOAUtils/Services/DataAgroService.cs
@@ -220,7 +220,7 @@ namespace SustitucionMOAUtils.Services
 
             repositorio.GuardarCambios();
 
-            string resultado = "El proveedor ha sido habilitado para cargar la documentación.";
+            string resultado = "El proveedor (CUIT: "+ proveedor.CUIT + ") ha sido habilitado para cargar la documentación.";
 
             return resultado;
         }
