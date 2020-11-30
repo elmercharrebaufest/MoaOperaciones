2a054cc6d706546db44e6a5277f494e4c76b301d
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2020-11-30 16:14:07 -0300

    Se quito el allowNulls en seciones visitadas

--- SustitucionMOADatabase/DBO/Tables/Usuario.sql
+++ SustitucionMOADatabase/DBO/Tables/Usuario.sql
@@ -6,7 +6,7 @@ CREATE TABLE [dbo].[Usuario](
 	[TipoUsuario_Id] [int] NULL,
 
  [UltimoLogin] SMALLDATETIME NULL, 
-    [SeccionesVisitadas] NVARCHAR(MAX) NULL DEFAULT '', 
+    [SeccionesVisitadas] NVARCHAR(MAX) NOT NULL DEFAULT '', 
     CONSTRAINT [PK_dbo.Usuario] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
