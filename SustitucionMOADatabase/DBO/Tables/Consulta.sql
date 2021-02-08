84ef4bdf91c40f5d73c4f2db0b341955098eee68
Becker Portilla <bportilla@baufest.com>
2021-02-08 09:35:00 -0300

    Modificacion de tablas

--- SustitucionMOADatabase/DBO/Tables/Consulta.sql
+++ SustitucionMOADatabase/DBO/Tables/Consulta.sql
@@ -1,38 +1,21 @@
 ﻿CREATE TABLE [dbo].[Consulta]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL,
-	[Proveedor_Id] INT NOT NULL,
-	[Nombre] [nvarchar](max) NOT NULL,
-	[Email] [nvarchar](max) NOT NULL,
-	[Telefono] [nvarchar](max) NULL,
+	[CodigoCorredor] varchar(max) NULL,
+	[RazonSocialCorredor] varchar(max) NULL,
+	[CodigoProveedor] varchar(max) NOT NULL,
+	[RazonSocialProveedor] varchar(max) NULL,
 	[Categoria_Id] INT NOT NULL,
+	[SubCategoria_Id] INT NOT NULL,
 	[Asunto] [nvarchar](max) NOT NULL,
 	[EstadoConsulta_Id] INT NOT NULL,
 	[FechaCreacion] DATETIME2 NOT NULL DEFAULT (getdate()), 
 	[FechaUltimaModificacion] DATETIME2 NOT NULL DEFAULT (getdate()), 
-	[RazonSocial] [nvarchar](max) NULL,
-	[NombreVendedor] [nvarchar](max) NULL,
-	[Contrato] [nvarchar](max) NULL,
-	[CUIT] [nvarchar](max) NULL,
-	[Comprobante] [nvarchar](max) NULL,
-	[FechaPago] DATETIME2 NULL, 
-	[Importe] NUMERIC NULL, 
-	[Impuesto] NUMERIC NULL, 
-	[Inscripcion] [nvarchar](max) NULL,
-	[Motivo] [nvarchar](max) NULL,
 CONSTRAINT [PK_dbo.Consulta] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Consulta]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Consulta_dbo.Proveedor_Proveedor_Id] FOREIGN KEY([Proveedor_Id])
-REFERENCES [dbo].[Proveedor] ([Id])
-ON DELETE CASCADE
-GO
-
-ALTER TABLE [dbo].[Consulta] CHECK CONSTRAINT [FK_dbo.Consulta_dbo.Proveedor_Proveedor_Id]
 GO
 
 ALTER TABLE [dbo].[Consulta]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Consulta_dbo.Categoria_Categoria_Id] FOREIGN KEY([Categoria_Id])
---
+++ SustitucionMOADatabase/DBO/Tables/ConsultaDetalle.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[ConsultaDetalle]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[Consulta_Id] INT NOT NULL,
+	[Fecha] DATETIME2 NULL, --FechaPago, FechaFactura, FechaOblea
+	[ComprobanteNo] NVARCHAR(max) NULL, --SalidaPagoNo, FacturaNo
+	[ContratoNo] NVARCHAR(max) NULL,
+	[Impuesto] DECIMAL(18, 3) NULL, --ImpuestoRetenido, ImpuestoPercibido, Impuesto
+	[Importe] DECIMAL(18, 3) NULL, --ImporteRetencion
+	[CausaConsulta_Id] INT NOT NULL,
+	[BolsaEmisoraOblea] NVARCHAR(max) NULL,
+	
+CONSTRAINT [PK_dbo.ConsultaDetalle] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
