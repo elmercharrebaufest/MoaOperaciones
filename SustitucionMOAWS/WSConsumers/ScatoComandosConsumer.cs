3303711d8bce208f97412f26ce71ed95b9a467bd
Martin <m>
2021-02-25 15:08:43 -0300

    Agrego referencia de scato

---
+++ SustitucionMOAWS/WSConsumers/ScatoComandosConsumer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SustitucionMOAModel.Models;
+using SustitucionMOAWS.ScatoComandosWebService;
+
+namespace SustitucionMOAWS.WSConsumers
+{
+    class ScatoComandosConsumer 
+    {
+        private readonly ServicioComandosClient servicioComandosClient = new ServicioComandosClient();
+
+        private void ObtenerTicketPesada(ConsultaTicketPesada consultaTicketPesada)
+        {
+            ObtenerTickets comando = new ObtenerTickets
+            {
+                CP = "1000"
+            };
+
+            var resultado = servicioComandosClient.Ejecutar(comando);
+
+            Console.Write("Test");
+
+            return ;
+        }
+    }
+}
