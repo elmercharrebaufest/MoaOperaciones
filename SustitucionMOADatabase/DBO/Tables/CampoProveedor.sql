04a71ec0c8659916f065f52158464463678d312a
Martin <m>
2021-03-10 11:23:53 -0300

    save

--- SustitucionMOADatabase/DBO/Tables/CampoProveedor.sql
+++ SustitucionMOADatabase/DBO/Tables/CampoProveedor.sql
@@ -7,6 +7,8 @@
     [Longitud] FLOAT NOT NULL,
     [Latitud] FLOAT NOT NULL,
     [Archivo_Id] INT NOT NULL,
+    [FechaCreacion] DATETIME NULL, 
+    [FechaModificacion] DATETIME NULL, 
     PRIMARY KEY(CampoCosecha_Id, Proveedor_Id),
     CONSTRAINT [FK_CampoProveedor_ToArchivo] FOREIGN KEY ([Archivo_Id]) REFERENCES [Archivo]([Id]),
     CONSTRAINT [FK_CampoProveedor_ToCampoCosecha] FOREIGN KEY ([CampoCosecha_Id]) REFERENCES [CampoCosecha]([Id]),
--- SustitucionMOAModel/Entities/CampoProveedor.cs
+++ SustitucionMOAModel/Entities/CampoProveedor.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SustitucionMOAModel.Entities
@@ -19,5 +20,8 @@ namespace SustitucionMOAModel.Entities
         
         public int Archivo_Id { get; set; }
         public virtual Archivo Archivo { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
     }
 }
