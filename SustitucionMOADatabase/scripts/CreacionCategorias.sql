d62c9bb6c43893fea45edef245170ca2d3acf4e1
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2021-03-16 10:11:51 -0300

    Agrego queys categorias y estados.

--- SustitucionMOADatabase/scripts/CreacionCategorias.sql
+++ SustitucionMOADatabase/scripts/CreacionCategorias.sql
@@ -46,6 +46,9 @@ BEGIN
 
 	INSERT INTO SubCategoria(Code, Nombre, Categoria_Id)
 	VALUES('INF', 'Informe comercial', @idCategoria)
+
+	INSERT INTO SubCategoria(Code, Nombre, Categoria_Id)
+	VALUES('CAP', 'Carta Presentacion', @idCategoria)
 END
 
 COMMIT TRAN
