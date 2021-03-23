12386e9a2947ebcc4ec9a171d5708a0c6757c2b6
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2021-03-23 10:56:45 -0300

    campoProveedordto get equals and hashcode

--- SustitucionMOAModel/Dto/CampoProveedorDto.cs
+++ SustitucionMOAModel/Dto/CampoProveedorDto.cs
@@ -39,17 +39,31 @@ namespace SustitucionMOAModel.Dto
                    NombreCosecha == dto.NombreCosecha &&
                    HectareasTotales == dto.HectareasTotales &&
                    HectareasSoja == dto.HectareasSoja &&
-                   ToneladasAprobadas == dto.ToneladasAprobadas;
+                   ToneladasAprobadas == dto.ToneladasAprobadas &&
+                   Latitud == dto.Latitud &&
+                   Longitud == dto.Longitud &&
+                   CampoCosechaId == dto.CampoCosechaId &&
+                   ProveedorNombre == dto.ProveedorNombre &&
+                   LocalidadNombre == dto.LocalidadNombre &&
+                   CampoSustentableId == dto.CampoSustentableId &&
+                   CosechaId == dto.CosechaId;
         }
 
         public override int GetHashCode()
         {
-            int hashCode = -1372739976;
+            int hashCode = -1557278676;
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NombreCampo);
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NombreCosecha);
             hashCode = hashCode * -1521134295 + HectareasTotales.GetHashCode();
             hashCode = hashCode * -1521134295 + HectareasSoja.GetHashCode();
             hashCode = hashCode * -1521134295 + ToneladasAprobadas.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Latitud);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Longitud);
+            hashCode = hashCode * -1521134295 + CampoCosechaId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ProveedorNombre);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(LocalidadNombre);
+            hashCode = hashCode * -1521134295 + CampoSustentableId.GetHashCode();
+            hashCode = hashCode * -1521134295 + CosechaId.GetHashCode();
             return hashCode;
         }
     }
