3a695716bbc7e7a6334d77ad15597b1c852cf7be
Martin <m>
2021-03-25 12:20:14 -0300

    Cambio nombre

--- SustitucionMOAModel/Dto/CamposSustentableReporte.cs
+++ SustitucionMOAModel/Dto/CamposSustentableReporte.cs
@@ -9,6 +9,6 @@
         public string HectareasTotales { get; set; }
         public string HectareasSoja { get; set; }
         public string Coordenadas { get; set; }
-        public string Departamento { get; set; }
+        public string Partido { get; set; }
     }
 }
