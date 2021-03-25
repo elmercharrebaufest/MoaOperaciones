ed90bf3dfa5211daa474042f7e31503e7e8bef1f
Martin <m>
2021-03-25 12:22:51 -0300

    .

--- SustitucionMOAUtils/Services/CampoSustentableService.cs
+++ SustitucionMOAUtils/Services/CampoSustentableService.cs
@@ -142,7 +142,7 @@ namespace SustitucionMOAUtils.Services
                     Pais = "Argentina",
                     Provincia = c.CampoCosecha.Campo.Localidad.Provincia.Nombre,
                     Coordenadas = string.Concat(c.Latitud, " ", c.Longitud),
-                    Departamento = c.CampoCosecha.Campo.Localidad.Partido.Descripcion
+                    Partido = c.CampoCosecha.Campo.Localidad.Partido.Descripcion
                 },
                 cp => cp.Proveedor_Id == proveedorId && cp.CampoCosecha.Cosecha_Id == cosecha.Id);
 
