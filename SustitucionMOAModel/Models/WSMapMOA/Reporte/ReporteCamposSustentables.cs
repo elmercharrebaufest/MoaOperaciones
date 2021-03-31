1801bb0d52cd0ace7e0231da9032e18841b05683
Mauro Corvaro <mcorvaro@baufest.com>
2021-03-31 09:49:10 -0300

    #ARMOA003-326-back - agrego entidad de reporte

---
+++ SustitucionMOAModel/Models/WSMapMOA/Reporte/ReporteCamposSustentables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SustitucionMOAModel.Models.WSMapMOA.Reporte
+{
+    public class ReporteCamposSustentables : ReporteBase
+    {
+        public
+
+        public override string GetBody()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetFecha()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
