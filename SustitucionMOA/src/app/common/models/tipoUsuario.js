7a0c4d44d1b5cfad2ade52773276d5eb13bb737d
Martin <m>
2021-04-09 12:52:23 -0300

    borro archivos .js .map

--- SustitucionMOA/src/app/common/models/tipoUsuario.js
+++
@@ -1,8 +0,0 @@
-var TipoUsuario = /** @class */ (function () {
-    function TipoUsuario(init) {
-        Object.assign(this, init);
-    }
-    return TipoUsuario;
-}());
-export { TipoUsuario };
-//# sourceMappingURL=tipoUsuario.js.map
