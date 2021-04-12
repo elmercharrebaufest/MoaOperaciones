ebe80ea6fdc2d5c8c461822d1ffb4142e192cbf1
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2021-04-12 16:56:55 -0300

    Hotfix test getRoles.

--- SustitucionMOATest/Controllers/UsuarioControllerTest.cs
+++ SustitucionMOATest/Controllers/UsuarioControllerTest.cs
@@ -52,17 +52,10 @@ namespace SustitucionMOATest.Controllers
         {
 
 
-            List<RolDropdownDto> listaRoles2 = new List<RolDropdownDto>() {
+            List<RolDropdownDto> listaRoles = new List<RolDropdownDto>() {
                                                 new RolDropdownDto { Id = 1, Nombre = "Rol 1" },
                                                 new RolDropdownDto { Id = 2, Nombre = "Rol 2" },
                                                 new RolDropdownDto { Id = 3, Nombre = "Rol 3" }};
-
-            Dictionary<string, List<RolDropdownDto>> listaRoles = new Dictionary<string, List<RolDropdownDto>>()
-            {
-                {
-                    "Interno", listaRoles2
-                }
-            };
 
             usuarioServiceMock.Setup(s => s.GetRoles()).Returns(listaRoles);
 
