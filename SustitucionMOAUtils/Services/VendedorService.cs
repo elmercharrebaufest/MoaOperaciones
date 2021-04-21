11a1f6825713cb938afaec88c39c12192eef28ee
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2021-04-21 17:05:16 -0300

    fix estado moa y sap.

--- SustitucionMOAUtils/Services/VendedorService.cs
+++ SustitucionMOAUtils/Services/VendedorService.cs
@@ -104,18 +104,15 @@ namespace SustitucionMOAUtils.Services
                 .Select(v => new Vendedor() 
                 { 
                     descVendedor = v.RazonSocial, 
-                    estado = "",
-                    estadoMoa = v.EstadoAprobacion == EstadoAprobacion.AnalisisDeNosis || v.EstadoAprobacion == EstadoAprobacion.EtapaFinal 
-                                || v.EstadoAprobacion == EstadoAprobacion.AprobacionPendiente || v.EstadoAprobacion == EstadoAprobacion.PendienteAprobacionCompras 
-                                ? "Alta en Gestión" : v.EstadoAprobacion == EstadoAprobacion.EdicionRequerida ? "Solicitud de información" : v.EstadoAprobacion.ToFriendlyString(),
-                    idVendedor = v.CodigoProveedor });
-
-            response.vendedores.AddRange(vendedoresAprobados);
+                    estado = "Alta interna Pendiente.",
+                    estadoMoa = v.EstadoAprobacionDescripcion,
+                    idVendedor = v.CodigoProveedor 
+                });
 
             response.vendedores = response.vendedores.Distinct().ToList();
             return response;
         }
-
+     
         public VendedorHabilitadoWSMOAResponse GetVendedorStatus(string cuit, string user)
         {
             try
