f3f45139cd59633c75357395f3c3d2d1d11404eb
Rocio Orlando <rorlando@baufest.com>
2021-04-26 09:58:18 -0300

    Dashboard compras

---
+++ SustitucionMOA/src/app/compras/Solp.ts
@@ -0,0 +1,5 @@
+export class Card  {
+    nombre: String;
+    path: String;
+}
+
---
+++ SustitucionMOA/src/app/compras/solp.service.ts
@@ -0,0 +1,15 @@
+import {map} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { Http, Response, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs';
+
+
+
+import { Formatter } from './../common/formatter/Formatter';
+import { BaseService } from './../common/services/BaseService';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class SolpService extends BaseService {
+
+}
