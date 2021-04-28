f6ee3f99d6eb32a42d442bc35c883edc636ede6a
Martin <m>
2021-04-28 11:21:06 -0300

    Arreglo error que no cierra la pesificación

--- SustitucionMOA/src/app/pesificacion/pesificacion.component.ts
+++ SustitucionMOA/src/app/pesificacion/pesificacion.component.ts
@@ -186,7 +186,7 @@ export class PesificacionComponent extends ListBaseComponent implements OnInit {
                     this.getListaContratos();
                     this.mensajeComponent.setSuccessMsg("Operacion realizada exitosamente");
                 }
-                $("#myModalConfirmacion").hide();
+                $("#hidemyModal").click();
                 this.spinnerSmallComponent.hideIt();
                 this.visibleEnviar = true;
                 return false;
