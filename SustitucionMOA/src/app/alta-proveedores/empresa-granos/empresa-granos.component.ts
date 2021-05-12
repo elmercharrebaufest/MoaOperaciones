68a279831589303285095906e16f0f701737aaf1
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2021-05-12 10:45:31 -0300

    Arreglo para que el asunto del mail y el modal no se vean para No granos.

--- SustitucionMOA/src/app/alta-proveedores/empresa-granos/empresa-granos.component.ts
+++ SustitucionMOA/src/app/alta-proveedores/empresa-granos/empresa-granos.component.ts
@@ -61,6 +61,7 @@ export class EmpresaGranosComponent extends ListBaseComponent {
     archivoSeleccionado: Archivo = null;
 
     listaArchivos: Array<Archivo> = [];
+    granosFlag = sessionStorage.getItem("granosFlag");
 
     informe = new InformeComercial();
 
