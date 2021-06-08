1015e5b14b5f7c7dbbabab26c4b8df0037328def
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2021-06-08 09:27:23 -0300

    Se cambio el asunto de mail y a quien esta dirigido.

--- SustitucionMOAUtils/Services/AltaEmpresaGranosService.cs
+++ SustitucionMOAUtils/Services/AltaEmpresaGranosService.cs
@@ -501,7 +501,7 @@ namespace SustitucionMOAUtils.Services
             {
 
                 var cuerpoTemplate = File.ReadAllText(EMAIL_TEMPLATE_AUDITORIA);
-                string asunto = "MOA Operaciones - Declaración jurada";
+                string asunto = "MOA Operaciones - Conflicto de Interés Declarado en Alta de Proveedor";
                 var vinculoEmpleadoMolinos = new StringBuilder();
                 var Vinculofuncionarios = new StringBuilder();
 
