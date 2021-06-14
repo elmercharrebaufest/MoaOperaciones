baf14bfbb1b8afff269a5166044c27a61fc2c2f5
Becker Portilla <bportilla@baufest.com>
2021-06-14 06:52:45 -0300

    Se agrega proyecto de webapi

---
+++ SustitucionMOAExternalAPI/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SustitucionMOAExternalAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [Authorize(Roles = "API")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [Authorize(Roles = "API2")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [Authorize(Roles = "API")]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [Authorize]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [Authorize]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SustitucionMOAExternalAPI/Managers/AuthenticationManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SustitucionMOAExternalAPI.Managers
+{
+    public static class AuthenticationManager
+    {
+        public static List<string> ObtenerRoles(string token)
+        {
+            return new List<string>()
+            {
+                token == "1" ? "API" : "API2"
+            };
+        }
+    }
+
+}
