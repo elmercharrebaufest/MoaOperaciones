2acb77bfd86c2d71affeb86a868bce249319bf61
Becker Portilla <bportilla@baufest.com>
2021-06-30 13:39:09 -0300

    Fix merge servicios scato y script rolapikey

--- SustitucionMOAUtils/Services/UsuarioService.cs
+++ SustitucionMOAUtils/Services/UsuarioService.cs
@@ -153,7 +153,7 @@ namespace SustitucionMOAUtils.Services
         {
             List<string> interno = new List<string>
             {
-                "ADM", "OPE", "APRO", "COMPRAS", "ADMINCCSS", "TODOS", "COMERCIAL", "SOLP"
+                "ADM", "OPE", "APRO", "COMPRAS", "ADMINCCSS", "TODOS", "COMERCIAL", "SOLP", "APIKEY"
             };
 
             List<string> contacto = new List<string>
