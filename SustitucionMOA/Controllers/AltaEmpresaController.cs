b4b65f843a0e901cef9ff0d9a6247712bb21188e
Martin <m>
2021-07-05 12:27:24 -0300

    Agrego posibilidad de agregar observaciones internas

--- SustitucionMOA/Controllers/AltaEmpresaController.cs
+++ SustitucionMOA/Controllers/AltaEmpresaController.cs
@@ -240,5 +240,29 @@ namespace SustitucionMOA.Controllers
                 return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
             }
         }
+
+        [CustomPermisoAuthorizeAttribute(Roles = Permiso.ABM_EMPRESAS)]
+        [HttpPost]
+        public JsonResult AgregarObservacion(int empresaId, string observacion)
+        {
+            try
+            {
+                return JsonCustom(new { data = altaEmpresaService.AgregarObservacion(empresaId, observacion, ClaimsPrincipalExtension.GetClaimValue("emails")) });
+            }
+            catch (InfoCustomException e)
+            {
+                return Json(new { info = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+            catch (ValidationCustomException e)
+            {
+                return Json(new { error = e.Message }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
+                return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
     }
 }
--- SustitucionMOATest/Controllers/AltaEmpresaControllerTest.cs
+++ SustitucionMOATest/Controllers/AltaEmpresaControllerTest.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SustitucionMOA.Controllers;
+using SustitucionMOA.Utils;
+using SustitucionMOAAssets;
+using SustitucionMOARepositorio;
+using SustitucionMOAUtils.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SustitucionMOATest.Controllers
 {
-    class AltaEmpresaControllerTest
+    public class AltaEmpresaControllerTest
     {
+        private AltaEmpresaController target;
+        private Mock<IAltaEmpresaService> altaEmpresaServiceMock;
+        private Mock<IRepositorio> repositorioMock;
+        private Mock<IDataAgroService> dataAgroServiceMock;
+        private string expectedJson;
+        private string resultJson;
+        private string mailUsuario = "mail@mail.com";
+
+        [SetUp]
+        public void SetUp()
+        {
+            altaEmpresaServiceMock = new Mock<IAltaEmpresaService>();
+            repositorioMock = new Mock<IRepositorio>();
+            dataAgroServiceMock = new Mock<IDataAgroService>();
+
+            var fakeIdentity = new GenericIdentity("User");
+
+            var claims = (ClaimsIdentity)fakeIdentity;
+
+            claims.AddClaim(new Claim(Globals.ClaimsUserNameType, mailUsuario));
+            claims.AddClaim(new Claim(Globals.ClaimsNombreType, "mail"));
+
+            var principal = new GenericPrincipal(fakeIdentity, null);
+
+            Thread.CurrentPrincipal = principal;
+
+            target = new AltaEmpresaController(altaEmpresaServiceMock.Object, repositorioMock.Object, dataAgroServiceMock.Object);
+        }
+
+        [Test()]
+        public void AgregarObservacionTest()
+        {
+            var expected = SuccessMsg.ObservacionAgregadaOK;
+
+            var proveedorId = 1;
+            var observacion = "Buen día!";
+
+            altaEmpresaServiceMock.Setup(s => s.AgregarObservacion(
+                                                        It.IsAny<int>(),
+                                                        It.IsAny<string>(),
+                                                        It.IsAny<string>()))
+                                   .Returns(expected);
+
+            var result = target.AgregarObservacion(proveedorId, observacion);
+
+            expectedJson = JsonConvert.SerializeObject(new { data = expected });
+            resultJson = JsonConvert.SerializeObject(result.Data);
+
+            Assert.NotNull(result);
+            Assert.AreEqual(expectedJson, resultJson);
+        }
     }
 }
