6392a6314a873e30ab762285ba7c70d4a8018a44
Dario <dario.20398@gmail.com>
2021-07-06 17:13:27 -0300

    ARMOA003-512 Modificaciones base de datos

---
+++ SustitucionMOADatabase/DBO/Tables/IngresosBrutosCoeficienteUnificado.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[IngresosBrutosCoeficienteUnificado]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+    [EstadoIngresosBrutosCoeficienteUnificado_Id] INT NOT NULL,
+    [Anticipo] INT NOT NULL, 
+    [Cuit] NVARCHAR(30) NOT NULL, 
+    [Sede] INT NOT NULL,
+	[FechaCarga] DATETIME NOT NULL, 
+    CONSTRAINT [PK_dbo.IngresosBrutosCoeficienteUnificado] PRIMARY KEY CLUSTERED
+	(
+		[Id] ASC
+	) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[IngresosBrutosCoeficienteUnificado]  WITH CHECK 
+	ADD CONSTRAINT [FK_dbo.IngresosBrutosCoeficienteUnificado_dbo.EstadoIngresosBrutosCoeficienteUnificado] 
+	FOREIGN KEY([EstadoIngresosBrutosCoeficienteUnificado_Id])
+	REFERENCES [dbo].[EstadoIngresosBrutosCoeficienteUnificado]([Id])
+	ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[IngresosBrutosCoeficienteUnificado] CHECK CONSTRAINT [FK_dbo.IngresosBrutosCoeficienteUnificado_dbo.EstadoIngresosBrutosCoeficienteUnificado]
+GO
---
+++ SustitucionMOADatabase/DBO/Tables/IngresosBrutosCoeficienteUnificadoDetalle.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[IngresosBrutosCoeficienteUnificadoDetalle]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[IngresosBrutosCoeficienteUnificado_Id] INT NOT NULL, 
+    [NumeroJurisdiccion] INT NOT NULL, 
+    [Jurisdiccion] VARCHAR(MAX) NOT NULL, 
+    [FechaInicio] DATETIME NULL, 
+    [FechaCese] DATETIME NULL, 
+    [CoeficienteIngresos] DECIMAL(10, 4) NOT NULL, 
+    [CoeficienteGastos] DECIMAL(10, 4) NOT NULL, 
+    [CoeficienteUnificado] DECIMAL(10, 4) NOT NULL,
+	CONSTRAINT [PK_dbo.IngresosBrutosCoeficienteUnificadoDetalle] PRIMARY KEY CLUSTERED 
+    (
+		[Id] ASC
+	) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[IngresosBrutosCoeficienteUnificadoDetalle]  
+    WITH CHECK ADD CONSTRAINT [FK_dbo.IngresosBrutosCoeficienteUnificadoDetalle_dbo.IngresosBrutosCoeficienteUnificado] 
+    FOREIGN KEY([IngresosBrutosCoeficienteUnificado_Id])
+    REFERENCES [dbo].[IngresosBrutosCoeficienteUnificado] ([Id])
+    ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[IngresosBrutosCoeficienteUnificadoDetalle] CHECK CONSTRAINT [FK_dbo.IngresosBrutosCoeficienteUnificadoDetalle_dbo.IngresosBrutosCoeficienteUnificado]
+GO
