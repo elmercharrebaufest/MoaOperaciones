6fc38f9c0f8944f412c11a514b3913df6e118928
Rocio Orlando <rorlando@baufest.com>
2021-07-14 12:46:14 -0300

    Fix col, pdf template

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -513,7 +513,6 @@ namespace SustitucionMOAUtils.Services
             solpValores.Add(SolpTemplateKeys.LISTADO_ADJUNTOS, "Adjuntos");
 
 
-
             templateString = CombineTemplateValues(templateString, solpValores);
 
             #region Generacion del pdf
