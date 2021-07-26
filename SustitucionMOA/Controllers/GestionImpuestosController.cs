37b8414fedaebf72c156215596641ae3cccf80c6
Dario <dario.20398@gmail.com>
2021-07-26 16:49:35 -0300

    GestionImpuestosController.cs

---
+++ SustitucionMOA/Controllers/GestionImpuestosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using SustitucionMOA.Utils;
+using SustitucionMOAAssets;
+using SustitucionMOAModel.CustomExceptions;
+using SustitucionMOAModel.Dto;
+using SustitucionMOAModel.Entities;
+using SustitucionMOAModel.Models.ViewModel;
+using SustitucionMOASecurity;
+using SustitucionMOAUtils.Interfaces;
+using SustitucionMOAUtils.Logger;
+
+namespace SustitucionMOA.Controllers
+{
+    public class GestionImpuestosController : BaseController
+    {
+        private readonly IGestionImpuestosService gestionImpuestosService;
+
+        public GestionImpuestosController(IGestionImpuestosService gestionImpuestosService)
+        {
+            this.gestionImpuestosService = gestionImpuestosService;
+        }
+
+        public JsonResult ListarCabeceras()
+        {
+            return JsonCustom(gestionImpuestosService.ListarCabeceras());
+        }
+
+        public JsonResult ListarDetalles(int idCabecera)
+        {
+            return JsonCustom(gestionImpuestosService.ListarDetalles(idCabecera));
+        }
+    }
+}
