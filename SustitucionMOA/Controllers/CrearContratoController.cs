fc31c3745d0e3594fd47e3f88ecddb265b3aa50c
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2021-07-28 15:43:50 -0300

    Cambios backend logear error terceros.

--- SustitucionMOA/Controllers/CrearContratoController.cs
+++ SustitucionMOA/Controllers/CrearContratoController.cs
@@ -1198,7 +1198,7 @@ namespace SustitucionMOA.Controllers
 
             if(usuario == null)
             {
-                throw new InfoCustomException(String.Format(InfoMsg.ElementoNoExiste, "usuario", usuario));
+                throw new Exception(String.Format(InfoMsg.ElementoNoExiste, "usuario", usuario));
             }
 
             var esAdmin = usuario.EsAdmin();
