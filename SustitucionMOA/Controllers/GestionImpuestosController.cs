f1c1c0499652d23f1d8078bc8ce963bec28f40a6
Dario <dario.20398@gmail.com>
2021-07-28 14:46:18 -0300

    GestionImpuestosController: agrego try/catch y método AutorizarCabecera()

--- SustitucionMOA/Controllers/GestionImpuestosController.cs
+++ SustitucionMOA/Controllers/GestionImpuestosController.cs
@@ -28,12 +28,67 @@ namespace SustitucionMOA.Controllers
 
         public JsonResult ListarCabeceras()
         {
-            return JsonCustom(gestionImpuestosService.ListarCabeceras());
+            try
+            {
+                return JsonCustom(gestionImpuestosService.ListarCabeceras());
+            }
+            catch (InfoCustomException e)
+            {
+                return Json(new { info = e }, JsonRequestBehavior.AllowGet);
+            }
+            catch (ValidationCustomException e)
+            {
+                return Json(new { error = e }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
+                return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
+            }
         }
 
         public JsonResult ListarDetalles(int idCabecera)
         {
-            return JsonCustom(gestionImpuestosService.ListarDetalles(idCabecera));
+            try
+            {
+                return JsonCustom(gestionImpuestosService.ListarDetalles(idCabecera));
+            }
+            catch (InfoCustomException e)
+            {
+                return Json(new { info = e }, JsonRequestBehavior.AllowGet);
+            }
+            catch (ValidationCustomException e)
+            {
+                return Json(new { error = e }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
+                return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult AutorizarCabecera(int idCabecera)
+        {
+            try
+            {
+                gestionImpuestosService.AutorizarCabecera(idCabecera);
+
+                return JsonCustom(new { });
+            }
+            catch (InfoCustomException e)
+            {
+                return Json(new { info = e }, JsonRequestBehavior.AllowGet);
+            }
+            catch (ValidationCustomException e)
+            {
+                return Json(new { error = e }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
+                return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
