b5fbe1d150f1dd091dfac5eb0b11107d649243dc
Enzo Alberto Villiate Porraz <evilliate@baufest.com>
2021-08-06 09:43:58 -0300

    Creacion minima del servicio.

---
+++ SustitucionMOAWS/WSConsumers/ObtenerCuentasSolpConsumerMOA.cs
@@ -0,0 +1,28 @@
+﻿using SustitucionMOAWS.CredentialService;
+using SustitucionMOAWS.ObtenerCuentasSolpWebServiceMOA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SustitucionMOAWS.WSConsumers
+{
+    class ObtenerCuentasSolpConsumerMOA
+    {
+        SI_MMRFC_OBTENER_CUENTASClient service = new SI_MMRFC_OBTENER_CUENTASClient();
+
+        public object request()
+        {
+            try
+            {
+                service.ClientCredentials.UserName.UserName = SAPCredential.getUserName();
+                service.ClientCredentials.UserName.Password = SAPCredential.getPassword();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
