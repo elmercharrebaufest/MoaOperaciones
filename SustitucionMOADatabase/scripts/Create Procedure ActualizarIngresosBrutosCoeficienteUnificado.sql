51851dfd97ed6b30d2e0024da37a1916e7b98b99
Dario <dario.20398@gmail.com>
2021-08-10 08:20:08 -0300

    Cuando RPA actualiza la cabecera CM05, pasar la consulta a "Cerrado". Esto se hace desde el SP

--- SustitucionMOADatabase/scripts/Create Procedure ActualizarIngresosBrutosCoeficienteUnificado.sql
+++ SustitucionMOADatabase/scripts/Create Procedure ActualizarIngresosBrutosCoeficienteUnificado.sql
@@ -3,9 +3,16 @@
 AS   
     DECLARE @EstadoIngresosBrutosCoeficienteUnificado_Id_Autorizado INT = 2
     DECLARE @EstadoIngresosBrutosCoeficienteUnificado_Id_Completado INT = 3
- 
+    DECLARE @EstadoConsultaCerrado INT = 5
+    
     UPDATE IngresosBrutosCoeficienteUnificado 
     SET EstadoIngresosBrutosCoeficienteUnificado_Id = @EstadoIngresosBrutosCoeficienteUnificado_Id_Completado 
     WHERE Id = @IdIngresosBrutosCoeficienteUnificado 
     and EstadoIngresosBrutosCoeficienteUnificado_Id = @EstadoIngresosBrutosCoeficienteUnificado_Id_Autorizado
+
+    UPDATE c
+    SET EstadoConsulta_Id = @EstadoConsultaCerrado
+    FROM Consulta c
+    JOIN IngresosBrutosCoeficienteUnificado i ON c.Id = i.Consulta_Id
+    WHERE i.Id = @IdIngresosBrutosCoeficienteUnificado
 GO
