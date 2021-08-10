fd8cb8a7b477640bc98cba5a2a21fb955760b0b5
Dario <dario.20398@gmail.com>
2021-08-10 08:15:02 -0300

    Agrego viewIngresosBrutosCoeficienteUnificado y SP ActualizarIngresosBrutosCoeficienteUnificado

---
+++ SustitucionMOADatabase/scripts/Create Procedure ActualizarIngresosBrutosCoeficienteUnificado.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE ActualizarIngresosBrutosCoeficienteUnificado
+    @IdIngresosBrutosCoeficienteUnificado int
+AS   
+    DECLARE @EstadoIngresosBrutosCoeficienteUnificado_Id_Autorizado INT = 2
+    DECLARE @EstadoIngresosBrutosCoeficienteUnificado_Id_Completado INT = 3
+ 
+    UPDATE IngresosBrutosCoeficienteUnificado 
+    SET EstadoIngresosBrutosCoeficienteUnificado_Id = @EstadoIngresosBrutosCoeficienteUnificado_Id_Completado 
+    WHERE Id = @IdIngresosBrutosCoeficienteUnificado 
+    and EstadoIngresosBrutosCoeficienteUnificado_Id = @EstadoIngresosBrutosCoeficienteUnificado_Id_Autorizado
+GO
