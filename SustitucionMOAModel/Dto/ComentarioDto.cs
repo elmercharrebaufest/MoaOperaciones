e0558552df8cf6b5f84f3790b4d8545b147e5a19
Dario <dario.20398@gmail.com>
2021-08-12 11:38:30 -0300

    Cambio a nulleables los campos Recordado y FechaRecordado en Comentario y ComentarioDto

--- SustitucionMOAModel/Dto/ComentarioDto.cs
+++ SustitucionMOAModel/Dto/ComentarioDto.cs
@@ -14,8 +14,8 @@ namespace SustitucionMOAModel.Dto
         public DateTime Fecha { get; set; }
         public int UsuarioId { get; set; }
         public UsuarioDto Usuario { get; set; }
-        public bool Recordado { get; set; }
-        public DateTime FechaRecordado { get; set; }
+        public bool? Recordado { get; set; }
+        public DateTime? FechaRecordado { get; set; }
         public IList<ArchivoDto> Archivos { get; set; }
         public IList<ComentarioRecordadoDto> ComentarioRecordados { get; set; }
         public int Consulta_Id { get; set; }
