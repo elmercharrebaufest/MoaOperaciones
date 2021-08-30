554d8aa7a681ccfd899896d5ab53b06da851db69
Dario <dario.20398@gmail.com>
2021-08-30 14:03:57 -0300

    Campo sustentable: agrego botón Descargar KMZ

--- SustitucionMOA/src/app/venta-sustentable/listado-campos/listado-campos.component.ts
+++ SustitucionMOA/src/app/venta-sustentable/listado-campos/listado-campos.component.ts
@@ -39,6 +39,7 @@ export class ListadoCamposComponent extends BaseComponent implements OnInit {
     data: any;
     esInterno: boolean = this.isAuthorized('VER TODOS CAMPOS SUSTENTABLE');
     editarCampos: boolean = this.isAuthorized('EDICION CAMPOS CREADOS')
+    borrarCampos: boolean = this.isAuthorized('EDICION CAMPOS CREADOS')
     esCorredor: boolean = sessionStorage.getItem("tipoUsuario") === "CORR";
     opcionesProveedores: any;
 
@@ -158,5 +159,44 @@ export class ListadoCamposComponent extends BaseComponent implements OnInit {
         );
 
         return false;
+    }
+
+    descargarKMZ(campoCosechaId: number, proveedorId: number) {
+        this.service.descargarArchivoKMZ(campoCosechaId, proveedorId).subscribe(
+            (result) => {
+                if (result.logout == true) {
+                    this.sessionDataService.logout();
+                }
+                else {
+                    var byteArray = new Uint8Array(result.FileContents);
+                    var blob = new Blob([byteArray], {
+                        type: "application/octet-stream",
+                    });
+
+                    if (window.navigator.msSaveOrOpenBlob) {
+                        // IE11
+                        window.navigator.msSaveOrOpenBlob(
+                            blob,
+                            result.FileDownloadName
+                        );
+                    } else {
+                        var url = window.URL.createObjectURL(blob);
+                        var link = document.createElement("a");
+                        document.body.appendChild(link);
+                        link.href = url;
+                        link.download = result.FileDownloadName;
+                        link.click();
+                        setTimeout(function () {
+                            window.URL.revokeObjectURL(url);
+                        }, 0);
+                        return false;
+                    }
+                }
+            },
+            (error) => {
+                //this.spinnerSmallComponent.hideIt();
+                this.mensajeComponent.setErrorMsg(error.message);
+            }
+        )
     }
 }
--- SustitucionMOA/src/app/venta-sustentable/venta-sustentable.service.ts
+++ SustitucionMOA/src/app/venta-sustentable/venta-sustentable.service.ts
@@ -150,4 +150,23 @@ export class VentaSustentableService extends BaseService {
             map(this.extractData)
         );
     }
+
+    descargarArchivoKMZ(campoCosechaId: number, proveedorId: number): Observable<any> {
+        this.headers = new Headers();
+        this.headers.append("Content-Type", "application/json");
+        this.headers.append("Accept", "q=0.8;application/json;q=0.9");
+        this.headers.append("Cache-control", "no-cache");
+        this.headers.append("Cache-control", "no-store");
+        this.headers.append("Expires", "0");
+        this.headers.append("Pragma", "no-cache");
+
+        let params: URLSearchParams = new URLSearchParams();
+        params.set("campoCosechaId", campoCosechaId.toString());
+        params.set("proveedorId", proveedorId.toString());
+
+        return this.http
+            //.get('/api/GestionImpuestos/DescargarFormularioCM05?idCabecera=' + idCabecera, { headers: this.headers })
+            .get('/api/CampoSustentable/DescargarArchivoKMZ', { search: params, headers: this.headers })
+            .pipe(map(this.extractData));
+    }
 }
