368bbcf42abc7321b0a16dc59d5312c7e2c39b04
Dario <dario.20398@gmail.com>
2021-09-03 15:32:26 -0300

    CM05: Agrego columnas Secuencia y SecuenciaId en la vista para RPA

--- SustitucionMOADatabase/scripts/Create View viewIngresosBrutosCoeficienteUnificado.sql
+++ SustitucionMOADatabase/scripts/Create View viewIngresosBrutosCoeficienteUnificado.sql
@@ -8,6 +8,8 @@ i.Sede,
 i.FechaCarga, 
 i.EstadoIngresosBrutosCoeficienteUnificado_Id AS EstadoId, 
 ei.Descripcion AS Estado,
+i.SecuenciaIngresosBrutosCoeficienteUnificado_Id AS SecuenciaId, 
+si.Descripcion AS Secuencia,
 id.Id AS DetalleId, 
 id.NumeroJurisdiccion, 
 id.Jurisdiccion, 
@@ -16,6 +18,7 @@ id.FechaCese,
 id.CoeficienteUnificado
 FROM IngresosBrutosCoeficienteUnificado i
 JOIN EstadoIngresosBrutosCoeficienteUnificado ei ON i.EstadoIngresosBrutosCoeficienteUnificado_Id = ei.Id
+JOIN SecuenciaIngresosBrutosCoeficienteUnificado si ON i.SecuenciaIngresosBrutosCoeficienteUnificado_Id = si.Id
 JOIN IngresosBrutosCoeficienteUnificadoDetalle id ON i.Id = id.IngresosBrutosCoeficienteUnificado_Id
 WHERE i.EstadoIngresosBrutosCoeficienteUnificado_Id = 2
 GO
