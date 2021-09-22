3c59d495e524f9a422f901427dd939e153a2899b
Villiate Enzo <evilliate@baufest.com>
2021-09-22 12:15:05 -0300

    Fix informe comercial "Debe seleccionar un proveedor"

--- SustitucionMOA/src/app/alta-proveedores/informe-comercial/informe-comercial.component.ts
+++ SustitucionMOA/src/app/alta-proveedores/informe-comercial/informe-comercial.component.ts
@@ -64,16 +64,19 @@ export class InformeComercialComponent extends BaseComponent implements OnInit {
         autocompletesLocalidad[i].setAttribute("autocomplete", "chrome-off");
       }
     }, 1000);
+
+    this.iniciarForm();
   }
 
   iniciarForm() {
-    if (this.proveedorId > 0) {
+    debugger
+    /*if (this.proveedorId > 0) {*/
       document.getElementById("openModalHiddenButton").click();
       this.obtenerCampanias();
-    }
+    /*}
     else {
       this.floatMsgService.setInfoMsg("No hay un proveedor seleccionado para completar el informe comercial.")
-    }
+    }*/
   }
 
 
