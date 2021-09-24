37314ee2b7addbcf6451e2e67692d9372c449ddb
Martin <m>
2021-09-24 15:25:38 -0300

    Saco guardado de solp

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -382,7 +382,7 @@ namespace SustitucionMOAUtils.Services
             }).ToList();
 
 
-            crearSolpConsumerMOA.Request(solpEntity);
+            //crearSolpConsumerMOA.Request(solpEntity);
 
 
             return solp;
