6a1e728d1b203b61e9a836bef11b9e4c67040c44
Dario <dario.20398@gmail.com>
2021-10-14 10:04:56 -0300

    Tabla y entidad ServicioSolp

---
+++ SustitucionMOAModel/Entities/ServicioSolp.cs
@@ -0,0 +1,15 @@
+﻿namespace SustitucionMOAModel.Entities
+{
+    public class ServicioSolp
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }//SERVICE
+        public string Descripcion { get; set; }//SHORT TEXT
+        public int? GrupoArticulos { get; set; }//MATL GROUP
+        public string TipoServicio { get; set; }//SERV CAT
+        public string AmbitoServicio { get; set; }//SERV TYPE
+        public int Edicion { get; set; }//EDITION
+        public string UnidadMedidaBase { get; set; }//BASE UOM
+        public string SSCItem { get; set; }//SSCItem
+    }
+}
