b189557934fac93ca1c6eddc4ae60c403fb2758d
Villiate Enzo <evilliate@baufest.com>
2021-11-02 11:09:47 -0300

    Agrego para que se pueda buscar sin 000

--- SustitucionMOAUtils/Services/HomeService.cs
+++ SustitucionMOAUtils/Services/HomeService.cs
@@ -114,7 +114,12 @@ namespace SustitucionMOAUtils.Services
             bool existePesificacionDelContrato = false;
             string fechaInicio = DateTime.Now.AddYears(-1).ToString("yyyy - MM - dd");
             string fechaFin = DateTime.Now.AddDays(+1).ToString("yyyy - MM - dd");
-            palabraABuscar.Trim().Replace("/t", "");
+            palabraABuscar.Trim().Replace("\t", "");
+
+            if(palabraABuscar.Length == 7)
+            {
+                palabraABuscar = String.Concat("000", palabraABuscar);
+            }
 
             try
             {
