57076e0d0e79c161f4a9d88f1bc57091e297b016
Rocio Orlando <rorlando@baufest.com>
2021-11-16 16:28:32 -0300

    feature/(ARMOA003-529): - Borrado de codigo comentado

--- SustitucionMOA/src/app/compras/SolpPasos/cabecera.component.ts
+++ SustitucionMOA/src/app/compras/SolpPasos/cabecera.component.ts
@@ -47,9 +47,6 @@ export class CabeceraComponent extends ListBaseComponent implements OnDestroy {
     claseDocumento: SelectItem[];
     centroEntrega: SelectItem[];
     monedaCompras: SelectItem[];
-    // articuloCompras: SelectItem[];
-    // solicitanteCompras: SelectItem[];
-    // grupoCompras: SelectItem[];
     almacenEntrega: SelectItem[];
     posiciones: SelectItem[];
     resultadoProveedores: string[];
@@ -57,8 +54,6 @@ export class CabeceraComponent extends ListBaseComponent implements OnDestroy {
     fechaDeLiberacion: any;
     hoy: Date = new Date();
     selectPosicion: any;
-
-    // solpActual: Solp;
 
     formularioPosicion: [FormGroup];
     formularioActual: FormGroup;
@@ -80,16 +75,7 @@ export class CabeceraComponent extends ListBaseComponent implements OnDestroy {
         { campo: 'concluido', esObligatorio: false, esFijo: true },
         { campo: 'indiceFijacion', esObligatorio: false, esFijo: true },
         { campo: 'selectCentroEntrega', esObligatorio: false, esFijo: false },
-        // { campo: 'nombreEntrega', esObligatorio: false, esFijo: true },
-        // { campo: 'codigoPostalEntrega', esObligatorio: false, esFijo: true },
         { campo: 'selectAlmacenEntrega', esObligatorio: false, esFijo: false },
-        // { campo: 'calleEntrega', esObligatorio: true, esFijo: true },
-        // { campo: 'paisEntrega', esObligatorio: false, esFijo: true },
-        // { campo: 'numeroEntrega', esObligatorio: true, esFijo: true },
-        // { campo: 'selectGrupoCompras', esObligatorio: false, esFijo: false },
-        // { campo: 'selectArticuloCompras', esObligatorio: true, esFijo: true },
-        // { campo: 'selectSolicitanteCompras', esObligatorio: true, esFijo: true },
-        // { campo: 'necesidadCompras', esObligatorio: false, esFijo: true },
         { campo: 'rubroElectrico', esObligatorio: false, esFijo: true },
         { campo: 'rubroCivil', esObligatorio: false, esFijo: true },
         { campo: 'rubroIngenieria', esObligatorio: false, esFijo: true },
@@ -258,12 +244,6 @@ export class CabeceraComponent extends ListBaseComponent implements OnDestroy {
                         x.CodigoDescripcion.toLowerCase().includes(event.query.toLowerCase()));
                 }
                 break;
-            // case 'GRUPO COMPRAS':
-            //     this.grupoCompras = this.combos.GrupoCompras.filter(x => x.CodigoDescripcion.toLowerCase().includes(event.query.toLowerCase()));
-            //     break;
-            // case 'ARTICULO COMPRAS':
-            //     this.articuloCompras = this.combos.GrupoArticulo.filter(x => x.CodigoDescripcion.toLowerCase().includes(event.query.toLowerCase()));
-            //     break;
             case 'MONEDA COMPRAS':
                 this.monedaCompras = this.combos.Moneda.filter(x => x.CodigoDescripcion.toLowerCase().includes(event.query.toLowerCase()));
                 break;
@@ -278,9 +258,4 @@ export class CabeceraComponent extends ListBaseComponent implements OnDestroy {
         el.scrollIntoView();
     }
 
-    // calcularFechaEntrega() {
-    //     let fechaNueva = new Date(this.model.fechaEntrega);
-    //     fechaNueva.setDate(fechaNueva.getDate() + parseInt(this.model.posicionActual.plazoDeEntrega.toString()));
-    //     this.model.posicionActual.fechaEntregaServicio = fechaNueva;
-    // }
 }
