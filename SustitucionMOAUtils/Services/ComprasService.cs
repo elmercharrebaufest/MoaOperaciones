bd4016c3347497cf8029b1960fca84bf53ee3a63
Martin <m>
2021-12-02 17:12:21 -0300

    Estado "Borrada en SAP"

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -1273,6 +1273,11 @@ namespace SustitucionMOAUtils.Services
                     ActualizarEstadoSolp(nroSolp, codigoSap.Id);
                 }
             }
+            else
+            {
+                var estadoEliminada = repositorio.Obtener<TablaSap>(x => x.Tabla == TablasSap.EstadoSolpSap && x.Descripcion == "Borrada");
+                ActualizarEstadoSolp(nroSolp, estadoEliminada.Id);
+            }
         }
 
         public void ActualizarEstadoSolp(string nroSolp, int idEstado)
