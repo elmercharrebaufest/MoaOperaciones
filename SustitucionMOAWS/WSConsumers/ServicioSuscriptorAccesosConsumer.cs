a4d6313ff1aef5c1bc7e0ab5548fa21253d07ed8
Becker Portilla <bportilla@baufest.com>
2021-12-07 07:44:48 -0300

    Integracion de api con servicio de accesos para autenticacion por qr

---
+++ SustitucionMOAWS/WSConsumers/ServicioSuscriptorAccesosConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SustitucionMOAModel.Models;
+using SustitucionMOAWS.Interfaces;
+using SustitucionMOAWS.ServicioSuscriptorAccesos;
+
+namespace SustitucionMOAWS.WSConsumers
+{
+    public class ServicioSuscriptorAccesosConsumer : IServicioSuscriptorAccesosConsumer
+    {
+        private readonly ServicioSuscriptorClient servicioSuscriptorClient = new ServicioSuscriptorClient();
+
+        public string ObtenerQRTransitoTemporal(string email)
+        {
+            return servicioSuscriptorClient.ObtenerQRTransitoTemporal(email);
+        }
+    }
+}
