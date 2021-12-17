b6d89874660efd4d6ace39cb70061f5fab9cc1ff
Sergio Amenta <samenta@baufest.com>
2021-12-17 11:20:51 -0300

    ARMOA003-725 : El corredor/cliente no tiene que poder ver el mensaje para los usuarios internos

--- SustitucionMOA/src/app/ordenes-de-carga/detalle/ordenes-de-carga.detalle.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/detalle/ordenes-de-carga.detalle.component.ts
@@ -93,7 +93,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
 
     //Está función va a desaparecer cuando hagamos el refactor de como mostrar los datos de esta pantalla
     verificarListado() {
-
+        debugger;
         if (this.esComercial) {
             this.mostrarListadoComercial = true;
             return;
@@ -115,6 +115,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
         }
     }
     verificarBotones() {
+        debugger;
         if (this.ordenDeCarga.Estado == EstadoOrdenDeCarga.Anulada) {
             return;
         }
@@ -155,6 +156,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
     }
 
     obtenerOrdenDeCarga() {
+        debugger;
         try {
             this.unsubscribe();
             this.subscriptionDropDowns = this.service.getOrdenDeCarga(this.ordenDeCargaId).subscribe(
@@ -166,7 +168,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
                     } else {
                         this.ordenDeCarga = result.data;
                         this.verificarBotones()
-                        if (this.ordenDeCarga.MensajeValidacionSAP != "") {
+                        if (this.ordenDeCarga.MensajeValidacionSAP != "" && this.esInterno) {
                             this.mensajeComponent.setInfoMsg(this.ordenDeCarga.MensajeValidacionSAP)
                         }
                     }
@@ -181,6 +183,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
 
 
     notificarTransporte() {
+        debugger;
         this.mensajeComponent.setMsgsEmpty();
         this.spinnerComponent.showIt();
         this.unsubscribe();
@@ -211,6 +214,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
 
 
     verificarTransporte() {
+        debugger;
         this.mensajeComponent.setMsgsEmpty();
         this.spinnerComponent.showIt();
         this.unsubscribe();
@@ -243,6 +247,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
     }
 
     verificarSituacionCrediticia() {
+        debugger;
         this.mensajeComponent.setMsgsEmpty();
         this.spinnerComponent.showIt();
         this.unsubscribe();
@@ -273,6 +278,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
     }
 
     abrirModalCorredores() {
+        debugger;
         this.mensajeComponent.setMsgsEmpty();
         this.spinnerComponent.showIt();
         this.unsubscribe();
@@ -303,6 +309,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
     }
 
     seleccionarContrato() {
+        debugger;
         this.mensajeComponent.setMsgsEmpty();
         this.spinnerComponent.showIt();
         this.unsubscribe();
@@ -334,6 +341,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
     }
 
     abrirModalContratos() {
+        debugger;
         this.mensajeComponent.setMsgsEmpty();
         this.spinnerComponent.showIt();
         this.unsubscribe();
