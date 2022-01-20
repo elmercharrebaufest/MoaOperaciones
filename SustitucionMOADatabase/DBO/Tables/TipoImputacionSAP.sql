f28ab2c16dfca9fcb36c97e7fd27e639ccc64ce6
eugenio martin <emartin@baufest.com>
2022-01-20 18:28:58 -0300

    fix tabla TipoImputacionSAP

--- SustitucionMOADatabase/DBO/Tables/TipoImputacionSAP.sql
+++ SustitucionMOADatabase/DBO/Tables/TipoImputacionSAP.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[TipoImputacionSAP]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[Descripcion] VARCHAR(200) NOT NULL,
 	[Codigo] VARCHAR(10) NOT NULL,
 	[TablaGeneral_Id] INT NULL,
