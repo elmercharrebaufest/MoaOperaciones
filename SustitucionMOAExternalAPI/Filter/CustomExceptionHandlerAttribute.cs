9178a82974f9a1c21adf782685bff6a42a143888
eugenio martin <emartin@baufest.com>
2022-03-30 17:43:28 -0300

    ARMOA003-988 - Crear Servicio  Actualizacion sap

---
+++ SustitucionMOAExternalAPI/Filter/CustomExceptionHandlerAttribute.cs
@@ -0,0 +1,63 @@
+﻿using SustitucionMOAUtils.Logger;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace SustitucionMOAExternalAPI.Filter
+{
+    public class CustomExceptionHandlerAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (!filterContext.ExceptionHandled)
+            {
+                try
+                {
+                    Log.ExternalAPIError(filterContext.Exception.GetOriginalException());
+                }
+                catch (Exception ex)
+                {
+                    Log.ExternalAPIError(ex);
+                }
+
+                filterContext.ExceptionHandled = true;
+            }
+            
+        }
+
+    }
+
+    public static class ExceptionExtensions
+    {
+        public static Exception GetOriginalException(this Exception ex)
+        {
+            if (ex.InnerException == null) return ex;
+
+            return ex.InnerException.GetOriginalException();
+        }
+
+        public static string GetAllFootprints(this Exception x)
+        {
+            var st = new StackTrace(x, true);
+            var frames = st.GetFrames();
+            var traceString = "";
+            foreach (var frame in frames)
+            {
+                if (frame.GetFileLineNumber() < 1)
+                    continue;
+
+                traceString +=
+                    "File: " + frame.GetFileName() +
+                    ", Method:" + frame.GetMethod().Name +
+                    ", LineNumber: " + frame.GetFileLineNumber();
+
+                traceString += "  -->  ";
+            }
+
+            return traceString;
+        }
+    }
+}
