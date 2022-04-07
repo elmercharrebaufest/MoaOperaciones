d66c95a46d726c1bd78b07f907ec86aa10ff01bf
Sergio <samenta@baufest.com>
2022-04-07 11:10:19 -0300

    se comento

--- SustitucionMOAUtils/Services/HomeService.cs
+++ SustitucionMOAUtils/Services/HomeService.cs
@@ -242,7 +242,7 @@ namespace SustitucionMOAUtils.Services
 
                         if (cartasPorteDescargas == null)
                         {
-                            cartasPorteDescargas = (CartaPorteDescargaWSMOAResponse)new RecepcionesConsumerMOA().request(proveedor, new List<FechaWS>(), new List<string> { palabra });
+                            cartasPorteDescargas = (CartaPorteDescargaWSMOAResponse)new RecepcionesConsumerMOA().request(proveedor, new List<FechaWS>(), new List<string> { "010101781256" });
                         }
                     }
                     catch (Exception e)
