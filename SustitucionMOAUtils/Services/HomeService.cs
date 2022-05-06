f75d840a63f052307f4dd98b9a7298985bfbb6f0
eugenio martin <emartin@baufest.com>
2022-05-06 14:44:26 -0300

    fix parameters y buscador

--- SustitucionMOAUtils/Services/HomeService.cs
+++ SustitucionMOAUtils/Services/HomeService.cs
@@ -237,12 +237,12 @@ namespace SustitucionMOAUtils.Services
                     {
                         if (cartasPorteAplicacion == null)
                         {
-                            cartasPorteAplicacion = (CartaPorteWSMOAResponse)new AplicacionesConsumerMOA().request(proveedor, new List<FechaWS> { new FechaWS { fechaFin = DateTime.Now.Date.AddDays(1), fechaInicio = new DateTime(2010, 1, 1) } }, new List<string> { palabra });
+                            cartasPorteAplicacion = (CartaPorteWSMOAResponse)new AplicacionesConsumerMOA().request(proveedor, new List<FechaWS> { new FechaWS { fechaFin = DateTime.Now.Date.AddDays(1), fechaInicio = DateTime.Now.Date.AddMonths(-6) } }, new List<string> { palabra });
                         }
 
                         if (cartasPorteDescargas == null)
                         {
-                            cartasPorteDescargas = (CartaPorteDescargaWSMOAResponse)new RecepcionesConsumerMOA().request(proveedor, new List<FechaWS> { new FechaWS { fechaFin = DateTime.Now.Date.AddDays(1), fechaInicio = new DateTime(2010, 1, 1) } }, new List<string> { palabra });
+                            cartasPorteDescargas = (CartaPorteDescargaWSMOAResponse)new RecepcionesConsumerMOA().request(proveedor, new List<FechaWS> { new FechaWS { fechaFin = DateTime.Now.Date.AddDays(1), fechaInicio = DateTime.Now.Date.AddMonths(-6) } }, new List<string> { palabra });
                         }
                     }
                     catch (Exception e)
