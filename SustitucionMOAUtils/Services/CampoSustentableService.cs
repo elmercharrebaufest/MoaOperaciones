6e5af7bacff0380c584c1741d23670874a2a8f75
ariera <ariera@baufest.com>
2022-05-12 22:51:35 -0300

    ARMOA003-1102-Bug cargar Archivos sustentables

--- SustitucionMOAUtils/Services/CampoSustentableService.cs
+++ SustitucionMOAUtils/Services/CampoSustentableService.cs
@@ -41,14 +41,12 @@ namespace SustitucionMOAUtils.Services
             this.dataAgroService = dataAgroService;
         }
 
-        public Resultado Agregar(string mailUsuario, CampoProveedor campoProveedor, HttpPostedFileBase archivoKmz)
+        public Resultado Agregar(string mailUsuario, CampoProveedor campoProveedor, HttpPostedFileBase archivoKmz, bool UsarArchivoId)
         {
+            string ruta = "";
             var usuario = repositorio.Obtener<Usuario>(u => u.Mail == mailUsuario);
-
             ValidarUsuario(usuario, campoProveedor.Proveedor_Id);
-
             ValidarCampo(usuario, campoProveedor, archivoKmz);
-
 
             var declaracion = repositorio.Obtener<DeclaracionCampoSustentable>(d => d.Cosecha_Id == campoProveedor.CampoCosecha.Cosecha_Id && d.CUIT == campoProveedor.CUIT);
 
@@ -59,18 +57,25 @@ namespace SustitucionMOAUtils.Services
             {
                 campoProveedor.Archivo = (new Archivo { FileKey = FileKeys.CampoSustentableKMZ, Ruta = "" });
             }
+            else
+            {
+                var archivoCampo = repositorio.Obtener<Archivo>(a => a.Id == campoProveedor.Archivo_Id);
+                ruta = archivoCampo.Ruta;
+            }
             campoProveedor.CampoCosecha.ToneladasAprobadas = -1;
 
             repositorio.Agregar(campoProveedor);
 
             repositorio.GuardarCambios();
 
-            if (campoProveedor.Archivo_Id == 0)
+            if (UsarArchivoId == false)
             {
                 GuardarArchivoKMZ(campoProveedor, archivoKmz);
-                repositorio.GuardarCambios();
+                repositorio.GuardarCambios();                
+
             }
-            var archivo = ConvertirArchivo64(archivoKmz);
+            //var ruta = ObtenerRutaArchivoKMZ(campoProveedor.CampoCosecha_Id, campoProveedor.Proveedor_Id);
+            var archivo = archivoKmz == null ? Convert.ToBase64String(System.IO.File.ReadAllBytes(ruta)) : ConvertirArchivo64(archivoKmz);
             InformarCampoSustentable(campoProveedor, archivo);
             return new Resultado { IdEntidad = campoProveedor.CampoCosecha_Id, Mensaje = SuccessMsg.CampoSustentableAgregado };
         }
