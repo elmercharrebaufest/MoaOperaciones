50adbeae47e1b996c482c70edb229b9deecb4016
Luis André Tolentino <ltolentino@baufest.com>
2022-07-13 13:51:52 -0300

    Merged PR 801: feat-cm05: Gestión de impuestos CM05
    Gestión de impuestos CM05

--- SustitucionMOAModel/Enums/EnumTipoMovimientoIngresosBrutosCoeficienteUnificado.cs
+++ SustitucionMOAModel/Enums/EnumTipoMovimientoIngresosBrutosCoeficienteUnificado.cs
@@ -6,6 +6,9 @@
         Autorizacion = 2,
         AutorizacionRevertida = 3,
         ExportacionExitosa = 4,
-        Error = 5
+        Error = 5,
+        EdicionEstado = 6,
+        EdicionCabecera = 7,
+        EdicionDetalle = 8,
     }
 }
