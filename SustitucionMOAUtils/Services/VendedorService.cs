7fe9491ee1eecc105b396350e3e2a493cbfa14af
Dylan Lopez <dylopez@baufest.com>
2022-08-22 16:41:06 -0500

    fix(ARMOA003-1060): A la validacion que agrega si CUIT es cadena vacia tambien devuelva el CUIT

--- SustitucionMOAUtils/Services/VendedorService.cs
+++ SustitucionMOAUtils/Services/VendedorService.cs
@@ -149,7 +149,7 @@ namespace SustitucionMOAUtils.Services
                     IdDataAgro = prov.IdDataAgro,
                     Mail = prov.Mail ?? "",
                     Observaciones = prov.Observaciones,
-                    RazonSocial = prov.RazonSocial ?? prov.CUIT,
+                    RazonSocial = !String.IsNullOrEmpty(prov.RazonSocial) ? prov.RazonSocial : prov.CUIT,
                     FechaSolicitud = prov.FechaSolicitud,
                     Comercial = prov.Comercial,
                     EstadoSIPER = prov.EstadoSIPER,
