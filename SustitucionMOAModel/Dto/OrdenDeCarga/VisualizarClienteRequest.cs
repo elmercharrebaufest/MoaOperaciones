59cec0b96ce11197e7d7d5b7abe0ef3bcf101329
Dylan Lopez <dylopez@baufest.com>
2022-08-25 10:46:11 -0500

    fix(ARMOA003-1677): Cambios para seleccionar el producto cuando se selecciona un contrato

---
+++ SustitucionMOAModel/Dto/OrdenDeCarga/VisualizarClienteRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SustitucionMOAModel.Dto.OrdenDeCarga
+{
+	public class VisualizarClienteRequest
+	{
+		public string Corredor { get; set; }
+		public string FechaInicio { get; set; }
+		public string FechaFin { get; set; }
+		public string Pendiente { get; set; }
+	}
+}
