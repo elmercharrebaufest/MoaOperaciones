c501a96e994546b90cff96289bd793f9c505372f
eugenio martin <emartin@baufest.com>
2022-09-06 16:57:36 -0300

    ARMOA003-1680

--- SustitucionMOAUtils/Services/OrdenDeCargaService.cs
+++ SustitucionMOAUtils/Services/OrdenDeCargaService.cs
@@ -2020,7 +2020,7 @@ namespace SustitucionMOAUtils.Services
                 }
             }
 
-            var fechaVencimiento = DateTime.Now.AddDays(dias);
+            var fechaVencimiento = desde.AddDays(dias);
             return fechaVencimiento;
         }
         #endregion
