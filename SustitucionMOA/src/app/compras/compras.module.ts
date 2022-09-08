68f7be72369ce27935053bdf544107f2afb93492
Israel Arellano <iarellano@baufest.com>
2022-09-08 17:59:45 -0500

    Popup contrato marco creado

--- SustitucionMOA/src/app/compras/compras.module.ts
+++ SustitucionMOA/src/app/compras/compras.module.ts
@@ -47,6 +47,7 @@ import { TabProveedoresComponent } from './solp/steps/posicion/tab-proveedores/t
 import { TabSubposicionComponent } from './solp/steps/posicion/tab-subposicion/tab-subposicion.component';
 import { StepperActionsComponent } from './solp/stepper-actions/stepper-actions.component';
 import { FinalizarSolpComponent } from './solp/finalizar/finalizar-solp.component';
+import { ContratoMarcoComponent } from './solp/steps/posicion/contrato-marco/contrato-marco.component';
 
 @NgModule({
     imports: [
@@ -96,7 +97,8 @@ import { FinalizarSolpComponent } from './solp/finalizar/finalizar-solp.componen
         TabSubposicionComponent,
         TabImputacionesComponent,
         StepperActionsComponent,
-        FinalizarSolpComponent
+        FinalizarSolpComponent,
+        ContratoMarcoComponent
     ],
     providers: [
         ComprasService,
