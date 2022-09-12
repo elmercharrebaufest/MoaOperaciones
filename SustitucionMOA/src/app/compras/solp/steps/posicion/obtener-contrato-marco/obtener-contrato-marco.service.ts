26e556561d2a26162f672396da1f707590010db4
César Marchena <cmarchena@baufest.com>
2022-09-12 13:45:42 -0500

    obtener contrato marco popup

---
+++ SustitucionMOA/src/app/compras/solp/steps/posicion/obtener-contrato-marco/obtener-contrato-marco.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ObtenerContratoMarcoService implements OnDestroy {
+  public toogleOn = new Subject<boolean>();
+
+  private visible = false;
+
+  ngOnDestroy() {
+    if (this.visible) { 
+      this.toogleOn.unsubscribe();
+    }
+  }
+
+  show(value: boolean) {
+    this.visible = value;
+    this.toogleOn.next(this.visible);
+  }
+
+  close() {
+    this.visible = false;
+    this.toogleOn.next(this.visible);
+  }
+}
