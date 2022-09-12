c0c5c601352e42d3e92ce42e0fcd538439ebaadb
ariera <ariera@baufest.com>
2022-09-12 20:38:41 -0300

    ARMOA003-1794-Se habilita boton solictar anulacion en Edicion Rechazada

--- SustitucionMOA/src/app/ordenes-de-carga/detalle/ordenes-de-carga.detalle.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/detalle/ordenes-de-carga.detalle.component.ts
@@ -258,7 +258,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
         //    return;
         //}
 
-        if (!(this.ordenDeCarga.Estado == EstadoOrdenDeCarga.AnulacionSolicitada || this.ordenDeCarga.Estado == EstadoOrdenDeCarga.EdicionSolicitada || this.ordenDeCarga.Estado == EstadoOrdenDeCarga.EdicionRechazada)) {
+        if (!(this.ordenDeCarga.Estado == EstadoOrdenDeCarga.AnulacionSolicitada || this.ordenDeCarga.Estado == EstadoOrdenDeCarga.EdicionSolicitada)) {
             if (sessionStorage.getItem("tipoUsuario") == "CLI")
             this.mostrarBotonSolicitarAnulacion = true;
             this.mostrarBotonEditar = true;
