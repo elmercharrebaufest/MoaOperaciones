f49f055a0d3b06c113a57b74d21dac2ef2a250ff
Israel Arellano <iarellano@baufest.com>
2022-09-13 16:14:02 -0500

    paso de datos entre contrato marco y cabecera

--- SustitucionMOA/src/app/compras/solp/steps/posicion/contrato-marco/contrato-marco.ts
+++ SustitucionMOA/src/app/compras/solp/steps/posicion/contrato-marco/contrato-marco.ts
@@ -1,11 +1,18 @@
 export interface ContratoMarco {
-    ProveedorFijo: number;
+    ProveedorFijo: string;
     NombreProveedor: string;
-    idContrato: number;
+    NumeroContratoSuperior: string;
+    NumeroMaterial: string;
+    NumeroPosicionContratoSuperior: string;
+    NumeroRegistroInfoCompras: string;
     UnidadMedida: string;
+    OrganizacionCompras: string;
+    CentroAprovisionamiento: string;
+    TipoDocumentoCompras: string;
+    TipoPosicionDocumento: string;
+    TipoPosicionDocumentoCompras: string;
+    idContrato: string;
     cantidad: string;
     precioTotal: string;
     moneda: string;
-    OrganizacionCompras: string;
-    CentroAprovisionamiento: string;
 }
