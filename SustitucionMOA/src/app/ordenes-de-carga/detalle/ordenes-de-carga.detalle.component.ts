02336ea4d40e842b745b94325acb8f7a46781037
eugenio martin <emartin@baufest.com>
2022-09-14 15:24:19 -0300

    fix mostrarBotonVerificarTransporte

--- SustitucionMOA/src/app/ordenes-de-carga/detalle/ordenes-de-carga.detalle.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/detalle/ordenes-de-carga.detalle.component.ts
@@ -296,6 +296,7 @@ export class OrdenesDeCargaDetalleComponent extends BaseComponent implements OnI
 
             if (!this.ordenDeCarga.TransporteExiste) {
                 this.mostrarBotonNotificarTransporte = true;
+                this.mostrarBotonVerificarTransporte = true;
             }
 
             if (this.ordenDeCarga.Estado == EstadoOrdenDeCarga.PendienteAprobacionCredito) {
