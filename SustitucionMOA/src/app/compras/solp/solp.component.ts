6ab3a2057b3b393aaf4c1927357ef3c16ad8fb55
César Marchena <cmarchena@baufest.com>
2022-09-21 15:04:07 -0500

    implementacion de metodo agregar items desde contrato marco

--- SustitucionMOA/src/app/compras/solp/solp.component.ts
+++ SustitucionMOA/src/app/compras/solp/solp.component.ts
@@ -718,7 +718,6 @@ export class SolpComponent extends BaseComponent implements OnInit {
                                 }
                                 this.listadoErrores = result.Errores;
                                 this.displayErrorSAP = true;
-
                             }
                             // if (this.solpActual.nroSolp) {
                             //     this.displaySAPEditar = true;
