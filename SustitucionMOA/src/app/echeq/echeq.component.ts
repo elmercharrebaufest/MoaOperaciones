79aa850e93feaf95ac78230e7936d1f42ee08954
Rocio Orlando <rorlando@baufest.com>
2022-10-03 17:33:22 -0300

    feature/(ARMOA003-1229): - Echeq

--- SustitucionMOA/src/app/echeq/echeq.component.ts
+++ SustitucionMOA/src/app/echeq/echeq.component.ts
@@ -33,10 +33,12 @@ export class EcheqBaseComponent extends ListBaseComponent {
         this.checkPermisos();
         sessionStorage.getItem("proveedor");
         this.navService.setSeccionList([
-            new Seccion('/echeq/gestion', 'echeq', 'Gestion'),
+                new Seccion('/echeq/gestion', 'echeq', 'Gestion'),
             new Seccion('/echeq/mis-cheq', 'echeq', 'Mis Echeq')
-        ]);    
+            ]);
     }
 
-   
+        // this.getData();
+    }
+
 }
--- SustitucionMOAWS/WSConsumers/EcheqVisualizarPendientePagoConsumerMOA.cs
+++ SustitucionMOAWS/WSConsumers/EcheqVisualizarPendientePagoConsumerMOA.cs
@@ -16,7 +16,7 @@ namespace SustitucionMOAWS.WSConsumers
     {
         SI_MPRFC_VISU_PENDIENTE_PAGOClient service = new SI_MPRFC_VISU_PENDIENTE_PAGOClient();
 
-        public EcheqVisualizarPendientePagoConsumerMOA()
+        public echeqVisualizarPendientePagoConsumerMOA()
         {
             service = new SI_MPRFC_VISU_PENDIENTE_PAGOClient();
             service.ClientCredentials.UserName.UserName = SAPCredential.getUserName();
