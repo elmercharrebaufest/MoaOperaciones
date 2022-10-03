f90dd8eea6d5a06ea994c4c38e9206eb1c2a6721
Rocio Orlando <rorlando@baufest.com>
2022-10-03 17:33:22 -0300

    feature/(ARMOA003-1229): - Echeq

--- SustitucionMOA/src/app/echeq/echeq-routing.module.ts
+++ SustitucionMOA/src/app/echeq/echeq-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     { path: '', component: EcheqGestionComponent },
     { path: "gestion", component: EcheqGestionComponent }
 
+const routes: Routes = [
+    { path: '', component: GestionEcheqComponent },
+    { path: 'gestion', component: GestionEcheqComponent },
+    { path: 'mis-echeq', component: MisEcheqComponent },
 ];
 
 @NgModule({
