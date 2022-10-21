41f2bc606aa0022dd8e492927f005c6e7162b7d6
Dylan Lopez <dylopez@baufest.com>
2022-10-20 23:38:03 -0500

    fix: Cambiando validarSiNoEstaEnSAP

--- SustitucionMOA/src/app/ordenes-de-carga/listado/ordenes-de-carga.listado.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/listado/ordenes-de-carga.listado.component.ts
@@ -259,9 +259,9 @@ export class OrdenesDeCargaListado extends ListBaseComponent implements OnInit {
         console.debug('call validarSiNoEstaEnSAP()');
         this.data.forEach((value, index) => {
             if (value.NoEstaEnSAP){
-                return true;
+                return false;
             }
         });
-        return false;
+        return true;
     }
 }
