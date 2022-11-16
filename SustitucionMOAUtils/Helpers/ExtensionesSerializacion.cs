13431d5b8f5e4d755cb8d460f412897ec949fac1
eugenio martin <emartin@baufest.com>
2022-11-16 10:02:00 -0300

    fix ToJson

--- SustitucionMOAUtils/Helpers/ExtensionesSerializacion.cs
+++ SustitucionMOAUtils/Helpers/ExtensionesSerializacion.cs
@@ -1,4 +1,6 @@
-﻿using SustitucionMOAModel.Entities;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SustitucionMOAModel.Entities;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,9 +25,22 @@ namespace SustitucionMOAUtils.Helpers
                     return Encoding.UTF8.GetString(ms.ToArray());
                 }
             }
-            catch (System.Exception)
+            catch (Exception)
             {
-                return "error al serializar el objeto.";
+                try
+                {
+                    string jsonObjecto = JsonConvert.SerializeObject(data, new JsonSerializerSettings()
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                        ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                        PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                    });
+                    return jsonObjecto;
+                }
+                catch (Exception)
+                {
+                    return "error al serializar el objeto.";
+                }
             }
 
         }
@@ -66,7 +81,7 @@ namespace SustitucionMOAUtils.Helpers
             return source;
         }
 
-        public static IList<T> CloneList<T>(this IList<T> source) where T: ICloneable
+        public static IList<T> CloneList<T>(this IList<T> source) where T : ICloneable
         {
             return source.Select(item => (T)item.Clone()).ToList();
         }
