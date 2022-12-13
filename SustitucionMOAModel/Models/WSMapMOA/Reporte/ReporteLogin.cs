4326848c13a328ab7e0f4a922b00087c2fd023ca
Ali Rigol <mrigol@baufest.com>
2022-12-13 11:31:50 -0300

    feat/ARMOA003-984: arreglo formato fecha de último login

--- SustitucionMOAModel/Models/WSMapMOA/Reporte/ReporteLogin.cs
+++ SustitucionMOAModel/Models/WSMapMOA/Reporte/ReporteLogin.cs
@@ -20,7 +20,7 @@ namespace SustitucionMOAModel.Models.WSMapMOA.Reporte
         {
             this.Mail = Mail;
             this.CUITRegistro = CUITRegistro;
-            this.UltimoLogin = UltimoLogin?.ToShortDateString() ?? string.Empty;
+            this.UltimoLogin = UltimoLogin?.ToString("dd/MM/yyyy hh:mm:ss") ?? string.Empty;
             this.Nombre = Nombre;
             this.CUITProveedor = CUITProveedor ?? string.Empty;
             this.RazonSocial = RazonSocial ?? string.Empty;
