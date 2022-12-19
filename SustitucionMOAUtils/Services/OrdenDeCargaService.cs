ccf39a755d67117210d30950b56a9c02b04a7a00
Ali Rigol <mrigol@baufest.com>
2022-12-19 17:05:12 -0300

    feat/ARMOA003-1897: crear job que valide la situacion crediticia de las ordenes de carga

--- SustitucionMOAUtils/Services/OrdenDeCargaService.cs
+++ SustitucionMOAUtils/Services/OrdenDeCargaService.cs
@@ -2278,5 +2278,16 @@ namespace SustitucionMOAUtils.Services
             return SuccessMsg.OrdenDeCargaActualizada;
 
         }
+        public void VerificarSituacionCrediticiaJob()
+        {
+            if (!repositorio.Obtener<HabilitacionJob>(a => a.Nombre == "VerificarSituacionCrediticia").Habilitado)
+                return;
+
+            var ordenes = repositorio.Listar<OrdenDeCarga>();
+            foreach(OrdenDeCarga orden in ordenes)
+            {
+                VerificarSituacionCrediticia(orden, true);
+            }
+        }
     }
 }
