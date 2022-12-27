aab287c137de8c221eff865e35d37160b72009f3
Ali Rigol <mrigol@baufest.com>
2022-12-27 13:40:43 -0300

    feat/ARMOA003-1941: agregar renderizado listado; agregar datos modelo aplicaiones ccpp

--- SustitucionMOA/src/app/aplicacion-ccpp/aplicacion-ccpp.model.ts
+++ SustitucionMOA/src/app/aplicacion-ccpp/aplicacion-ccpp.model.ts
@@ -1,14 +1,34 @@
 import { DropdownOption } from '../common/view-child/dropdown/dropdown.component';
-export enum EstadoAplicacionCCPP{
+export enum EstadoAplicacionCCPP {
     Pendiente,
     Aplicado,
     Error
 }
-export interface AplicacionCCPP{}
+export type ColorEstado = 'red'|'green'|'orange'
+
+export interface AplicacionCCPP {
+    Id: number;
+    Proveedor_Id?: number;
+    Proveedor?: any;
+    Contrato: string;
+    CartaPorte: string;
+    Estado: EstadoAplicacionCCPP
+    Kilogramos: number;
+    Usuario_Id?: number;
+    Usuario?: any;
+    MailUsuario?: string;
+    RazonSocial?: string;
+    ColorEstado?: ColorEstado;
+    LabelEstado?: string;
+    Error?: string;
+    FechaAlta: Date;
+    FechaActualizacion: Date;
+}
+
 export const SeccionAplicacionCCPP = "aplicaciones-ccpp";
 export const estadosAplicacionCCPP: DropdownOption[] = [
-    {value:"", label: "Todos"},
-    {value:EstadoAplicacionCCPP.Aplicado.toString(), label: "Aplicado"},
-    {value:EstadoAplicacionCCPP.Error.toString(), label: "Error"},
-    {value:EstadoAplicacionCCPP.Pendiente.toString(), label: "Pendiente"},
+    { value: "", label: "Todos" },
+    { value: EstadoAplicacionCCPP.Aplicado.toString(), label: "Aplicado" },
+    { value: EstadoAplicacionCCPP.Error.toString(), label: "Error" },
+    { value: EstadoAplicacionCCPP.Pendiente.toString(), label: "Pendiente" },
 ] 
