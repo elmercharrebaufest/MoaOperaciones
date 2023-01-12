b66acf0ab8b8efcdf87fe2ceaccddb8e213eb6ff
Ali Rigol <mrigol@baufest.com>
2023-01-12 16:46:16 -0300

    feat/ARMOA003-1953: remover bad import

--- SustitucionMOAUtils/Services/OrdenDeCargaService.cs
+++ SustitucionMOAUtils/Services/OrdenDeCargaService.cs
@@ -23,7 +23,6 @@ using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
-using System.util.collections;
 using Mod = SustitucionMOAModel.Models;
 
 namespace SustitucionMOAUtils.Services
