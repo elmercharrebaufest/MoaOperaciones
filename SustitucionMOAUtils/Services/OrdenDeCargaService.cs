1b340789086f29d1c7631e1a372eab4ef690b2e0
Ali Rigol <mrigol@baufest.com>
2023-01-31 17:25:07 -0300

    feat/ARMOA003-1972:agregar mensaje para cuando no se hayan hecho modificaciones(no se encuentra ningún orden en ese estado)
    (cherry picked from commit 9b0fca2c2b47a910d13be077d70735dbfce300d1)

--- SustitucionMOAUtils/Services/OrdenDeCargaService.cs
+++ SustitucionMOAUtils/Services/OrdenDeCargaService.cs
@@ -2402,6 +2402,8 @@ namespace SustitucionMOAUtils.Services
         public string EnviarOrdenesASAP(List<int> ordenesId)
         {
             var ordenes = repositorio.Listar<OrdenDeCarga>(a => ordenesId.Contains(a.Id) && a.Estado == EstadoOrdenDeCarga.SinEnviarASAP);
+            if (ordenes.Count() == 0)
+                throw new InfoCustomException("No se han hecho modificaciones");
             var correctas = new List<int>();
             var incorrectas = new List<int>();
             foreach (var ordenDeCarga in ordenes)
@@ -2427,6 +2429,7 @@ namespace SustitucionMOAUtils.Services
                     incorrectas.Add(ordenDeCarga.Id);
                 }
             }
+            
             if (correctas.Count() == 0 && incorrectas.Count() > 0)
                 throw new Exception($"Error al intentar enviar las ordenes solicitadas");
             if (correctas.Count() < incorrectas.Count())
