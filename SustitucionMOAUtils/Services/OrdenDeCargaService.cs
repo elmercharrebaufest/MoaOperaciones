3500ac67018545aa1629d0001f5199d68a9defdf
Ali Rigol <mrigol@baufest.com>
2023-02-02 12:24:14 -0300

    feat/ARMOA003-1972: agregar condicion para enviar a SAP

--- SustitucionMOAUtils/Services/OrdenDeCargaService.cs
+++ SustitucionMOAUtils/Services/OrdenDeCargaService.cs
@@ -2415,7 +2415,7 @@ namespace SustitucionMOAUtils.Services
                     NumeroPedidoIngresado = ordenDeCarga.NumeroPedidoIngresado,
                     MailUsuarioSAP = String.Empty
                 };
-                CrearOrdenEnSAP(crearOrdenEnSAPRequest);
+                CrearOrdenEnSAP(crearOrdenEnSAPRequest, true);
             }
             return $"Se han enviado las ordenes";
         }
