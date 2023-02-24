7dd14328b93545318714ee12c18e90d2e58b4654
Rocio Orlando <rorlando@baufest.com>
2023-02-24 16:12:14 -0300

    feature/(ARMOA018-50): - add boton de descargas al dash del comprador

--- SustitucionMOA/src/app/compras/dashboard-comprador/listado-dashboard-comprador/listado-dashboard-comprador.component.ts
+++ SustitucionMOA/src/app/compras/dashboard-comprador/listado-dashboard-comprador/listado-dashboard-comprador.component.ts
@@ -144,5 +144,49 @@ export class ListadoDashboardCompradorComponent extends ListBaseComponent {
 
     }
 
+    generarZipPliego(idSolp) {
+        this.blockUI.start('Generando ')
+        this.service.descargarZipPliego(idSolp)
+            .subscribe(
+                (result) => {
+                    if (result.logout == true) {
+                        this.sessionDataService.logout();
+                    }
+                    else {
+                        var byteArray = new Uint8Array(result.FileContents);
+                        var blob = new Blob([byteArray], {
+                            type: "application/octet-stream",
+                        });
+
+                        if (window.navigator.msSaveOrOpenBlob) {
+                            // IE11
+                            window.navigator.msSaveOrOpenBlob(
+                                blob,
+                                result.FileDownloadName
+                            );
+                        } else {
+                            var url = window.URL.createObjectURL(blob);
+                            var link = document.createElement("a");
+                            document.body.appendChild(link);
+                            link.href = url;
+                            link.download = result.FileDownloadName;
+                            link.click();
+                            setTimeout(function () {
+                                window.URL.revokeObjectURL(url);
+                            }, 0);
+                            this.blockUI.stop();
+                            return false;
+                        }
+                        this.blockUI.stop();
+                    }
+                },
+                (error) => {
+                    this.spinnerSmallComponent.hideIt();
+                    this.mensajeComponent.setErrorMsg(error.message);
+                }
+            )
+    }
+
+
 }
 
