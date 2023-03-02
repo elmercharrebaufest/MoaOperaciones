a584452a332a18896bb19d5a2f70a2330047fb35
Brisa Melgarejo <bmelgarejo@baufest.com>
2023-03-02 16:47:33 -0300

    ARMOA018-40
    Se agrego el nuevo metodo para listar los proveedores

--- SustitucionMOA/Controllers/ComprasController.cs
+++ SustitucionMOA/Controllers/ComprasController.cs
@@ -712,6 +712,26 @@ namespace SustitucionMOA.Controllers
             }
         }
 
+        [HttpGet]
+        public ActionResult ListarProveedores(string filtro)
+        {
+            try
+            {               
+                var result = usuarioService.ListarProveedores(filtro);
+                return JsonCustom(new { data = result });
+            }
+            catch (WSCustomException e)
+            {
+                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
+                return Json(new { error = ErrorMsg.ErrorWS }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
+                return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
 
     }
 }
