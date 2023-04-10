537c233bce37f03889db4dce0822b078f17b85a1
Ali Rigol <mrigol@baufest.com>
2023-04-10 16:16:23 -0300

    feat/ARMOA003-2070: agregar campos cuits necesarios; con validaciones y servicios

--- SustitucionMOAWS/Interfaces/IOrdenCargaConsumerMOA.cs
+++ SustitucionMOAWS/Interfaces/IOrdenCargaConsumerMOA.cs
@@ -24,5 +24,6 @@ namespace SustitucionMOAWS.Interfaces
         ResultadoGenerico AnularEntregaOrdenCarga(string nroEntrega);
         ResultadoGenerico ModificarEntregaOrdenCarga(ModificarEntregaOrdenCargaSAP datosEntrega);
         bool VerificarContratoAbierto(string contrato);
+        string VericarEstadoSISA(string cuit);
     }
 }
