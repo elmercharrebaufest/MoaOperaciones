48ac7199fac0eaaf3eb36ef97c1cc700882cc6a0
Ali Rigol <mrigol@baufest.com>
2023-04-11 12:23:21 -0300

    feat/ARMOA003-2070: rollback validaciones seran en otro tk, además de estar mal hechas

--- SustitucionMOA/Controllers/OrdenDeCargaController.cs
+++ SustitucionMOA/Controllers/OrdenDeCargaController.cs
@@ -745,27 +745,5 @@ namespace SustitucionMOA.Controllers
             }
             return JsonCustom(response);
         }
-        [HttpGet]
-        public ActionResult ValidarCUIT(string cuit)
-        {
-            try
-            {
-                var data = ordenDeCargaService.ValidarCUIT(cuit);
-                return JsonCustom(new { data });
-            }
-            catch (InfoCustomException e)
-            {
-                return Json(new { info = e.Message }, JsonRequestBehavior.AllowGet);
-            }
-            catch (ValidationCustomException e)
-            {
-                return Json(new { error = e.Message }, JsonRequestBehavior.AllowGet);
-            }
-             catch (Exception e)
-            {
-                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
-                return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
-            }
-        }
     }
 }
