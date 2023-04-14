a507c3866839fb082f3c3251a352825970c72ad6
Diego Hidalgo <dhidalgo@baufest.com>
2023-04-14 01:51:07 -0300

    ARMOA003-2110 Adaptar RFC ControlCarga para que reciba más de una respuesta. Se agrega clase para manejar las respuestas de SAP

--- SustitucionMOAWS/Interfaces/IOrdenCargaConsumerMOA.cs
+++ SustitucionMOAWS/Interfaces/IOrdenCargaConsumerMOA.cs
@@ -3,6 +3,7 @@ using SustitucionMOAModel.Entities;
 using SustitucionMOAModel.Models;
 using SustitucionMOAModel.Models.WSMapMOA.OrdenCarga;
 using SustitucionMOAWS.Enum.OrdenCargaConsumer;
+using SustitucionMOAWS.ResponseHandler.OrdenCarga;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,7 @@ namespace SustitucionMOAWS.Interfaces
 {
     public interface IOrdenCargaConsumerMOA
     {
-        string ControlCargaRequest(string cliente, string contrato, string corredor, string cuit, string material, string pedido, string soloSisa);
+        ControlCargaResponseHandler ControlCargaRequest(string cliente, string contrato, string corredor, string cuit, string material, string pedido, string soloSisa);
         string CrearOrdenRequest(string cliente, string contrato, string corredor, decimal kilos, string material, string pedidoInput, string usuarioSAP, string forzarCreacion, out string pedidoOutput);
         string OrdenCargaControlEstadoRequest(string entrega, string pedido, string transportista);
         string OrdenCargaEntregadaRequest(string documento, decimal kilos, string nombreConductor, string patenteAcoplado, string patenteChasis, string pedido, string tipoDocumento, string transportista, out string mensaje);
