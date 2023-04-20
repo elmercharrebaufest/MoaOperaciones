b1a5fa88bd8a82c43519fb8d1b4bbca0d6229ceb
eugenio martin <emartin@baufest.com>
2023-04-20 15:04:58 -0300

    fix codigo duplicado

--- SustitucionMOA/src/app/compras/dashboard-proveedor/listado-dashboard-proveedor/listado-dashboard-proveedor.component.ts
+++ SustitucionMOA/src/app/compras/dashboard-proveedor/listado-dashboard-proveedor/listado-dashboard-proveedor.component.ts
@@ -200,52 +200,52 @@ export class ListadoDashboardProveedorComponent extends ListBaseComponent {
             )
     }
 
-  publicarCotizacion(Id: string) {
-    this.goToSeccionParam('/compras/dashboard-proveedor/cotizacion', Id);
-}
+    publicarCotizacion(Id: string) {
+        this.goToSeccionParam('/compras/dashboard-proveedor/cotizacion', Id);
+    }
 
-generarZipPliego(idSolp) {
-  this.blockUI.start('Generando ')
-  this.service.descargarZipPliego(idSolp)
-      .subscribe(
-          (result) => {
-              if (result.logout == true) {
-                  this.sessionDataService.logout();
-              }
-              else {
-                  var byteArray = new Uint8Array(result.FileContents);
-                  var blob = new Blob([byteArray], {
-                      type: "application/octet-stream",
-                  });
+    generarZipPliego(idSolp) {
+        this.blockUI.start('Generando ')
+        this.service.descargarZipPliego(idSolp)
+            .subscribe(
+                (result) => {
+                    if (result.logout == true) {
+                        this.sessionDataService.logout();
+                    }
+                    else {
+                        var byteArray = new Uint8Array(result.FileContents);
+                        var blob = new Blob([byteArray], {
+                            type: "application/octet-stream",
+                        });
 
-                  if (window.navigator.msSaveOrOpenBlob) {
-                      // IE11
-                      window.navigator.msSaveOrOpenBlob(
-                          blob,
-                          result.FileDownloadName
-                      );
-                  } else {
-                      var url = window.URL.createObjectURL(blob);
-                      var link = document.createElement("a");
-                      document.body.appendChild(link);
-                      link.href = url;
-                      link.download = result.FileDownloadName;
-                      link.click();
-                      setTimeout(function () {
-                          window.URL.revokeObjectURL(url);
-                      }, 0);
-                      this.blockUI.stop();
-                      return false;
-                  }
-                  this.blockUI.stop();
-              }
-          },
-          (error) => {
-              this.mensajeComponent.setErrorMsg(error.message);
-              this.blockUI.stop();
-          }
-      )
-}
+                        if (window.navigator.msSaveOrOpenBlob) {
+                            // IE11
+                            window.navigator.msSaveOrOpenBlob(
+                                blob,
+                                result.FileDownloadName
+                            );
+                        } else {
+                            var url = window.URL.createObjectURL(blob);
+                            var link = document.createElement("a");
+                            document.body.appendChild(link);
+                            link.href = url;
+                            link.download = result.FileDownloadName;
+                            link.click();
+                            setTimeout(function () {
+                                window.URL.revokeObjectURL(url);
+                            }, 0);
+                            this.blockUI.stop();
+                            return false;
+                        }
+                        this.blockUI.stop();
+                    }
+                },
+                (error) => {
+                    this.mensajeComponent.setErrorMsg(error.message);
+                    this.blockUI.stop();
+                }
+            )
+    }
     descargarArchivo({ archivoId }) {
         if (archivoId == 0) {
             let SolpId = this.legajo[0].SolpId;
@@ -341,99 +341,6 @@ generarZipPliego(idSolp) {
         }
     }
 
-    descargarArchivo({ archivoId }) {
-        if (archivoId == 0) {
-            let SolpId = this.legajo[0].SolpId;
-            this.blockUI.start("Generando...");
-            this.service.getPdf(SolpId)
-                .subscribe(
-                    (result) => {
-                        if (result.logout == true) {
-                            this.sessionDataService.logout();
-                        }
-                        else {
-                            var byteArray = new Uint8Array(result.FileContents);
-                            var blob = new Blob([byteArray], {
-                                type: "application/octet-stream",
-                            });
 
-                            this.downloadArchivoLocal(blob, result.FileDownloadName);
-                        }
-                        this.blockUI.stop();
-                    },
-                    (error) => {
-                        this.blockUI.stop();
-                        this.mensajeComponent.setErrorMsg(error.message);
-                    }
-                )
-        } else if (archivoId < 0) {
-            this.blockUI.start("Generando...");
-            this.service.getPdfPeticionDeOfertaUsuario(archivoId)
-                .subscribe(
-                    (result) => {
-                        if (result.logout == true) {
-                            this.sessionDataService.logout();
-                        }
-                        else {
-                            var byteArray = new Uint8Array(result.FileContents);
-                            var blob = new Blob([byteArray], {
-                                type: "application/octet-stream",
-                            });
-
-                            this.downloadArchivoLocal(blob, result.FileDownloadName);
-                        }
-                        this.blockUI.stop();
-                    },
-                    (error) => {
-                        this.blockUI.stop();
-                        this.mensajeComponent.setErrorMsg(error.message);
-                    }
-                )
-        }
-        else {
-            this.blockUI.start("Descargando...");
-            this.service.DescargarArchivo(archivoId)
-                .subscribe(
-                    (result) => {
-                        if (result.logout == true) {
-                            this.sessionDataService.logout();
-                        }
-                        else {
-                            var byteArray = new Uint8Array(result.FileContents);
-                            var blob = new Blob([byteArray], {
-                                type: "application/octet-stream",
-                            });
-                            this.downloadArchivoLocal(blob, result.FileDownloadName);
-                            this.blockUI.stop();
-                        }
-                    },
-                    (error) => {
-                        this.mensajeComponent.setErrorMsg(error.message);
-                        this.blockUI.stop();
-                    }
-                )
-        }
-    }
-
-    private downloadArchivoLocal(blob: Blob, nombreArchivo: string): void {
-        if (window.navigator.msSaveOrOpenBlob) {
-            // IE11
-            window.navigator.msSaveOrOpenBlob(
-                blob,
-                nombreArchivo
-            );
-        } else {
-            var url = window.URL.createObjectURL(blob);
-            var link = document.createElement("a");
-            document.body.appendChild(link);
-            link.href = url;
-            link.download = nombreArchivo;
-            link.click();
-            setTimeout(function () {
-                window.URL.revokeObjectURL(url);
-            }, 0);
-            return;
-        }
-    }
 
 }
