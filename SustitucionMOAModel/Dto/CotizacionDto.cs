3f75daf35a6d4eb292d27b5543de0905c41e61d0
Rocio Orlando <rorlando@baufest.com>
2023-04-20 10:14:53 -0300

    feature/(ARMOA018-63): - Ver ofertas

--- SustitucionMOAModel/Dto/CotizacionDto.cs
+++ SustitucionMOAModel/Dto/CotizacionDto.cs
@@ -65,6 +65,8 @@ namespace SustitucionMOAModel.Dto
         public DateTime? FechaOriginal { get; set; }
         public int CantidadPendiente { get; set; }
         public decimal TotalPesos { get; set; }
+        public decimal TotalGlobalSubPos { get; set; }
+        public decimal TotalPosicionCotizacion { get; set; }
     }
 
     public class CotizacionSubPosicionDto
