636e1335057d8c268201cca8d7637191dec0afe3
Rocio Orlando <rorlando@baufest.com>
2023-04-25 13:36:03 -0300

    ARMOA018-63:- Ver ofertas
    cosas repetidas

--- SustitucionMOAModel/Dto/CotizacionDto.cs
+++ SustitucionMOAModel/Dto/CotizacionDto.cs
@@ -63,8 +63,6 @@ namespace SustitucionMOAModel.Dto
         public DateTime? FechaOriginal { get; set; }
         public int CantidadPendiente { get; set; }
         public decimal TotalPesos { get; set; }
-        public decimal TotalGlobalSubPos { get; set; }
-        public decimal TotalPosicionCotizacion { get; set; }
     }
 
     public class CotizacionSubPosicionDto
--- SustitucionMOARepositorio/ConsultasEF/ComparadorOfertasConsulta.cs
+++ SustitucionMOARepositorio/ConsultasEF/ComparadorOfertasConsulta.cs
@@ -145,7 +145,6 @@ namespace SustitucionMOARepositorio.ConsultasEF
                                                                         TotalARPCotizacionPosicion = 0,
                                                                         TotalPosicionCotizacion = 0,
                                                                         TotalPesos = 0,
-                                                                        TotalPosicionCotizacion = 0,
                                                                         CotizacionSubPosiciones = (from subpos in contexto.Set<CotizacionSubPosicion>()
                                                                                                    where p.Id == subpos.CotizacionPosicion_Id
                                                                                                    select new CotizacionSubPosicionDto()
