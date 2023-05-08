655c6694476e1e4f128fb7e8525377664b18f189
Rocio Orlando <rorlando@baufest.com>
2023-05-08 12:38:49 -0300

    feature/(ARMOA018-91): Panel Horas - Revision Tecnica
    Componente panel de hs nuevo, tabla, icono nuevo en revision tecnica, agregado de cotizacion hs en el service de ObtenerPeticionDeOfertaParaCircular

--- SustitucionMOA/src/app/compras/dashboard-proveedor/cotizacion-formulario/cotizacion-servicio/cotizacion-servicio.component.ts
+++ SustitucionMOA/src/app/compras/dashboard-proveedor/cotizacion-formulario/cotizacion-servicio/cotizacion-servicio.component.ts
@@ -331,7 +331,7 @@ export class CotizacionServicioComponent extends ListBaseComponent implements On
             CotizacionesHoras: this.index == 0 ? this.peticion.Cotizacion.CotizacionesHoras.filter(x => x.Gremio == 'UOCRA') : this.peticion.Cotizacion.CotizacionesHoras.filter(x => x.Gremio != 'UOCRA'),
             CotizacionSubposiciones: this.subposiciones
         }
-        console.log(coti, "cotizacion")
+        // console.log(coti, "cotizacion")
         return coti;
         
     }
