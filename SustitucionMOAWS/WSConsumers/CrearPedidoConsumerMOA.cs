eedc04c6e1d158443d36e5a7f1935b9671a48465
eugenio martin <emartin@baufest.com>
2023-05-08 12:52:06 -0300

    fix precio material OC

--- SustitucionMOAWS/WSConsumers/CrearPedidoConsumerMOA.cs
+++ SustitucionMOAWS/WSConsumers/CrearPedidoConsumerMOA.cs
@@ -195,7 +195,7 @@ namespace SustitucionMOAWS.WSConsumers
                 IM_POITEM.QUANTITY = esPosicionDeMateriales ? adjudicacionPosicion.Cantidad : 0;
                 IM_POITEM.QUANTITYSpecified = esPosicionDeMateriales ? true : false;
                 IM_POITEM.PO_UNIT = esPosicionDeMateriales ? posicion.Unidad.Descripcion : "001";
-                IM_POITEM.NET_PRICE = esPosicionDeMateriales ? (decimal)posicion.PrecioBruto : CalcularPrecioBrutoServicio(posicion, adjudicacionPosicion);
+                IM_POITEM.NET_PRICE = esPosicionDeMateriales ? (decimal)adjudicacionPosicion.CotizacionPosicion.Precio : CalcularPrecioBrutoServicio(posicion, adjudicacionPosicion);
                 IM_POITEM.NET_PRICESpecified = true;
                 IM_POITEM.PRICE_UNIT = 1;
                 //IM_POITEM.PRICE_UNITSpecified = true;
