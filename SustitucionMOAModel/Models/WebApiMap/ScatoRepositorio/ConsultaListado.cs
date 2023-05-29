4a329d3f479fef8f3663e9d5162cd7038197f7cd
Ali Rigol <mrigol@baufest.com>
2023-05-29 17:59:23 -0300

    feat/ARMOA003-2195: usar scato web api en vez del schema provisto por scato

--- SustitucionMOAModel/Models/WebApiMap/ScatoRepositorio/ConsultaListado.cs
+++ SustitucionMOAModel/Models/WebApiMap/ScatoRepositorio/ConsultaListado.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SustitucionMOAModel.Models.WebApiMap.ScatoRepositorio
+﻿namespace SustitucionMOAModel.Models.WebApiMap.ScatoRepositorio
 {
     public class ConsultaListado<T>
     {
