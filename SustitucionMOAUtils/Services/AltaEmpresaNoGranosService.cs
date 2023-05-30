fd718e3b3f406f4189ea637bd8da79122a797b31
Evangelino Miranda <emiranda@baufest.com>
2023-05-30 16:20:25 -0500

    Cuando se registra una alta interna se le asigna el estado Alta Solicitada

--- SustitucionMOAUtils/Services/AltaEmpresaNoGranosService.cs
+++ SustitucionMOAUtils/Services/AltaEmpresaNoGranosService.cs
@@ -66,7 +66,7 @@ namespace SustitucionMOAUtils.Services
             proveedor.Mail = email;
             proveedor.Telefono = telefono;
             proveedor.RealizarAnalisisNOSIS = realizarAnalisisNOSIS;
-            proveedor.EstadoAprobacion = EstadoAprobacion.DocumentacionPendiente;
+            proveedor.EstadoAprobacion = altaInterna? EstadoAprobacion.AprobacionPendiente : EstadoAprobacion.DocumentacionPendiente;
             proveedor.IdRubro = IdRubro;
             proveedor.CondicionDePago = CondicionDePago;
             proveedor.ServicioPrestado = ServicioPrestado;
@@ -92,7 +92,7 @@ namespace SustitucionMOAUtils.Services
                     new ProveedorHistorialAprobacion
                     {
                         Fecha = DateTime.Now,
-                        EstadoAprobacion = EstadoAprobacion.DocumentacionPendiente,
+                        EstadoAprobacion = proveedor.EstadoAprobacion,
                         Observacion = observacionInterna,
                         Usuario_Id = usuarioId
                     }
