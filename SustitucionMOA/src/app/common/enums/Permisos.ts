6c400819e6e58413baeb330d17537a711912571b
Ali Rigol <mrigol@baufest.com>
2023-06-07 15:10:29 -0300

    feat/ARMOA003-2220: agregado permiso para habilitar campo reventa

---
+++ SustitucionMOA/src/app/common/enums/Permisos.ts
@@ -0,0 +1,5 @@
+export enum Permiso {
+    FasModificarCampoReventa = "FAS - MODIFICAR CAMPO REVENTA",
+    FasVerOrdenesComerciales = "VER ORDENES DE CARGA PARA COMERCIALES",
+    EnviarSAP = "ENVIAR A SAP",
+}
