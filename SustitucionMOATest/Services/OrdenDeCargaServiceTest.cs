7b7af4926d33a09c0f922fbbbdb7d87d351ff7fb
Ali Rigol <mrigol@baufest.com>
2023-06-07 15:10:29 -0300

    feat/ARMOA003-2220: agregado permiso para habilitar campo reventa

--- SustitucionMOATest/Services/OrdenDeCargaServiceTest.cs
+++ SustitucionMOATest/Services/OrdenDeCargaServiceTest.cs
@@ -22,6 +22,7 @@ using SustitucionMOAWS.ResponseHandler.OrdenCarga;
 using SustitucionMOAWS.Enum.OrdenCargaConsumer;
 using SustitucionMOAModel.Util;
 using SustitucionMOAModel.Models.WSMapMOA.OrdenCarga;
+using SustitucionMOAModel.Models.WSMapMOA.Pesificacion;
 
 namespace SustitucionMOATest.Services
 {
@@ -39,6 +40,15 @@ namespace SustitucionMOATest.Services
 
         private ScatoRepo.Respuesta<ScatoRepo.Chofer> _respuestaChofer;
         private ScatoRepo.Respuesta<ScatoRepo.Chofer> _respuestaTransporte;
+
+        private Proveedor _proveedorUsuario;
+        private Usuario _usuario;
+        private string _mailSesionUsuario;
+        private List<Rol> _rolesUsuario;
+
+        private Rol _rolAdministracion;
+        private PermisoPorRol _permisoVerOrdenesComerciales;
+
 
         private Proveedor _proveedorUsuario;
         private Usuario _usuario;
@@ -140,12 +150,6 @@ namespace SustitucionMOATest.Services
                 },
 
                 Roles = _rolesUsuario
-            };
-            _respuestaTransporte = new ScatoRepo.Respuesta<ScatoRepo.Chofer>
-            {
-                Data = new ScatoRepo.Chofer { },
-                Messages = new ScatoRepo.MessageItem[] { },
-                IsValid = false
             };
         }
 
@@ -1393,9 +1397,6 @@ namespace SustitucionMOATest.Services
                 );
             var result = target.ValidarCuitTransporteDigito("11111111111");
 
-            Assert.That(result, Is.False);
-        }
-
         [Test]
         public void Agregar_UsuarioNoPuedeModificarReventa_ThrowValidationCustomException()
         {
@@ -1422,40 +1423,6 @@ namespace SustitucionMOATest.Services
             var result = target.Agregar(ordenDeCarga, _mailSesionUsuario);
 
             Assert.That(result.Mensaje, Is.EqualTo(SuccessMsg.OrdenDeCargaAgregada));
-
-        }
-        [Test]
-        public void ValidarCuitTransporteDigito_CuitTransporteNoExiste_ReturnsTrue()
-        {
-            _respuestaTransporte.Messages = new ScatoRepo.MessageItem[]
-            {
-                new ScatoRepo.MessageItem
-                {
-                    MessageCode = ScatoRepo.CodigoMensajeObtenerChoferPorCuil.ChoferNoEncontrado
-                }
-            };
-            mIScatoRepositorioClient.Setup(src => src.ObtenerTransportePorCuit(It.IsAny<string>())).Returns(
-                _respuestaTransporte
-                );
-
-            var result = target.ValidarCuitTransporteDigito("11111111111");
-
-            Assert.That(result, Is.True);
-
-        }
-        [Test]
-        public void ValidarCuitTransporteDigito_CuitTransporteExiste_ReturnsTrue()
-        {
-            _respuestaTransporte.IsValid = true;
-
-            mIScatoRepositorioClient.Setup(src => src.ObtenerTransportePorCuit(It.IsAny<string>())).Returns(
-                _respuestaTransporte
-                );
-
-            var result = target.ValidarCuitTransporteDigito("11111111111");
-
-            Assert.That(result, Is.True);
-
         }
         private void AddProvider(int id, EstadoAprobacion estadoAprobacion, string observaciones, string razonSocial, string mail, string cUIT, TipoUsuario tipoProveedor)
         {
