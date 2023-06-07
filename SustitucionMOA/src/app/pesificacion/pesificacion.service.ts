399408ab351435ff8336c9a05bc81aa573094961
Evangelino Miranda <emiranda@baufest.com>
2023-06-07 17:15:13 -0500

    Se aumenta el timeout para las consultas a SAP y se incluye el loading spinner para mostrar el progreso de la consulta

--- SustitucionMOA/src/app/pesificacion/pesificacion.service.ts
+++ SustitucionMOA/src/app/pesificacion/pesificacion.service.ts
@@ -28,18 +28,18 @@ export class PesificacionService extends BaseService {
     protected getFechaPesificacion() {
         return this.http
             .get('/api/pesificacion/getFechaPesificacion')
-            .pipe(timeoutWith(30000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
+            .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
     }
 
     protected getSoja200() {
         return this.http
             .get('/api/pesificacion/getSoja200')
-            .pipe(timeoutWith(30000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
+            .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
     }
     public getDolarGirasol(): Observable<ApiResponse<DolarGirasol>> {
         return this.http
             .get<ApiResponse<DolarGirasol>>('/api/pesificacion/getDolarGirasol')
-            .pipe(timeoutWith(30000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
+            .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
     }
     protected setComprobantePesificacion(contrato: string, fijacion: string, cantidad: number) {
         let payload = new FormData();
@@ -47,7 +47,7 @@ export class PesificacionService extends BaseService {
         payload.append("contrato", JSON.stringify(data));
         return this.http
             .post('/api/pesificacion/setComprobante', payload)
-            .pipe(timeoutWith(30000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
+            .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
     }
 
     protected setComprobantesPesificacion(file: any): Observable<any> {
@@ -55,19 +55,18 @@ export class PesificacionService extends BaseService {
         payload.append("file", file);
         return this.http
             .post('/api/pesificacion/setComprobantes', payload, { headers: this.headersPost })
-            .pipe(timeoutWith(30000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
+            .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
     }
 
     public getContratos(): Observable<any> {
         return this.http
             .get('/api/pesificacion/GetContratos')
-            .pipe(timeoutWith(30000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
+            .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
     }
 
     public getPesificacionesSap(): Observable<any> {
         return this.http
             .get('/api/pesificacion/PesificacionesSap')
-            .pipe(timeoutWith(30000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))))
-        // .pipe(map(this.extractData));
+            .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde "))));
     }
 }
