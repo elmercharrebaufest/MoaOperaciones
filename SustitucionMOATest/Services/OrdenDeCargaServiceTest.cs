898e799e213c34116fe6bd6258a0542f997e8145
Ali Rigol <mrigol@baufest.com>
2023-06-07 15:10:29 -0300

    feat/ARMOA003-2220: agregado permiso para habilitar campo reventa

--- SustitucionMOATest/Services/OrdenDeCargaServiceTest.cs
+++ SustitucionMOATest/Services/OrdenDeCargaServiceTest.cs
@@ -77,6 +77,15 @@ namespace SustitucionMOATest.Services
         private PermisoPorRol _permisoVerOrdenesComerciales;
 
 
+        private Proveedor _proveedorUsuario;
+        private Usuario _usuario;
+        private string _mailSesionUsuario;
+        private List<Rol> _rolesUsuario;
+
+        private Rol _rolAdministracion;
+        private PermisoPorRol _permisoVerOrdenesComerciales;
+
+
         [SetUp]
         public void SetUp()
         {
