a887bcb27be53acaa1df5cdab8f630bc6a01a5f7
Ali Rigol <mrigol@baufest.com>
2023-06-12 09:54:00 -0300

    feat/ARMOA003-2240: agregar validaciones de digito para cuit transporte

--- SustitucionMOATest/Services/OrdenDeCargaServiceTest.cs
+++ SustitucionMOATest/Services/OrdenDeCargaServiceTest.cs
@@ -50,42 +50,6 @@ namespace SustitucionMOATest.Services
         private PermisoPorRol _permisoVerOrdenesComerciales;
 
 
-        private Proveedor _proveedorUsuario;
-        private Usuario _usuario;
-        private string _mailSesionUsuario;
-        private List<Rol> _rolesUsuario;
-
-        private Rol _rolAdministracion;
-        private PermisoPorRol _permisoVerOrdenesComerciales;
-
-
-        private Proveedor _proveedorUsuario;
-        private Usuario _usuario;
-        private string _mailSesionUsuario;
-        private List<Rol> _rolesUsuario;
-
-        private Rol _rolAdministracion;
-        private PermisoPorRol _permisoVerOrdenesComerciales;
-
-
-        private Proveedor _proveedorUsuario;
-        private Usuario _usuario;
-        private string _mailSesionUsuario;
-        private List<Rol> _rolesUsuario;
-
-        private Rol _rolAdministracion;
-        private PermisoPorRol _permisoVerOrdenesComerciales;
-
-
-        private Proveedor _proveedorUsuario;
-        private Usuario _usuario;
-        private string _mailSesionUsuario;
-        private List<Rol> _rolesUsuario;
-
-        private Rol _rolAdministracion;
-        private PermisoPorRol _permisoVerOrdenesComerciales;
-
-
         [SetUp]
         public void SetUp()
         {
@@ -177,6 +141,12 @@ namespace SustitucionMOATest.Services
                 },
 
                 Roles = _rolesUsuario
+            };
+            _respuestaTransporte = new ScatoRepo.Respuesta<ScatoRepo.Chofer>
+            {
+                Data = new ScatoRepo.Chofer { },
+                Messages = new ScatoRepo.MessageItem[] { },
+                IsValid = false
             };
         }
 
@@ -1424,6 +1394,9 @@ namespace SustitucionMOATest.Services
                 );
             var result = target.ValidarCuitTransporteDigito("11111111111");
 
+            Assert.That(result, Is.False);
+        }
+
         [Test]
         public void Agregar_UsuarioNoPuedeModificarReventa_ThrowValidationCustomException()
         {
@@ -1450,6 +1423,40 @@ namespace SustitucionMOATest.Services
             var result = target.Agregar(ordenDeCarga, _mailSesionUsuario);
 
             Assert.That(result.Mensaje, Is.EqualTo(SuccessMsg.OrdenDeCargaAgregada));
+
+        }
+        [Test]
+        public void ValidarCuitTransporteDigito_CuitTransporteNoExiste_ReturnsTrue()
+        {
+            _respuestaTransporte.Messages = new ScatoRepo.MessageItem[]
+            {
+                new ScatoRepo.MessageItem
+                {
+                    MessageCode = ScatoRepo.CodigoMensajeObtenerChoferPorCuil.ChoferNoEncontrado
+                }
+            };
+            mIScatoRepositorioClient.Setup(src => src.ObtenerTransportePorCuit(It.IsAny<string>())).Returns(
+                _respuestaTransporte
+                );
+
+            var result = target.ValidarCuitTransporteDigito("11111111111");
+
+            Assert.That(result, Is.True);
+
+        }
+        [Test]
+        public void ValidarCuitTransporteDigito_CuitTransporteExiste_ReturnsTrue()
+        {
+            _respuestaTransporte.IsValid = true;
+
+            mIScatoRepositorioClient.Setup(src => src.ObtenerTransportePorCuit(It.IsAny<string>())).Returns(
+                _respuestaTransporte
+                );
+
+            var result = target.ValidarCuitTransporteDigito("11111111111");
+
+            Assert.That(result, Is.True);
+
         }
         private void AddProvider(int id, EstadoAprobacion estadoAprobacion, string observaciones, string razonSocial, string mail, string cUIT, TipoUsuario tipoProveedor)
         {
