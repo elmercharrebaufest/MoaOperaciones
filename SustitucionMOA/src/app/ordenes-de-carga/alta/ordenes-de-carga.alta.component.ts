532ca712e3e079a9e1847d528e79743903652bdb
Ali Rigol <mrigol@baufest.com>
2023-06-12 14:00:42 -0300

    feat/ARMOA003-2240: bloquear envio sin producto

--- SustitucionMOA/src/app/ordenes-de-carga/alta/ordenes-de-carga.alta.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/alta/ordenes-de-carga.alta.component.ts
@@ -221,6 +221,11 @@ export class OrdenesDeCargaAlta extends BaseComponent implements OnInit {
             }
         }
 
+        if (!this.ordenDeCarga.Producto_Id) {
+            this.mensajeComponent.setInfoMsg("Seleccione un contrato.");
+            return false;
+        }
+
         return true;
     }
 
