23e3a17e961f08a182c79dfb84d4131d1a863537
Ali Rigol <mrigol@baufest.com>
2023-06-13 15:43:55 -0300

    feat/ARMOA003-2226: agregar campo numero factura

--- SustitucionMOA/src/app/common/models/ordenes-de-carga/obtenerContratosDisponiblesResponse.ts
+++ SustitucionMOA/src/app/common/models/ordenes-de-carga/obtenerContratosDisponiblesResponse.ts
@@ -1,15 +1,19 @@
 import { Material } from "../material";
 
 export interface ObtenerContratosDisponiblesResponse {
-    
     Contratos: ContratoOrdenFas[];
-    
     Info: string;
     Error: string;
     Logout: boolean;
 }
 
+export enum TipoContrato {
+    Normal = "NORMAL",
+    FacturaAnticipada = "ANTICIPADO",
+}
+
 export interface ContratoOrdenFas {
     NumeroContrato: string;
-    Producto: Material
+    Producto: Material;
+    TipoContrato: TipoContrato;
 }
