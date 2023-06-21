0e68b79dbb1e6240beb0ac54597d4fa32f58e60c
Ali Rigol <mrigol@baufest.com>
2023-06-21 15:10:38 -0300

    feat/ARMOA003-2226: arreglar endpoint para obtener las facturas disponibles

--- SustitucionMOAUtils/Services/OrdenDeCargaService.cs
+++ SustitucionMOAUtils/Services/OrdenDeCargaService.cs
@@ -76,7 +76,7 @@ namespace SustitucionMOAUtils.Services
                 LlenarOrdenAlta(ordenDeCarga, usuario);
 
                 var puedeEnviarASAP = usuario.TienePermiso(PermisoEnum.EnviarASap);
-                
+
                 var crearPedido = VerificarOrden(ordenDeCarga, ordenDeCarga.Cliente, false, puedeEnviarASAP);
                 Log.Debug(this.GetType().Name, "Agregar", $" crearPedido: {crearPedido}");
                 repositorio.Agregar(ordenDeCarga);
