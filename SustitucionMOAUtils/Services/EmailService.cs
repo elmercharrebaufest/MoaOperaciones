02bce5d4e352d85d80fb1e7e6786e83a55facc32
Diego Hidalgo <dhidalgo@baufest.com>
2023-06-21 22:28:29 -0300

    ARMOA003-2243 Enviar mail al seleccionar un contrato (cuando hay varios) y está vencido. Se agrega prefijo por ambiente en el asunto.

---
+++ SustitucionMOAUtils/Services/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using SustitucionMOAUtils.Email;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace SustitucionMOAUtils.Services
+{
+    public abstract class EmailService
+    {
+        protected string GenerarAsunto(string asunto)
+        {
+            var prefijoAsunto = ConfigurationManager.AppSettings["EmailAsuntoPrefijo"];
+            return prefijoAsunto + asunto;
+        }
+
+        protected List<string> ObtenerListaDestinatarios(IEnumerable<string> gruposDeMails)
+        {
+            var destinatarios = new List<string>();
+            foreach (var grupo in gruposDeMails.Where(x => !string.IsNullOrEmpty(x)))
+            {
+                destinatarios.AddRange(grupo.Split(';'));
+            }
+            return destinatarios.Distinct().ToList();
+        }
+
+        // Este método es para poder mockear el envío de mail al testear
+        protected virtual void EnviarMail(EmailSenderData emailSenderData)
+        {
+            EmailSender.EnviarMail(emailSenderData);
+        }
+    }
+}
