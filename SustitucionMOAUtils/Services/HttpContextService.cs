ac65db9be4986f183f234db96306817a62a4cfdd
eugenio martin <emartin@baufest.com>
2023-06-26 16:14:25 -0300

    fix ninject compras

--- SustitucionMOAUtils/Services/HttpContextService.cs
+++ SustitucionMOAUtils/Services/HttpContextService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SustitucionMOAUtils.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SustitucionMOAUtils.Services
 {
-    public class HttpContextService
+    public class HttpContextService : IHttpContextService
     {
         public HttpContextService()
         {
