391226f223c50c228d9b8484f9666f9270485074
Ali Rigol <mrigol@baufest.com>
2023-06-28 19:10:14 -0300

    feat/ARMOA003-2229: fix

--- SustitucionMOAModel/Util/TipoContratoFAS.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SustitucionMOAModel.Util
-{
-    public struct TipoContratoFAS
-    {
-        public const string NORMAL = "NORMAL";
-        public const string ANTICIPADO = "ANTICIPADO";
-    }
-}
