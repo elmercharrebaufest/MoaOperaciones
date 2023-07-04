f5a185627671023cc421fe888d3267effdecb1c2
Ali Rigol <mrigol@baufest.com>
2023-07-04 12:22:18 -0300

    fix/ARMOA003-2242: agregar el cuit del cliente al objeto ordenDeCarga

--- SustitucionMOA/src/app/ordenes-de-carga/alta/ordenes-de-carga.alta.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/alta/ordenes-de-carga.alta.component.ts
@@ -131,6 +131,7 @@ export class OrdenesDeCargaAlta extends BaseComponent implements OnInit {
         }
         if (this.esCliente()) {
             this.clienteCodigo = sessionStorage.getItem("proveedor");
+            this.ordenDeCarga.CUITCliente = Number.parseInt(this.clienteCodigo);
             if (this.ordenDeCargaId == 0 && !(this.esComercial || this.esCorredor)) {
                 this.cargarContratosDisponibles(this.clienteCodigo);
             }
