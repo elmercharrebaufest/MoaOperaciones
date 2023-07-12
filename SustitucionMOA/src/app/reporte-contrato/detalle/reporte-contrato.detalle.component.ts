bc61313ac569a244b3f60a217269a3d40d1d8bff
Evangelino Miranda <emiranda@baufest.com>
2023-07-12 09:01:05 -0500

    Se retira el codigo para imprimir en consola

--- SustitucionMOA/src/app/reporte-contrato/detalle/reporte-contrato.detalle.component.ts
+++ SustitucionMOA/src/app/reporte-contrato/detalle/reporte-contrato.detalle.component.ts
@@ -62,7 +62,6 @@ export class DetalleComponent extends ListBaseComponent implements OnInit {
                         this.mensajeComponent.setInfoMsg(result.info);
                     } else {
                         this.detalles = result.data;
-                        console.log('this.detalles--->>', this.detalles);
                         this.obtenerKilos();
                     }
                 },
