19101b7d7d0af6558c73777f86cdf6d1868b17f8
Ali Rigol <mrigol@baufest.com>
2023-07-27 19:37:01 -0300

    feat/ARMOA003-2334: agregar creación de pedido en SAP al usar boton Verificar Transporte - limpieza de métodos deprecados

--- SustitucionMOA/Controllers/OrdenDeCargaController.cs
+++ SustitucionMOA/Controllers/OrdenDeCargaController.cs
@@ -314,53 +314,6 @@ namespace SustitucionMOA.Controllers
         }
 
         [HttpGet]
-        public ActionResult ObtenerPedidos(int ordenId)
-        {
-            try
-            {
-                return JsonCustom(new { data = ordenDeCargaService.ObtenerPedidos(ordenId) });
-            }
-            catch (InfoCustomException e)
-            {
-                return Json(new { info = e.Message }, JsonRequestBehavior.AllowGet);
-            }
-            catch (ValidationCustomException e)
-            {
-                return Json(new { error = e.Message }, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception e)
-            {
-                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
-                return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult SeleccionarPedido(int ordenId, string pedido)
-        {
-            try
-            {
-                var mailUsuario = SessionPersister.getUsername();
-
-                return JsonCustom(new { data = ordenDeCargaService.SeleccionarPedido(ordenId, pedido, mailUsuario) });
-            }
-            catch (InfoCustomException e)
-            {
-                return Json(new { info = e.Message }, JsonRequestBehavior.AllowGet);
-            }
-            catch (ValidationCustomException e)
-            {
-                return Json(new { error = e.Message }, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception e)
-            {
-                Log.Error(System.Web.HttpContext.Current.Request.UserHostAddress, SessionPersister.getUsername(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name, e);
-                return Json(new { error = ErrorMsg.Error }, JsonRequestBehavior.AllowGet);
-            }
-        }
-
-
-        [HttpGet]
         public ActionResult VerificarSituacionCrediticia(int ordenId)
         {
             try
@@ -391,7 +344,7 @@ namespace SustitucionMOA.Controllers
             {
                 var mailUsuario = SessionPersister.getUsername();
 
-                return JsonCustom(new { data = ordenDeCargaService.VerificarTransporte(ordenId) });
+                return JsonCustom(new { data = ordenDeCargaService.VerificarTransporte(ordenId, mailUsuario) });
             }
             catch (InfoCustomException e)
             {
