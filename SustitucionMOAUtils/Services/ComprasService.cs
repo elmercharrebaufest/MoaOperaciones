ab7fe2f639a37456ae0027698b53b83d771cd2ae
Rocio Orlando <rorlando@baufest.com>
2023-08-02 16:30:22 -0300

    ARMOA018-384B
    Error al eliminar una subposicion que tiene una cotizacion

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -819,6 +819,13 @@ namespace SustitucionMOAUtils.Services
 
                         foreach (var subpos in subposEliminadas.ToList())
                         {
+                            if (subpos.Cotizaciones.Any()) 
+                            {
+                                foreach (var cotizacion in subpos.Cotizaciones.ToList())
+                                {
+                                    repositorio.Remover(cotizacion);
+                                }
+                            }
                             repositorio.Remover(subpos);
                         }
                     }
@@ -2153,7 +2160,18 @@ namespace SustitucionMOAUtils.Services
                 if (subPosicionesBorradas.Count() > 0)
                 {
                     var subposborradas = repositorio.Listar<SolpSubposicion>(x => subPosicionesBorradas.Contains(x.Id));
-                    repositorio.RemoverTodos<SolpSubposicion>(subposborradas);
+
+                    foreach (var subpos in subposborradas.ToList())
+                    {
+                        if (subpos.Cotizaciones.Any())
+                        {
+                            foreach (var cotizacion in subpos.Cotizaciones.ToList())
+                            {
+                                repositorio.Remover(cotizacion);
+                            }
+                        }
+                        repositorio.Remover(subpos);
+                    }
                 }
                 repositorio.GuardarCambios();
                 Logger.Log.Info($"ObtenerSolpesDesdeSAPJob fin");
