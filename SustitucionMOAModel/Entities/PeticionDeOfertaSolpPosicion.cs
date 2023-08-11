aa28260818a69c5a48fd8632ef1285e9f4a94c57
Dani Rodriguez Palacio <drodriguez@baufest.com>
2023-08-11 12:25:23 -0300

    ARMOA018-498 - Mejoras al crear OC desde registro info
    Se agrega el número de orden y de registro info a partir de los cuales se genera la nueva OC.

--- SustitucionMOAModel/Entities/PeticionDeOfertaSolpPosicion.cs
+++ SustitucionMOAModel/Entities/PeticionDeOfertaSolpPosicion.cs
@@ -1,9 +1,4 @@
-﻿//using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SustitucionMOAModel.Entities
@@ -14,12 +9,12 @@ namespace SustitucionMOAModel.Entities
         public int Id { get; set; }
         public int PeticionDeOferta_Id { get; set; }
         public int SolpPosicion_Id { get; set; }    
+        public string RegistroInfo_Num { get; set; }    
 
         [ForeignKey("PeticionDeOferta_Id")]
         public virtual PeticionDeOferta PeticionDeOferta { get; set; }
 
         [ForeignKey("SolpPosicion_Id")]
         public virtual SolpPosicion SolpPosicion { get; set; }
-
     }
 }
