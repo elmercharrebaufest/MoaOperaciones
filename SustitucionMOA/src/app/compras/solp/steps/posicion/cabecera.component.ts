e6cc40f40706a29ca306872a32ffe6e69853f6bc
eugenio martin <emartin@baufest.com>
2023-08-17 10:29:44 -0300

    ARMOA018-535

--- SustitucionMOA/src/app/compras/solp/steps/posicion/cabecera.component.ts
+++ SustitucionMOA/src/app/compras/solp/steps/posicion/cabecera.component.ts
@@ -917,6 +917,7 @@ export class CabeceraComponent extends ListBaseComponent implements OnDestroy {
     //Funciones de la tabla
     onSelectServicio(posicion: SolpPosicion, dt) {
         posicion.tareaSubcontratar = posicion.codigoServicio.Descripcion;
+        posicion.textoSuministro = posicion.codigoServicio.Descripcion;
         posicion.tareaSubcontratarObj = { ...posicion.codigoServicio };
         this.autocompletarCamposMaterial(posicion);
 
@@ -927,6 +928,7 @@ export class CabeceraComponent extends ListBaseComponent implements OnDestroy {
 
     onSelectTarea(posicion: SolpPosicion, dt) {
         posicion.tareaSubcontratar = posicion.tareaSubcontratarObj.Descripcion;
+        posicion.textoSuministro = posicion.tareaSubcontratarObj.Descripcion;
         posicion.codigoServicio = { ...posicion.tareaSubcontratarObj };
         
         this.autocompletarCamposMaterial(posicion);
