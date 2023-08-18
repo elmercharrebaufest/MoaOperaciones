46810f96829719396842abced14d60e04490f050
eugenio martin <emartin@baufest.com>
2023-08-18 09:32:26 -0300

    fix listado fas

--- SustitucionMOAModel/Entities/OrdenDeCarga.cs
+++ SustitucionMOAModel/Entities/OrdenDeCarga.cs
@@ -80,7 +80,7 @@ namespace SustitucionMOAModel.Entities
         public string DescripcionCodigoVerificacionSap { get; set; }
         public bool Reventa { get; set; }
 
-        public int? UsuarioCreacion_Id { get; set; }
+        public int UsuarioCreacion_Id { get; set; }
         [ForeignKey("UsuarioCreacion_Id")]
         public virtual Usuario UsuarioCreacion { get; set; }
 
