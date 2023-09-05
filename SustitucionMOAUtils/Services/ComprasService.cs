0bc48fcb9ef158fe32e18026fc54d9ec459f0e31
Dani Rodriguez Palacio <drodriguez@baufest.com>
2023-09-05 11:47:57 -0300

    ARMOA018-585-586 - Correcciones adicional con trabajo ya hecho
    Se corrige la generación de dos PO en lugar de una y el envío incorrecto del mail de la PO.

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -301,7 +301,7 @@ namespace SustitucionMOAUtils.Services
                         CrearCotizacionConTrabajoYaHecho(solpEntity);
                     }
 
-                    if (respuestaGuardarSOLP.Mensaje == "OK" && finalizoPrimeraVez && solp.Adicional == true)
+                    if (respuestaGuardarSOLP.Mensaje == "OK" && finalizoPrimeraVez && solp.TrabajoYaHecho != true && solp.Adicional == true)
                     {
                         CrearPeticionAutomatica(solpEntity, new List<int> { solp.ProveedorAsignadoId.Value }, null, false);
                     }
@@ -5271,7 +5271,7 @@ namespace SustitucionMOAUtils.Services
                 Adjuntos = null,
                 RegistroInfo = esRegistroInfo
             };
-            var resultado = GrabarPeticionDeOferta(peticion, null, solp.Adicional == true, registroInfoLista);
+            var resultado = GrabarPeticionDeOferta(peticion, null, solp.TrabajoYaHecho != true && solp.Adicional == true, registroInfoLista);
             var peticionEntidad = repositorio.Obtener<PeticionDeOferta>(resultado.IdEntidad);
 
             return peticionEntidad;
