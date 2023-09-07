1f8bddf805f64e39135ec84d28e840f11805e75b
Ali Rigol <mrigol@baufest.com>
2023-09-07 14:09:39 -0300

    hotfix/ARMOA003-2413: reducir costo del log de orden a la hora de verificar compensacion

--- SustitucionMOAUtils/Services/OrdenDeCargaService.cs
+++ SustitucionMOAUtils/Services/OrdenDeCargaService.cs
@@ -2559,7 +2559,7 @@ namespace SustitucionMOAUtils.Services
             if (orden == null)
                 throw new InfoCustomException("No se encontró la orden");
 
-            Log.Info($"Verificar Compensacion: orden: {orden.ToJson()}");
+            Log.Info($"Verificar Compensacion: orden: {orden.ToDto().ToJson()}");
             if (orden.Estado != EstadoOrdenDeCarga.PendienteCompensacion)
                 return;
 
