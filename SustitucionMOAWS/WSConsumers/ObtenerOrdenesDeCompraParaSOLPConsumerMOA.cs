49c7f500eded1241e8d5fd655ecfb6440f038c25
Brisa Melgarejo <bmelgarejo@baufest.com>
2023-09-12 12:45:59 -0300

    ARMOA018-570
    Z_MMRFC_OC_PARA_SOLP

--- SustitucionMOAWS/WSConsumers/ObtenerOrdenesDeCompraParaSOLPConsumerMOA.cs
+++ SustitucionMOAWS/WSConsumers/ObtenerOrdenesDeCompraParaSOLPConsumerMOA.cs
@@ -20,7 +20,8 @@ namespace SustitucionMOAWS.WSConsumers
 
         public List<OrdenDeCompraSAPDto> Request(string SOLPED, string POSICION)
         {
-            service = new Z_MMRFC_OC_PARA_SOLPPortTypeClient();
+            var url = "http://gslopidevqa00.molinosagro.ad:50000/XISOAPAdapter/MessageServlet?senderParty=&amp;senderService=BC_MOA_Operaciones&amp;receiverParty=&amp;receiverService=&amp;interface=Z_MMRFC_OC_PARA_SOLP&amp;interfaceNamespace=urn%3Asap-com%3Adocument%3Asap%3Arfc%3Afunctions";
+            service = new Z_MMRFC_OC_PARA_SOLPPortTypeClient(SAPCredential.CrearSapBasicBinding(), SAPCredential.DevolverEndpoint(url));
             service.ClientCredentials.UserName.UserName = SAPCredential.getUserName();
             service.ClientCredentials.UserName.Password = SAPCredential.getPassword();
             ZMMES0027[] lista = service.Z_MMRFC_OC_PARA_SOLP(POSICION, SOLPED, out string exito, out string resultado);
