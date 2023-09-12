e119e3690bd310ecf1de870d395e3e6a238c5294
Brisa Melgarejo <bmelgarejo@baufest.com>
2023-09-12 11:12:14 -0300

    ARMOA018-570
    SI_MMRFC_OBTENER_SERVICIOS

--- SustitucionMOAWS/WSConsumers/ObtenerServiciosSolpConsumerMOA.cs
+++ SustitucionMOAWS/WSConsumers/ObtenerServiciosSolpConsumerMOA.cs
@@ -17,7 +17,9 @@ namespace SustitucionMOAWS.WSConsumers
 
         public ObtenerServiciosSolpConsumerMOA()
         {
-            service = new SI_MMRFC_OBTENER_SERVICIOSClient();
+            var url = "http://gslopidevqa00.molinosagro.ad:50000/XISOAPAdapter/MessageServlet?senderParty=&amp;senderService=BC_MOA_Operaciones&amp;receiverParty=&amp;receiverService=&amp;interface=SI_MMRFC_OBTENER_SERVICIOS&amp;interfaceNamespace=urn%3AOPERACIONES";
+
+            service = new SI_MMRFC_OBTENER_SERVICIOSClient(SAPCredential.CrearSapBasicBinding(), SAPCredential.DevolverEndpoint(url));
             service.ClientCredentials.UserName.UserName = SAPCredential.getUserName();
             service.ClientCredentials.UserName.Password = SAPCredential.getPassword();
         }
