d36fab5c56379325138f5640997225ff1d140de5
Ali Rigol <mrigol@baufest.com>
2023-09-20 13:51:30 -0300

    fix/ARMOA003-2440: agregada condición y evitar una carrera entre las peticiones para obtener clientes

--- SustitucionMOA/src/app/ordenes-de-carga/alta/ordenes-de-carga.alta.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/alta/ordenes-de-carga.alta.component.ts
@@ -110,9 +110,12 @@ export class OrdenesDeCargaAlta extends BaseComponent implements OnInit {
             if (params["id"] > 0) this.ordenDeCargaId = params["id"];
         });
         this.navService.setSeccionList([]);
-        this.onCorredorFocusOut('', false);
         this.obtenerMateriales();
-        if (this.esCorredor) {
+
+        if (this.esComercial) {
+            this.onCorredorFocusOut('', false);
+        }
+        else if (this.esCorredor) {
             this.CodigoCorredor = sessionStorage.getItem("proveedor");
             if (this.ordenDeCargaId == 0) {
                 this.cargarClientes(this.CodigoCorredor);
@@ -991,7 +994,7 @@ export class OrdenesDeCargaAlta extends BaseComponent implements OnInit {
     }
 
     validarSisaCorredorCliente() {
-        
+
         if (!this.ordenDeCarga.ContratoSeleccionado || !this.validaCPEDG) {
             this.mensajeComponent.setMsgsEmpty();
             return;
