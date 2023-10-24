6a33b0c9a0a6b81cc701a1926b7e0619c7dfd7b1
Ali Rigol <mrigol@baufest.com>
2023-10-24 17:36:54 -0300

    fix/ARMOA003-2479: arreglar cnrt

--- SustitucionMOA/src/app/ordenes-de-carga/alta/ordenes-de-carga.alta.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/alta/ordenes-de-carga.alta.component.ts
@@ -121,6 +121,7 @@ export class OrdenesDeCargaAlta extends BaseComponent implements OnInit, IOrdene
 
     intermediarioFleteCuitFormatoValido: boolean = true;
     escalableCNRT?: boolean;
+    errorAlValidarEscalable = false;
     ordenActivaScato: boolean = false;
     mensajeValidacionScato: string = "";
 
@@ -1507,6 +1508,7 @@ export class OrdenesDeCargaAlta extends BaseComponent implements OnInit, IOrdene
             .subscribe(res => {
                 this.validando.Escalable = false;
                 const validezCNRTResponse = this.manejarErroresApiResponse(res)
+                this.errorAlValidarEscalable = !!(res.error || res.info);
                 if (!validezCNRTResponse) {
                     this.setValorEscalable()
                 } else {
