8040d927d3b409716304814c817dcf90b5c263e9
Brisa Melgarejo <bmelgarejo@baufest.com>
2023-10-31 12:10:20 -0300

    QACompras
    Fix

--- SustitucionMOA/src/app/common/models/ordenes-de-carga/ValidarCamionResponse.ts
+++ SustitucionMOA/src/app/common/models/ordenes-de-carga/ValidarCamionResponse.ts
@@ -4,3 +4,4 @@ export interface ValidarCamionResponse {
 }
 
 export const MSG_ALERTA_NO_ESCALABLE = { severity: 'warn', summary: 'Escalable', detail: 'Los datos no corresponden a un camión escalable.', life: 5000 };
+export const MSG_ALERTA_CAMION_NO_EXISTE = { severity: 'warn', summary: 'Camión', detail: 'Las patentes no corresponden a un camión válido.', life: 5000 };
