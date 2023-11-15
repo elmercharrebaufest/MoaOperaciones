a5e44c6863182977d7c64f2166e9f66a1c9b0935
Ali Rigol <mrigol@baufest.com>
2023-11-15 14:24:37 -0300

    fix en órdenes de carga y aplicaciones carta porte

--- SustitucionMOA/src/app/aplicacion-ccpp/listado/listado.component.ts
+++ SustitucionMOA/src/app/aplicacion-ccpp/listado/listado.component.ts
@@ -12,6 +12,7 @@ import { DropdownComponent, DropdownOption } from '../../common/view-child/dropd
 
 import * as XLSX from 'xlsx';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { Permiso } from '../../common/enums/Permisos';
 
 @Component({
   selector: 'app-listado',
@@ -29,7 +30,7 @@ export class ListadoComponent extends AplicacionCcppBaseComponent implements OnD
   ccppFiltro = new FormControl();
   estadoFiltro = new FormControl();
   clienteFiltro = new FormControl();
-  esAdmin = this.isAuthorized('ADMIN APLICACIONES CCPP');
+  esAdmin = this.isAuthorized(Permiso.AdminAppCCPP); 
   disabled = false;
   show = false;
   develop = true;
@@ -137,5 +138,19 @@ export class ListadoComponent extends AplicacionCcppBaseComponent implements OnD
     this.opcionesEstadoAplicacionCCPP = FiltroEstados;
     this.opcionesClientes = FiltroClientes;
   }
-  eliminarAplicacion(aplicacion: AplicacionCCPP) { }
+  eliminarAplicacion(aplicacion: AplicacionCCPP) {
+    this.blockUI.start("Eliminando ...");
+    try {
+      this.service.eliminarAplicacion(aplicacion.Id).subscribe({
+        next: (res) => {
+          this.blockUI.stop();
+          this.getListado();
+        },
+        error: (err) => this.blockUI.stop()
+        ,
+      })
+    } catch (error) {
+      this.blockUI.stop()
+    }
+  }
 }
