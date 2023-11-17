33f888d87e4f0555ada33da9c25a43cfebc41c09
BAUNET\mpuca <mpuca@baufest.com>
2023-11-17 12:34:01 -0300

    ARMOA003-2526 - Filtrar contratos sin cesion de pago

--- SustitucionMOAUtils/Services/EcheqService.cs
+++ SustitucionMOAUtils/Services/EcheqService.cs
@@ -10,6 +10,7 @@ using SustitucionMOAModel.Dto;
 using SustitucionMOAModel.Entities;
 using SustitucionMOAModel.Models;
 using SustitucionMOAModel.Models.WSMapMOA.CartaPorte.Formulario;
+using SustitucionMOAModel.Models.WSMapMOA.Contrato.Detalle;
 using SustitucionMOAModel.Models.WSMapMOA.Echeq;
 using SustitucionMOARepositorio;
 using SustitucionMOAUtils.Interfaces;
@@ -57,18 +58,21 @@ namespace SustitucionMOAUtils.Services
                 result.Where(x => x.Clasificacion == "PRODUCTOR" && x.MarcaCheque == true).ToList().ForEach(x => x.Documentos.ForEach(k => k.MarcaCheque = true));
                 result.Where(x => x.Clasificacion != "PRODUCTOR" && x.Documentos.Any(e => e.MarcaCheque == true)).ToList().ForEach(k => k.MarcaCheque = true);
 
+                List<string> cttosConCesionPago = this.GetContratosConCesionPago(result, proveedor);
+                var resultFiltrado = result.Where(x => !cttosConCesionPago.Contains(x.Contrato)).ToList();
+
                 List<string> documentos = result.Where(a => a.MarcaCheque).SelectMany(a => a.Documentos.Where(b => b.MarcaCheque).Select(b => b.Documento)).ToList();
                 var aperturas = repositorio.Listar<EcheqApertura>(x => x.Estado == true &&
                    documentos.Contains(x.EcheqLiquidacion.Documento));
 
                 foreach (var apertura in aperturas)
                 {
-                    EcheqLiquidacionDto liquidacion = result
+                    EcheqLiquidacionDto liquidacion = resultFiltrado
                         .Where(a => a.Contrato == apertura.EcheqLiquidacion.EcheqNegocio.Contrato && a.Pedido == apertura.EcheqLiquidacion.EcheqNegocio.Pedido)
                         .SelectMany(a => a.Documentos).Where(a => a.Documento == apertura.EcheqLiquidacion.Documento).SingleOrDefault();
                     liquidacion.Aperturas.Add(new EcheqAperturaDto { OrdenCheque = apertura.OrdenCheque, ImporteCheque = apertura.ImporteCheque });
                 }
-                return result;
+                return resultFiltrado;
             }
             catch (ValidationCustomException e)
             {
@@ -84,6 +88,37 @@ namespace SustitucionMOAUtils.Services
             }
         }
 
+        private List<string> GetContratosConCesionPago(List<EcheqNegocioDto> result, string proveedor)
+        {
+            HashSet<string> cttos = new HashSet<string>(result.Select(c => c.Contrato));
+            List<string> cttosConCesionPago = new List<string>();
+
+            foreach (string ctto in cttos)
+            {
+               if(this.ContratoTieneCesionDePago(proveedor, ctto))
+                {
+                    cttosConCesionPago.Add(ctto);
+                }
+            }
+
+            return cttosConCesionPago;
+        }
+
+        public bool ContratoTieneCesionDePago(string proveedor, string contrato)
+        {
+            ContratoDetalleWSMOAResponse response = (ContratoDetalleWSMOAResponse)new ContratoDetalleConsumerMOA().request(proveedor, contrato);
+            if (response == null || response.error == "06")
+            {
+                throw new InfoCustomException(String.Format(InfoMsg.ElementoNoExiste, "Contrato", contrato));
+            }
+
+            if (response.error != null && response.error != "" && response.error != "01")
+            {
+                throw new ValidationCustomException(ErrorMsg.Error);
+            }
+
+            return response.condicionesPago.Any(condicion => condicion.Trim().ToUpper().Contains("CESIÓN DE PAGOS"));
+        }
         public string MarcarContrato(EcheqRequestModel request)
         {
             try
