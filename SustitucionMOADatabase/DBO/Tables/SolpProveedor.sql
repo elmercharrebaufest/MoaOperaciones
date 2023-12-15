b6e1a3ceb584b4551271c90e6e50c6b515aa01e4
Brisa Melgarejo <bmelgarejo@baufest.com>
2023-12-15 14:56:56 -0300

    ARMOA018-841
    Se agregó el cascade

--- SustitucionMOADatabase/DBO/Tables/SolpProveedor.sql
+++ SustitucionMOADatabase/DBO/Tables/SolpProveedor.sql
@@ -10,7 +10,7 @@ CONSTRAINT [PK_dbo.SolpProveedor] PRIMARY KEY CLUSTERED
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
-	CONSTRAINT [FK_SolpProveedor_SolpPosicion] FOREIGN KEY (SolpPosicion_Id) REFERENCES [SolpPosicion]([Id]),
+	CONSTRAINT [FK_SolpProveedor_SolpPosicion] FOREIGN KEY (SolpPosicion_Id) REFERENCES [SolpPosicion]([Id]) ON DELETE CASCADE,
 	CONSTRAINT [FK_SolpProveedor_Proveedor] FOREIGN KEY (Proveedor_Id) REFERENCES [Proveedor]([Id]),
 	CONSTRAINT [FK_SolpProveedor_TablaGeneral_TipoFiltroProveedorSolp] FOREIGN KEY (TipoFiltroProveedorSolp_Id) REFERENCES [TablaGeneral]([Id]),
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
