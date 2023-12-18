074c795061d508ec1460fa048783ae6b06e4a23b
Brisa Melgarejo <bmelgarejo@baufest.com>
2023-12-18 17:49:03 -0300

    ARMOA018-816
    Hice la validacion para que valide las solps tratadas

--- SustitucionMOA/src/app/compras/dashboard-comprador/listado-dashboard-comprador/listado-dashboard-comprador.component.ts
+++ SustitucionMOA/src/app/compras/dashboard-comprador/listado-dashboard-comprador/listado-dashboard-comprador.component.ts
@@ -77,6 +77,8 @@ export class ListadoDashboardCompradorComponent extends ListBaseComponent {
     displayChatInterno: boolean = false;
     chatLeido: boolean = false;
     public chat: ChatComprasDto;
+    tratada: any;
+
     fechaInicio: any = null;
     fechaFin: any = null;
     rangeDates: Date[];
@@ -342,8 +344,30 @@ export class ListadoDashboardCompradorComponent extends ListBaseComponent {
             )
     }
 
-    publicarCotizacion(Id: string) {
-        this.goToSeccionParam('/compras/peticion-de-oferta-formulario', Id);
+    publicarCotizacion(Id: string, nroSolp: string) {   
+        this.blockUI.start('Cargando...');  
+        this.service.validarSolpTratada(nroSolp)
+        .subscribe(
+            (result) => {
+                if (result.logout == true) {
+                    this.sessionDataService.logout();
+                }
+                else {                   
+                    if(result){  
+                        this.floatMsgService.setErrorMsg("No se puede crear una nueva PO por que la SOLP fue tratada desde SAP");
+                    }else{
+                       this.goToSeccionParam('/compras/peticion-de-oferta-formulario', Id);
+                    }
+                    this.blockUI.stop();
+                }
+            },
+            (error) => {
+                this.blockUI.stop();
+                this.mensajeComponent.setErrorMsg(error.message);
+            }
+        )
+       
+        
     }
 
     onRowDblClick(a, b) {
--- SustitucionMOATest/Services/ComprasServiceTest.cs
+++ SustitucionMOATest/Services/ComprasServiceTest.cs
@@ -263,6 +263,7 @@ namespace SustitucionMOATest.Services
                                     CpEntrega = "CP",
                                     CalleEntrega = "Calle",
                                     NumeroEntrega = "NroEntrega",
+
                                     Subposiciones = new List<SolpSubposicion> { new SolpSubposicion {
                                         Id = 1, Tarea = "Tarea", Cantidad = 2, PrecioBruto = 500, Unidad = new TablaSap { CodigoSap = "UNI" } } }
                                 }
@@ -2224,6 +2225,18 @@ namespace SustitucionMOATest.Services
         }
 
         [Test]
+
+        public void ValidarSolpTratadaTest()
+        {
+            repositorioMock.Setup(y => y.Obtener(It.IsAny<Expression<Func<Solp, bool>>>())).Returns(solp);
+            obtenerSolpConsumerMOAMock.Setup(y => y.RequestSolpWithNroAndDates(It.IsAny<ObtenerSolpRequest>())).Returns(new ObtenerSolpSAPResponse
+            {
+                Posiciones = new List<PosicionSolpSAP> { new PosicionSolpSAP { Cantidad = 1000, NumeroPosicion = "00011" } }
+            });
+            target.ValidarSolpTratada("02929292");
+            repositorioMock.Verify(y => y.Obtener(It.IsAny<Expression<Func<Solp, bool>>>()), Times.Once);
+        }
+
         public void ListarSolpCompradorOk()
         {
             var listaSolp = new ListaPaginada<SolpDto>(new List<SolpDto> { new SolpDto { Id = 1, ItemsTotales = 7 } }, 1, 10, 5);
@@ -2234,5 +2247,4 @@ namespace SustitucionMOATest.Services
             Assert.AreEqual(listaSolp.GetType(), result.GetType());
         }
     }
-
 }
