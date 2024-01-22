814468a9b8bf7b852fab9f5082e8609553f39772
Brisa Melgarejo <bmelgarejo@baufest.com>
2024-01-22 14:08:53 -0300

    ARMOA018-900
    Agrego que la entidad tambien se guarda ademas del id porque despues uso la entidad en el finalizar

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -7721,6 +7721,7 @@ namespace SustitucionMOAUtils.Services
             {
 
                 var respuestaGuardarSOLP = new RespuestaGuardarSOLP { Solp = new SolpDto { NroSolp = solp.NroSolp } };
+                var proveedores = repositorio.Listar<Usuario>();
                 if (nroOrdenDeCompra.Count > 0)
                 {
                     foreach (var nro in nroOrdenDeCompra)
@@ -7731,6 +7732,7 @@ namespace SustitucionMOAUtils.Services
                         {
                             var posicion = solp.Posiciones.Where(x => x.Indice == Int32.Parse(posicionSap.Indice)).FirstOrDefault();
                             posicion.ProveedorAdjudicado_Id = proveedor.Usuario_Id;
+                            posicion.ProveedorAdjudicado = proveedores.Where(x => x.Id == proveedor.Usuario_Id).FirstOrDefault();
                             posicion.RegistroInfoNro = posicionSap.RegistroInfo;
                             posicion.OrganizacionDeComprasCodigo = ordenDeCompra.Cabecera.OrganizacionDeComprasCodigo;
                         }
@@ -7745,9 +7747,9 @@ namespace SustitucionMOAUtils.Services
                 Logger.Log.Error(e);
                 throw;
             }
-        }
+        }    
 
-    }
+}
 
 
     public static class SolpTemplateKeys
