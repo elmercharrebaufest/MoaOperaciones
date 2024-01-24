8446b3fcba9fa8d47cc7a5b8de1f2a32efd89bce
Brisa Melgarejo <bmelgarejo@baufest.com>
2024-01-24 14:29:20 -0300

    ARMOA018-932
    Los datos del usuario comprador se llaman cuando se hace click al finalizar

--- SustitucionMOA/src/app/compras/solp/solp.component.ts
+++ SustitucionMOA/src/app/compras/solp/solp.component.ts
@@ -875,7 +875,7 @@ export class SolpComponent extends BaseComponent implements OnInit {
                     } else {
                         this.combos = result;
                         this.combos.flagSolpFinalizada = this.flagSolpFinalizada;
-                        this.obtenerUsuarioCompras();
+                       // this.obtenerUsuarioCompras();
                         this.setupCentroPorDefecto();
                         this.setupDireccionCentroPorDefecto();
                         this.setupMonedaPorDefecto();
@@ -992,6 +992,7 @@ export class SolpComponent extends BaseComponent implements OnInit {
 
     // Abre el modal del boton finalizar
     showFinalizarDialog() {
+        this.obtenerUsuarioCompras();
         this.displayFinalizar = true;
     }
 
