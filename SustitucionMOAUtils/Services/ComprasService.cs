47deb182321a8909fe015c23bd0e605a19ff5c1b
Eugenio Martin <emartin@baufest.com>
2024-02-23 09:20:41 -0300

    ARMOA018-1007

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -6378,7 +6378,7 @@ namespace SustitucionMOAUtils.Services
                         var subpos = new CotizacionSubposicionesDto
                         {
                             Precio = (decimal)subposicion.PrecioBruto,
-                            Cantidad = (int)subposicion.Cantidad,
+                            Cantidad = (decimal)subposicion.Cantidad,
                             UnidadDeMedidaId = subposicion.Unidad_Id,
                             SolpSubPosicionId = subposicion.Id,
                             CotizacionPosicionId = peticionEntidad.Posiciones.Where(pos => pos.SolpPosicion_Id == posicion.Id).FirstOrDefault().Id,
