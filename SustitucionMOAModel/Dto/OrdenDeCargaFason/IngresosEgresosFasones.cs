528f59628c0c457fc286f0716dbe7e8626f14c6e
Ali Rigol <mrigol@baufest.com>
2024-03-20 14:05:29 -0300

    feat/ARMOA003-2702: agregar propiedades para interactuar con Scato-Fason

--- SustitucionMOAModel/Dto/OrdenDeCargaFason/IngresosEgresosFasones.cs
+++ SustitucionMOAModel/Dto/OrdenDeCargaFason/IngresosEgresosFasones.cs
@@ -10,8 +10,9 @@ namespace SustitucionMOAModel.Dto.OrdenDeCargaFason
 	public class IngresosEgresosFasones
 	{
 		public long FasonId { get; set; }
-		public int Cantidad { get; set; }
-		public DateTime FechaIngreso { get; set; }
+		public double PesadaTara { get; set; }
+        public double PesadaNeto { get; set; }
+        public DateTime FechaIngreso { get; set; }
 		public DateTime FechaEgreso { get; set; }
 		public string NroRemito { get; set; }
 		public string UniMedCant { get; set; }
