a92df76a3901c3d0a89ff58d2999312070a1dbc3
Ezequiel Gomez <egomez@huenei.com>
2024-04-16 15:13:25 -0300

    fix insert usuario

--- SustitucionMOADatabase/scripts/PostDeployment/Varios.sql
+++ SustitucionMOADatabase/scripts/PostDeployment/Varios.sql
@@ -1,6 +1,6 @@
 ﻿IF NOT EXISTS (SELECT TOP 1 1 FROM Usuario WHERE Mail = 'moaoperaciones@molinosagro.com.ar') 
 BEGIN
-	insert into Usuario values ('moaoperaciones@molinosagro.com.ar','30715118773',1,2,null,'',0,null,null,'',null)
+	insert into Usuario values ('moaoperaciones@molinosagro.com.ar','30715118773',1,2,null,'',0,null,null,'',null,null)
 END
 
 IF NOT EXISTS (SELECT TOP 1 1 FROM Configuracion WHERE Code = 'Soja200Desde') 
