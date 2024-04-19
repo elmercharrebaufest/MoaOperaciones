b1b516b1688c2b938daae6b22383e0aef94b6cbc
Rocio Orlando <rorlando@baufest.com>
2024-04-19 16:36:35 -0300

    ARMOA018-1117: Se genera un error al intentar descargar el legajo
    Cambio en la condicion del metodo PuedeDescargarPliegoDesdeLink

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -2870,7 +2870,7 @@ namespace SustitucionMOAUtils.Services
                 }
             }
 
-            if (solp.TipoSolp.Descripcion == "SIN_PLIEGO" && solp.Pliego.Archivos.Count > 0)
+            if (solp.TipoSolp == null || (solp.TipoSolp.Descripcion == "SIN_PLIEGO" && solp.Pliego.Archivos.Count == 0))
             {
                 return SolpDescargaZipPorLink.SinArchivos;
             }
