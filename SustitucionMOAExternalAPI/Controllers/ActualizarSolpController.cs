67b16a69df17a3b03ba6c2ec26e29b345a12f92c
Brisa <bmelgare@baufest.com>
2024-05-03 11:21:09 -0300

    ARMOA018-1153
    Cambios en la api para que haga un control de las colas

--- SustitucionMOAExternalAPI/Controllers/ActualizarSolpController.cs
+++ SustitucionMOAExternalAPI/Controllers/ActualizarSolpController.cs
@@ -1,6 +1,7 @@
 ﻿using SustitucionMOAUtils.Interfaces;
 using SustitucionMOAUtils.Logger;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -9,6 +10,8 @@ namespace SustitucionMOAExternalAPI.Controllers
     public class ActualizarSolpController : ApiController
     {
         private readonly IComprasService comprasService;
+        private readonly Queue<string> colaDeSolicitudes = new Queue<string>();
+        private readonly object lockObj = new object(); 
 
         public ActualizarSolpController(IComprasService comprasService)
         {
@@ -20,25 +23,62 @@ namespace SustitucionMOAExternalAPI.Controllers
         {
             try
             {
-                Log.ExternalAPIInfo(string.Format("Inicio Se informaron cambios para la SOLP: {0}", nrosolp));
+                Log.ExternalAPIInfo($"Inicio Se informaron cambios para la SOLP: {nrosolp}");
 
-                // Iniciar el proceso de comprasService.ObtenerSolpesDesdeSAPJob de manera asincrónica
-                Task.Run(() =>
+                lock (lockObj)
                 {
-                    comprasService.ObtenerSolpesDesdeSAPJob(new SustitucionMOAWS.WSConsumers.ObtenerSolpRequest
-                    {
-                        NumeroSolp = nrosolp.TrimStart('0').PadLeft(10, '0'),
-                        FechaDesde = new DateTime(2010, 01, 01),
-                        FechaHasta = DateTime.Now.Date.AddDays(1)
-                    });
+                    colaDeSolicitudes.Enqueue(nrosolp.TrimStart('0').PadLeft(10, '0'));
+                }
+
+                if (colaDeSolicitudes.Count == 1)
+                {
+                    ProcesarSiguienteSolicitud();
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                Log.ExternalAPIError(ex);
+                return InternalServerError(ex); 
+            }
+        }
+
+
+        private void ProcesarSiguienteSolicitud()
+        {
+            string nrosolp;
+            lock (lockObj)
+            {
+                if (colaDeSolicitudes.Count == 0)
+                {
+                    return;
+                }
+                nrosolp = colaDeSolicitudes.Peek();
+            }
+
+            try
+            {
+                comprasService.ObtenerSolpesDesdeSAPJob(new SustitucionMOAWS.WSConsumers.ObtenerSolpRequest
+                {
+                    NumeroSolp = nrosolp,
+                    FechaDesde = new DateTime(2010, 01, 01),
+                    FechaHasta = DateTime.Now.Date.AddDays(1)
                 });
             }
             catch (Exception ex)
             {
                 Log.ExternalAPIError(ex);
             }
+            finally
+            {
+                lock (lockObj)
+                {
+                    colaDeSolicitudes.Dequeue();
+                }
 
-            return Ok();
+                ProcesarSiguienteSolicitud();
+            }
         }
 
     }
