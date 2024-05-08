24a3a238056b7df61fe330160417ce6b499c743c
Muhammad Ali Rigol <Ali.Rigol@molinosagro.com.ar>
2024-05-08 13:03:56 +0000

    Revert "Revert "fix/ARMOA003-AltaOCMultifirma: utilicar cliente seleccionado""

--- SustitucionMOAUtils/Services/OrdenDeCargaService.cs
+++ SustitucionMOAUtils/Services/OrdenDeCargaService.cs
@@ -1667,17 +1667,18 @@ namespace SustitucionMOAUtils.Services
         private void LlenarOrdenAltaCorredorCliente(OrdenDeCarga ordenDeCarga, Usuario usuario)
         {
             var esComercial = usuario.TienePermiso(PermisoEnum.VerOrdenesDeCargaParaComerciales);
-            Log.Info($"Llenar Orden de carga para alta. esComercial: {esComercial}.");
+            var esMultifirma = usuario.TienePermiso(PermisoEnum.SeleccionarVendedor);
+            Log.Info($"Llenar Orden de carga para alta. esComercial: {esComercial}. esMultifirma {esMultifirma}");
 
             Proveedor cliente = null;
-            if (esComercial)
+            if (esComercial || (esMultifirma && ordenDeCarga.CUITCliente != usuario.CUITRegistro))
             {
                 cliente = repositorio.Obtener<Proveedor>(x =>
                     x.CUIT == ordenDeCarga.CUITCliente &&
                     x.EstadoAprobacion == EstadoAprobacion.Aprobado &&
                     x.TipoProveedor.Id == (int)TipoUsuarioEnum.Cliente);
 
-                if (!string.IsNullOrEmpty(ordenDeCarga.CUITCorredor))
+                if (!string.IsNullOrEmpty(ordenDeCarga.CUITCorredor) && esComercial)
                 {
                     var corredor = repositorio.Obtener<Proveedor>(x =>
                         x.CUIT == ordenDeCarga.CUITCorredor &&
