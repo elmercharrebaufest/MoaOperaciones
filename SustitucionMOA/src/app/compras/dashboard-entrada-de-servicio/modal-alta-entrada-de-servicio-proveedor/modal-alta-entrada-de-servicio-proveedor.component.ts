f0f5cdbe7928b9785f703efe3ec4d4b008a18c09
Nelson Garcette <Nelson.Garcette@molinosagro.com.ar>
2024-06-12 19:29:59 +0000

    Merged PR 1265: MMSN-1104/1109-Dashboard Proveedor Documento
    Al ingresar una certificación, en la pantalla de resumen.
    
    Actualizar el label FECHA DOCUMENTO por Fecha del prestación del servicio
    
    +
    
    Agregar validación para el ingreso de Fecha de prestación del servicio en el resumen de certificaciones - Proveedor
    
    De no completarse la fecha, se emitirá el mensaje: “Ingrese por la fecha de prestación del servicio”.

--- SustitucionMOA/src/app/compras/dashboard-entrada-de-servicio/modal-alta-entrada-de-servicio-proveedor/modal-alta-entrada-de-servicio-proveedor.component.ts
+++ SustitucionMOA/src/app/compras/dashboard-entrada-de-servicio/modal-alta-entrada-de-servicio-proveedor/modal-alta-entrada-de-servicio-proveedor.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ComprasService } from '../../compras.service';
-import { ConfirmationService } from 'primeng/api';
+import { ConfirmationService, Message } from 'primeng/api';
 import { CalendarModule } from 'primeng/calendar';
 import { forEach } from '@angular/router/src/utils/collection';
 import { FormsModule } from '@angular/forms';
@@ -50,6 +50,7 @@ export class ModalAltaEntradaDeServicioProveedorComponent implements OnInit {
     fechaDocMax: Date;
     fechaContabilizacionMin: Date;
     fechaContabilizacionMax: Date;
+    documentDateMsg: Message[] = [];
 
     entrySheetData = {
         "EntrySheetHeader": {
@@ -236,53 +237,54 @@ export class ModalAltaEntradaDeServicioProveedorComponent implements OnInit {
     }
 
     certificarPosicion() {
-        this.buildEntrySheet();
-        this.service.postCreateAsync(this.entrySheetObjects).subscribe(
-            (response) => {
-                this.mensajeError = '';
-                let resultMsj: string[] = [];
-                let msjTypes: string[] = [];
+        if (this.validateValues() === true) {
+            this.buildEntrySheet();
+            this.service.postCreateAsync(this.entrySheetObjects).subscribe(
+                (response) => {
+                    this.mensajeError = '';
+                    let resultMsj: string[] = [];
+                    let msjTypes: string[] = [];
 
-                response.data.forEach(element => {
-                    if (!element) {
-                        this.mensajeError = 'Error del servidor, vuelva a intentarlo más tarde.'
+                    response.data.forEach(element => {
+                        if (!element) {
+                            this.mensajeError = 'Error del servidor, vuelva a intentarlo más tarde.'
+                        }
+
+                        if (!element.Message) {
+                            element.Message = "Ha ocurrido un error por favor inténtelo nuevamente más tarde."
+                        }
+
+                        let msj = element.Message.startsWith("Sólo es posible contabilizar en ") ||
+                            element.Message.startsWith("Contabilice en ") ?
+                            "El período se encuentra cerrado, por favor contabilice en el periodo actual." : element.Message;
+
+                        resultMsj.push("<li>" + msj + "</li>");
+
+                        if (!msjTypes.includes(element.Type)) {
+                            msjTypes.push(element.Type);
+                        }
+                    });
+
+                    this.mensajeError = resultMsj.join("");
+
+                    this.confirmationService.confirm({
+                        message: "<ul>" + this.mensajeError + "</ul>",
+                        accept: () => this.cerrarMensajes(msjTypes),
+                        rejectVisible: false
+                    });
+                },
+                (error) => {
+                    this.confirmationService.confirm({
+                        message: error.error.Message,
+                        accept: () => {
+                            this.closeDialog.emit();
+                        },
+                        rejectVisible: false
                     }
-
-                    if (!element.Message) {
-                        element.Message = "Ha ocurrido un error por favor inténtelo nuevamente más tarde."
-                    }
-
-                    let msj = element.Message.startsWith("Sólo es posible contabilizar en ") ||
-                        element.Message.startsWith("Contabilice en ") ?
-                        "El período se encuentra cerrado, por favor contabilice en el periodo actual." : element.Message;
-
-                    resultMsj.push("<li>" + msj + "</li>");
-
-                    if (!msjTypes.includes(element.Type)) {
-                        msjTypes.push(element.Type);
-                    }
-                });
-
-                this.mensajeError = resultMsj.join("");
-
-                this.confirmationService.confirm({
-                    message: "<ul>" + this.mensajeError + "</ul>",
-                    accept: () => this.cerrarMensajes(msjTypes),
-                    rejectVisible: false
-                });
-            },
-            (error) => {
-                this.confirmationService.confirm({
-                    message: error.error.Message,
-                    accept: () => {
-                        this.closeDialog.emit();
-                    },
-                    rejectVisible: false
+                    );
                 }
-                );
-            }
-        );
-
+            );
+        }
     }
 
     buildEntrySheet() {
@@ -380,5 +382,15 @@ export class ModalAltaEntradaDeServicioProveedorComponent implements OnInit {
     //Redondeo de decimales
     round(num: number, decimals: number) {
         return Number(num.toFixed(decimals));
+    }
+
+    validateValues() {
+        if (this.fechaDocumento === null || this.fechaDocumento === undefined || this.fechaDocumento.toString() === '') {
+            this.documentDateMsg = [];
+            this.documentDateMsg.push({ severity: 'error', summary: '', detail: 'Ingrese una fecha de prestación del servicio' });
+            return false;
+        }
+        this.documentDateMsg = [];
+        return true;
     }
 }
