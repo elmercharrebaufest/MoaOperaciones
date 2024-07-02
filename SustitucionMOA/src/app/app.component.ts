b6ff34e9b2a7d59ca68b1fedff13b36296865ec1
Eugenio Martin <emartin@baufest.com>
2024-07-02 10:58:32 -0300

    ARMOA003-2850

--- SustitucionMOA/src/app/app.component.ts
+++ SustitucionMOA/src/app/app.component.ts
@@ -51,12 +51,12 @@ export class AppComponent implements OnDestroy {
     @HostListener('window:keydown', ['$event'])
     keyEvent(event: KeyboardEvent) {
         if (event.altKey == true && event.ctrlKey == true && event.key == "b") {
-            document.getElementById("aboutScreenBTN").click();
+           // document.getElementById("aboutScreenBTN").click();
         };
     }
 
     closeAboutScreen() {
-        document.getElementById("aboutScreenBTN").click();
+       // document.getElementById("aboutScreenBTN").click();
     }
 
     ngOnInit() {
