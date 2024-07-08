3133a6228e4b4e6f74ac08080f128009e1916983
Rocio Orlando <rorlando@baufest.com>
2024-07-08 16:13:08 -0300

    ARMOA018-1275: El panel de Agrupar TH debe mostrar los documentos
    Nuevo componente adjuntos solp
    Nueva columna e iconos
    Agregado de servicio, controller, Dto
    Cambio de parametros en el officetohtml para que tome varias url

---
+++ SustitucionMOAModel/Dto/Compras/AdjuntosSolpDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SustitucionMOAModel.Dto.Compras
+{
+    public class AdjuntosSolpDto
+    {
+        public string Pliego { get; set; }
+        public string PDF { get; set; }
+        public List<ArchivoDto> ArchivosEspecificacionesTecnicas { get; set; } = new List<ArchivoDto>();
+        public List<ArchivoDto> ArchivosCotizacion { get; set; } = new List<ArchivoDto>();
+        public List<ArchivoDto> ArchivosCondicionesEspeciales { get; set; } = new List<ArchivoDto>();
+        public string ObservacionCondicionesEspeciales { get; set; }
+    }
+}
