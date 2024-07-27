1892f6691144ccad28db52232faad9694b0a67d9
Ali Rigol <mrigol@baufest.com>
2024-07-26 21:46:11 -0300

    feat/ARMOA018-1327: cambiar formato hora de historial cotizaciones

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -9574,7 +9574,7 @@ namespace SustitucionMOAUtils.Services
             {
                 Id = x.Id,
                 Cotizacion_Id = x.Cotizacion_Id,
-                FechaFinalizacion = x.FechaFinalizacion.ToString("dd-MM-yyyy hh:mm:ss") + "hs",
+                FechaFinalizacion = x.FechaFinalizacion.ToString("dd-MM-yyyy HH:mm:ss") + "hs",
                 Log = x.Log,
                 Usuario_Id = x.Usuario_Id,
                 UsuarioRazonSocial = x.Usuario.ObtenerRazonSocial()
