17933b26b381a30516ed5032a42fc5b0333e7bd2
Ali Rigol <mrigol@baufest.com>
2024-08-28 14:10:53 -0300

    fix/ARMOA018-1387: permitir descarga de nuevos archivos para legajo externo

--- SustitucionMOA/src/app/compras/legajo-externo/legajo-externo.component.ts
+++ SustitucionMOA/src/app/compras/legajo-externo/legajo-externo.component.ts
@@ -4,7 +4,7 @@ import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { ComprasService } from '../compras.service';
 import { MensajeComponent } from '../../common/view-child/mensaje/mensaje.component';
 import { LegajoExternoDto } from '../../modelos/legajoExternoDto';
-import { LegajoDto } from '../../modelos/compras/legajoDto';
+import { LegajoDto, LegajoTipo } from '../../modelos/compras/legajoDto';
 
 @Component({
     selector: 'app-legajo-externo',
@@ -136,7 +136,18 @@ export class LegajoExternoComponent implements OnInit {
                         this.mensajeComponent.setErrorMsg(error.message);
                         this.blockUI.stop();
                     })
-        } else {
+        } else if (legajoDto.Tipo == LegajoTipo.Cotizacion && legajoDto.ArchivoId) {
+            this.descargarHistorialCotizaciones(legajoDto.ArchivoId);
+        }
+        else if (legajoDto.Tipo == LegajoTipo.HistorialMovimientos) {
+            this.descargarHistorialMovimientos();
+        }
+        else if (legajoDto.Tipo == LegajoTipo.RevisionTecnica && legajoDto.ArchivoId) {
+            this.descargarRevisionTecnica(legajoDto.ArchivoId);
+        } else if (legajoDto.Tipo == LegajoTipo.CotizacionAdjunto) {
+            this.descargarAdjuntosProveedores();
+        }
+        else {
             this.blockUI.start("Descargando...");
             this.service.DescargarArchivo(legajoDto.ArchivoId)
                 .subscribe(
@@ -248,5 +259,60 @@ export class LegajoExternoComponent implements OnInit {
                     this.mensajeComponent.setErrorMsg(error.message);
                     this.blockUI.stop();
                 })
+    }
+    private descargarHistorialCotizaciones(cotizacionId: number) {
+        this.blockUI.start("Descargando...");
+        this.service.descargarHistorialCotizaciones(cotizacionId).subscribe(
+            (response) => {
+                var byteArray = new Uint8Array(response.FileContents);
+                var blob = new Blob([byteArray], {
+                    type: "application/octet-stream",
+                });
+                this.downloadArchivoLocal(blob, response.FileDownloadName);
+                this.blockUI.stop();
+            },
+            (error) => {
+                this.mensajeComponent.setErrorMsg(error.message);
+                this.blockUI.stop();
+            }
+        )
+    }
+
+    private descargarRevisionTecnica(peticionDeOfertaId: number) {
+        this.blockUI.start("Descargando...");
+        this.service.descargarRevisionTecnica(peticionDeOfertaId).subscribe(
+            (response) => {
+                var byteArray = new Uint8Array(response.FileContents);
+                var blob = new Blob([byteArray], {
+                    type: "application/octet-stream",
+                });
+                this.downloadArchivoLocal(blob, response.FileDownloadName);
+                this.blockUI.stop();
+            },
+            (error) => {
+                this.mensajeComponent.setErrorMsg(error.message);
+                this.blockUI.stop();
+            }
+        )
+    }
+    descargarHistorialMovimientos() {
+        this.blockUI.start("Descargando...");
+        let idPeticion = this.legajo.ListaLegajos[0].PeticionDeOfertaId;
+        this.service.descargarArchivoHistorialMovimientos(idPeticion)
+            .subscribe(
+                (result) => {
+                    this.blockUI.stop();
+                    var byteArray = new Uint8Array(result.FileContents);
+                    var blob = new Blob([byteArray], {
+                        type: "application/octet-stream",
+                    });
+                    this.downloadArchivoLocal(blob, result.FileDownloadName);
+                    ;
+                },
+                (error) => {
+                    this.mensajeComponent.setErrorMsg(error.message);
+                    this.blockUI.stop();
+                }
+            )
     }
 }
