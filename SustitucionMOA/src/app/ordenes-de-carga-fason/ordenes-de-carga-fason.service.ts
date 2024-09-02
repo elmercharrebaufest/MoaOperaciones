a774bd32c4a1650e07522506e62e363ef3923824
Diego Hidalgo <dhidalgo@baufest.com>
2024-09-02 10:05:44 -0300

    ARMOA003-3023 Editar OC Fason sin MOA

--- SustitucionMOA/src/app/ordenes-de-carga-fason/ordenes-de-carga-fason.service.ts
+++ SustitucionMOA/src/app/ordenes-de-carga-fason/ordenes-de-carga-fason.service.ts
@@ -192,13 +192,7 @@ export class OrdenesDeCargaFasonService extends OrdenesBaseService {
       )
       .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde"))));
   }
-  public actualizarSolicitudEdicion(ordenId: number, aprobado: boolean) {
-    return this.http
-      .post<ApiResponse<OrdenDeCargaFasonDto>>(
-        '/api/OrdenDeCargaFason/ActualizarSolicitudEdicion', { ordenId, aprobado }
-      )
-      .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde"))));
-  }
+  
   public anularOrden(ordenId: number) {
     return this.http
       .post<ApiResponse<OrdenDeCargaFasonDto>>(
