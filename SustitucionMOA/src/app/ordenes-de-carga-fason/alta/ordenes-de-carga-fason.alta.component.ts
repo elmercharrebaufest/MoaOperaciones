28452a9a6561dff398c40eaddee4f57feb7e8ead
Ali Rigol <mrigol@baufest.com>
2024-09-04 14:32:14 -0300

    fix/ARMOA003-3113: pantalla de edición fason con perfil cliente

--- SustitucionMOA/src/app/ordenes-de-carga-fason/alta/ordenes-de-carga-fason.alta.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga-fason/alta/ordenes-de-carga-fason.alta.component.ts
@@ -418,7 +418,7 @@ export class OrdenesDeCargaFasonAltaComponent
 
     obtenerOrdenDeCarga() {
         try {
-            this.subscriptionDropDowns = this.service.getOrdenDeCargaFason(this.ordenDeCargaFasonId).subscribe(
+            const obtenerOrdenSubscripcion = this.service.getOrdenDeCargaFason(this.ordenDeCargaFasonId).subscribe(
                 result => {
                     if (result.logout == true) {
                         this.sessionDataService.logout();
@@ -449,6 +449,7 @@ export class OrdenesDeCargaFasonAltaComponent
                     this.mensajeComponent.setErrorMsg(error.message);
                 }
             );
+            this.localSubscriptions.add(obtenerOrdenSubscripcion)
         } catch (e) {
             this.mensajeComponent.setErrorMsg(e);
         }
