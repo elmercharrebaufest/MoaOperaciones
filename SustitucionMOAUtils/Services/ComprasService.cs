d5a2fbae5d1064a602ad8c93a441d10e41e6be1b
Eugenio Martin <emartin@baufest.com>
2024-09-23 11:05:20 -0300

    ARMOA003-3225 -El registro info no se genera cuando existe un material no catalogado en la primera linea

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -6753,7 +6753,7 @@ namespace SustitucionMOAUtils.Services
 
                 if (ValidarCreacionDeRegistroInfo(cotizacion))
                 {
-                    if (!cotizacion.CotizacionPosiciones.All(x => x.NoDisponible == true))
+                    if (!cotizacion.CotizacionPosiciones.All(x => x.NoDisponible == true && x.PeticionDeOfertaSolpPosicion.SolpPosicion.MaterialSolp != null))
                     {
                         var registros = CrearRegistroInfoDto(cotizacion);
                         if (registros.Any(x => !x.EsModificar))
@@ -6785,8 +6785,7 @@ namespace SustitucionMOAUtils.Services
         private static bool ValidarCreacionDeRegistroInfo(Cotizacion cotizacion)
         {
             return cotizacion.PeticionDeOfertaUsuario.PeticionDeOferta.RegistroInfo != true && cotizacion.CotizacionEstado_Id == (int)CotizacionEstadoEnum.Cotizado
-                                && cotizacion.CotizacionPosiciones.FirstOrDefault().PeticionDeOfertaSolpPosicion.SolpPosicion.TipoPosicion.Codigo == "MATERIALES" &&
-                                cotizacion.CotizacionPosiciones.FirstOrDefault().PeticionDeOfertaSolpPosicion.SolpPosicion.MaterialSolp != null;
+                                && cotizacion.CotizacionPosiciones.FirstOrDefault().PeticionDeOfertaSolpPosicion.SolpPosicion.TipoPosicion.Codigo == "MATERIALES";
         }
 
         private void CrearRegistroInfo(Cotizacion cotizacion, List<RegistroInfoDto> registros)
