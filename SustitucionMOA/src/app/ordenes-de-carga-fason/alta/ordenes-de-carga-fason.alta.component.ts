5e8708975a4b21c482163c290cc696f43898c098
Ali Rigol <mrigol@baufest.com>
2024-09-25 11:11:39 -0300

    feat/ARMOA003-3189: quitar warning  cuando no habrá remitente comercial fason

--- SustitucionMOA/src/app/ordenes-de-carga-fason/alta/ordenes-de-carga-fason.alta.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga-fason/alta/ordenes-de-carga-fason.alta.component.ts
@@ -1152,9 +1152,7 @@ export class OrdenesDeCargaFasonAltaComponent
 
     asignarRemitenteComercial() {
         this.ordenDeCargaFason.RemitenteComercial = this.usaRemitenteComercial;
-        if (!this.ordenDeCargaFason.RemitenteComercial) {
-            this.floatMsgService.setInfoMsg("Su CUIT no será considerado como remitente comercial.")
-        } else {
+        if (this.ordenDeCargaFason.RemitenteComercial) {
             this.floatMsgService.setInfoMsg("Su CUIT será considerado como remitente comercial.")
         }
     }
