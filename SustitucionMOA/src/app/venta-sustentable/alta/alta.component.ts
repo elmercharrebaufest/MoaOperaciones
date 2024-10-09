fc10fcebfc9c9cccececac1151276088321ea3d9
Diego Hidalgo <dhidalgo@baufest.com>
2024-10-08 22:09:47 -0300

    ARMOA003-3311 CCSS Mostrar warning cuando se selecciona el mismo proveedor del usuario

--- SustitucionMOA/src/app/venta-sustentable/alta/alta.component.ts
+++ SustitucionMOA/src/app/venta-sustentable/alta/alta.component.ts
@@ -14,7 +14,7 @@ import { DeclaracionConformidadComponent } from '../declaracion-conformidad/decl
 import { isUndefined } from 'util';
 import { VendedorProveedor } from '../../common/models/vendedorProveedor';
 import { finalize } from 'rxjs/operators';
-import { MessageService } from 'primeng/api';
+import { Message, MessageService } from 'primeng/api';
 export interface DatosCopiar {
     NombreCampo: string;
     NombreCosecha: string;
@@ -93,7 +93,7 @@ export class AltaComponent extends BaseComponent implements OnInit {
 
     ingresarProveedorPorCUIT: boolean;
     esCorredor: boolean = sessionStorage.getItem("tipoUsuario") === "CORR";
-    codigoProveedor: string = sessionStorage.getItem("proveedor");
+    codigoProveedor: string = sessionStorage.getItem("proveedor") || "";
     campoCosechaId: any;
     CodigoProveedorEdit: string = "";
     campoProveedorE: any;
@@ -207,6 +207,9 @@ export class AltaComponent extends BaseComponent implements OnInit {
 
     onselectProveedor(proveedor?: VendedorProveedor) {
         if (proveedor) {
+            if (this.codigoProveedor == proveedor.idVendedor) {
+                this.mostrarWarningCuitsIguales();
+            }
             this.proveedorSeleccionado = proveedor;
             this.getProveedorId(this.proveedorSeleccionado.idVendedor);
         }
@@ -291,9 +294,6 @@ export class AltaComponent extends BaseComponent implements OnInit {
 
                     //Si cambió el CUIT le saco la razón social
                     if (this.CUITInicial != this.CUIT) {
-                        console.log("Lo limpio")
-                        console.log("this.CUITInicial:", this.CUITInicial)
-                        console.log("this.CUIT:", this.CUIT)
                         this.declaracionComformidad.razonSocialDeclaracion = "";
                     }
                     this.declaracionComformidad.proveedorId = this.proveedorId;
@@ -467,19 +467,19 @@ export class AltaComponent extends BaseComponent implements OnInit {
         }
 
         if (!this.hectareasTotales) {
-            this.mensajeComponent.setErrorMsg("Falta completar hectareas totales.");
+            this.mensajeComponent.setErrorMsg("Falta completar hectáreas totales.");
             return true;
         }
         if (this.hectareasTotales <= 0) {
-            this.mensajeComponent.setErrorMsg("Hectareas totales no puede ser 0 o un numero negativo.");
+            this.mensajeComponent.setErrorMsg("Hectáreas totales no puede ser 0 o un número negativo.");
             return true;
         }
         if (!this.hectareasSoja) {
-            this.mensajeComponent.setErrorMsg("Falta completar hectareas de soja.");
+            this.mensajeComponent.setErrorMsg("Falta completar hectáreas de soja.");
             return true;
         }
         if (this.hectareasSoja <= 0) {
-            this.mensajeComponent.setErrorMsg("Hectareas de soja no puede ser 0 o un numero negativo.");
+            this.mensajeComponent.setErrorMsg("Hectáreas de soja no puede ser 0 o un número negativo.");
             return true;
         }
         if (this.hectareasSoja > this.hectareasTotales) {
@@ -533,12 +533,7 @@ export class AltaComponent extends BaseComponent implements OnInit {
             this.revisarDeclaracionJurada();
         }
     }
-    mensajeCuitsIguales = {
-        severity: 'warn',
-        summary: 'Atención',
-        detail: 'Recuerde que en el campo Proveedor y CUIT tiene que poner los datos del titular de la Carta de Porte'
-    }
-
+    
     revisarDeclaracionJurada() {
         if ((this.CUIT == "" || this.CUIT.length == 11) && this.cosechaId > 0) {
             if (this.esCorredor && this.operarComo == 1
@@ -548,15 +543,27 @@ export class AltaComponent extends BaseComponent implements OnInit {
             this.declaracionComformidad.verificarDeclaracion();
         }
         if (this.CUIT == sessionStorage.getItem('cuit')) {
-            this.messageService.add(this.mensajeCuitsIguales);
+            this.mostrarWarningCuitsIguales();
         }
     }
+
     onSePreseleccionaProveedor() {
         this.sePreseleccionoProveedor = true;
-        this.corredorDebeCargarCuit = false
+        this.corredorDebeCargarCuit = false;
     }
+
     onQuery(value: string) {
         this.proveedorTexto = value;
-        this.proveedorSeleccionado = null
+        this.proveedorSeleccionado = null;
+    }
+
+    mostrarWarningCuitsIguales() {
+        const mensajeCuitsIguales: Message = {
+            severity: 'warn',
+            summary: 'Atención',
+            detail: 'Recuerde que en el campo Proveedor y CUIT tiene que poner los datos del titular de la Carta de Porte',
+            life: 10000
+        }
+        this.messageService.add(mensajeCuitsIguales);
     }
 }
