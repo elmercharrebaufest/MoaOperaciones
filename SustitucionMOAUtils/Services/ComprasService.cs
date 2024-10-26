74e449103273c9a97bb27437c04486e35da52511
Eugenio Martin <emartin@baufest.com>
2024-10-26 11:46:32 -0300

    Enhance error logging and refactor ComprasService.cs
    Enhanced error logging by adding Log.Error(e) in catch blocks and using e.Message for error messages. Simplified code by removing unnecessary try-catch blocks and redundant code. Adjusted filtering logic in ListarSolp method for correct handling of conditions. Improved code formatting and consistency.

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -891,7 +891,8 @@ namespace SustitucionMOAUtils.Services
                 }
                 catch (Exception e)
                 {
-                    resultadoCrearSolp.Errores.Add(new CrearSolpConsumerMOAError { Mensaje = "Server Error", Tipo = "E" });
+                    Log.Error(e);
+                    resultadoCrearSolp.Errores.Add(new CrearSolpConsumerMOAError { Mensaje = e.Message, Tipo = "E" });
                 }
 
                 respuestaGuardarSOLP.Errores = new List<string>();
@@ -941,9 +942,10 @@ namespace SustitucionMOAUtils.Services
                     }
                     catch (Exception e)
                     {
+                        Log.Error(e);
                         ObtenerSolpesDesdeSAPJob(new ObtenerSolpRequest { NumeroSolp = solpEntity.NroSolp, FechaDesde = new DateTime(2010, 01, 01), FechaHasta = DateTime.Now.Date.AddDays(1) });
                         respuestaGuardarSOLP.Solp = TraerSolpId(solpEntity.Id);
-                        resultadoEditarSolp.Errores.Add(new ModificarSolpConsumerMOAError { Mensaje = "Server Error", Tipo = "E" });
+                        resultadoEditarSolp.Errores.Add(new ModificarSolpConsumerMOAError { Mensaje = e.Message, Tipo = "E" });
                     }
 
                 }
@@ -1229,94 +1231,88 @@ namespace SustitucionMOAUtils.Services
 
         public ListaPaginada<SolpDto> ListarSolp(UsuarioDto usuarioActual, Paginacion paginacion, string nroSolp, string nombrePedido, DateTime? desde, DateTime? hasta, bool sap, bool mantenimiento, bool web, bool repoAutomatica, bool contratoMarco, List<int> usuarios = null, List<int> estados = null, List<int> centros = null, List<int> grupoDeCompras = null, List<int> claseDocumento = null, List<string> tipoImputacion = null, List<int> valorTipoImputacion = null)
         {
-            try
+            var fechaHasta = hasta != null ? hasta.Value.AddDays(1) : (DateTime?)null;
+            var hoy = DateTime.Now.Date;
+            var usuariosCompras = repositorio.Listar<UsuarioCompras>();
+            Usuario usuario = repositorio.Obtener<Usuario>(u => u.Id == usuarioActual.Id);
+            var rol = usuario.Roles.Any(r => r.Codigo == "COMPRADOR") ? "COMPRADOR" : "SOLP";
+            nroSolp = nroSolp.Trim();
+            //var peticionCierre = repositorio.Listar<PeticionDeOfertaCierre, PeticionDeOfertaCierreDto>(pc => new PeticionDeOfertaCierreDto());
+
+            if (!string.IsNullOrEmpty(nroSolp) && !nroSolp.StartsWith("0"))
             {
-                var fechaHasta = hasta != null ? hasta.Value.AddDays(1) : (DateTime?)null;
-                var hoy = DateTime.Now.Date;
-                var usuariosCompras = repositorio.Listar<UsuarioCompras>();
-                Usuario usuario = repositorio.Obtener<Usuario>(u => u.Id == usuarioActual.Id);
-                var rol = usuario.Roles.Any(r => r.Codigo == "COMPRADOR") ? "COMPRADOR" : "SOLP";
-                nroSolp = nroSolp.Trim();
-                //var peticionCierre = repositorio.Listar<PeticionDeOfertaCierre, PeticionDeOfertaCierreDto>(pc => new PeticionDeOfertaCierreDto());
-
-                if (!string.IsNullOrEmpty(nroSolp) && !nroSolp.StartsWith("0"))
-                {
-                    nroSolp = "0" + nroSolp;
-                }
-
-                Expression<Func<Solp, bool>> filtro = x => x.FechaBorrado == null; //&& x.UsuarioCreacion_Id == usuarioActual.Id;
-
-                if (usuarioActual.Permisos.Contains("VER TODAS SOLPS"))
-                {
-                    filtro = (x => x.FechaBorrado == null);
-                }
-
-                string[] pedidos = nombrePedido.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                var todasLasSolp = repositorio.Listar<Solp, SolpDto>(x => new SolpDto
-                {
-                    UsuarioActual = new UsuarioDto { Mail = x.UsuarioCreacion != null ? x.UsuarioCreacion.Mail : "" },
-                    Id = x.Id,
-                    NroSolp = x.NroSolp,
-                    VerCircular = x.TrabajoYaHecho == null || x.TrabajoYaHecho == false,
-                    NombreDeObra = x.Pliego == null ? "" : x.Pliego.NombreObra,
-                    FechaCreacion = x.FechaCreacion,
-                    EstadoDocumento = new TablaEstadoDto { Descripcion = x.EstadoDocumento == null ? "" : x.EstadoDocumento.Descripcion, Color = x.EstadoDocumento == null ? "" : x.EstadoDocumento.Color, Codigo = x.EstadoDocumento == null ? "" : x.EstadoDocumento.Codigo },
-                    EstadoSolpSap_Id = x.NroSolp != null && x.Posiciones.All(p => !p.Estado) ? -1 : (x.EstadoSolpSap != null ? x.EstadoSolpSap_Id : 0),
-                    EstadoSolpSap = new TablaSapDto { Descripcion = x.EstadoSolpSap != null ? x.EstadoSolpSap.Descripcion : "", Id = x.EstadoSolpSap != null ? x.EstadoSolpSap.Id : 0 },
-                    EstadoSolpDescripcion = x.NroSolp != null && x.Posiciones.All(p => !p.Estado) ? "Borrado en SAP" : (x.EstadoSolpSap != null ? x.EstadoSolpSap.Descripcion : ""),
-                    TipoSolp = new TablaGeneralDto { Descripcion = x.TipoSolp != null ? x.TipoSolp.Descripcion : "", Codigo = x.TipoSolp != null ? x.TipoSolp.Codigo : "" },
-                    VincularPliego = !x.Pliego_Id.HasValue,
-                    TieneCondicionesGenerales = x.Pliego == null ? null : x.Pliego.TieneCondicionesGenerales,
-                    RevisadoPor = x.Pliego == null ? "" : x.Pliego.RevisadoPor,
-                    TipoSolpSap = x.TipoSolpSap,
-                    EstadoPasos = x.EstadoPasos,
-                    PosicionesEstado = x.Posiciones.All(p => !p.Estado),
-                    ItemPorPagina = paginacion.ItemsPorPagina,
-                    Pagina = paginacion.Pagina,
-                    TipoPosicionCodigo = x.Posiciones.Select(posiciones => posiciones.TipoPosicion.Codigo).FirstOrDefault(),
-                    SolpConAdjuntos = x.Pliego.Archivos.Where(r => r.FileKey == FileKeys.AdjuntoCotizacionesSolp || r.FileKey == FileKeys.AdjuntoCotizacionesSolpCondEsp).Any(),
-                    ChatSinLeer = x.ChatInternoCompras.Any(a => !a.Leido && a.Usuario.Roles.Any(r => r.Codigo != rol))
-                                 || x.Posiciones.Any(po => po.Peticiones
-                                .SelectMany(se => se.PeticionDeOferta.Usuarios
-                                .SelectMany(re => re.ChatExterno))
-                                .Any(al => !al.Leido && al.Usuario.Roles.Any(r => r.Codigo != rol)))
-                                ,
-                    TieneMensajesChatInterno = x.ChatInternoCompras.Count > 0,
-                    TienePeticionDeOferta = x.Posiciones.Any(posi => posi.Peticiones.Any()),
-                    ClaseDocumento_Id = x.ClaseDocumento_Id,
-                    TieneMensajesChatExterno = x.Posiciones.Select(po => po.Peticiones
-                                            .SelectMany(se => se.PeticionDeOferta.Usuarios
-                                            .SelectMany(re => re.ChatExterno)))
-                                            .Where(chatExterno => chatExterno.Any())
-                                            .Any(),
-
-
-                },
-                paginacion,
-                x => x.FechaBorrado == null && (string.IsNullOrEmpty(nroSolp) || x.NroSolp.ToUpper().StartsWith(nroSolp.ToUpper())) &&
-                (!estados.Any() || (x.EstadoSolpSap_Id != null && estados.Contains((int)x.EstadoSolpSap_Id)) || (estados.Any(y => y == -1) && x.NroSolp != null && x.Posiciones.All(p => !p.Estado))) &&
-                (!usuarios.Any() || (x.UsuarioCreacion_Id != null && usuarios.Contains((int)x.UsuarioCreacion_Id))) &&
-                (sap && x.TipoSolpSap == 3 || mantenimiento && x.TipoSolpSap == 2 || repoAutomatica && x.TipoSolpSap == 4 ||
-                (web && (x.TipoSolpSap == null || x.TipoSolpSap == 1)) || (!sap && !mantenimiento && !web && !repoAutomatica)) &&
-                (desde == null || x.FechaCreacion >= desde.Value) && (fechaHasta == null || x.FechaCreacion <= fechaHasta.Value)
-                && (!pedidos.Any() || pedidos.All(p => x.Pliego.NombreObra.ToUpper().Contains(p.ToUpper()))) &&
-                (!contratoMarco || x.Posiciones.Any(p => !string.IsNullOrEmpty(p.NumeroContratoSuperior))) &&
-                (!centros.Any() || x.Posiciones.Any(c => centros.Contains(c.Centro_Id))) && (!grupoDeCompras.Any() || x.Posiciones.Any(gc => grupoDeCompras.Contains((int)gc.GrupoCompras_Id))) &&
-                (!claseDocumento.Any() || claseDocumento.Contains((int)x.ClaseDocumento_Id)) && (!tipoImputacion.Any() || x.Posiciones.Any(c => tipoImputacion.Contains(c.TipoImputacion.Codigo))) &&
-                (!valorTipoImputacion.Any() || x.Posiciones.Any(p => valorTipoImputacion.Contains((int)p.ValorTipoImputacion_Id)) || x.Posiciones.Any(p => p.Subposiciones.Any(sp => valorTipoImputacion.Contains((int)sp.TipoImputacion_Id)))));
-
-
-                if (todasLasSolp.Items != null && todasLasSolp.Items.Any())
-                {
-                    todasLasSolp.Items.FirstOrDefault().ItemsTotales = todasLasSolp.ItemsTotales;
-                }
-
-                return todasLasSolp;
+                nroSolp = "0" + nroSolp;
             }
-            catch (Exception e)
+
+            Expression<Func<Solp, bool>> filtro = x => x.FechaBorrado == null; //&& x.UsuarioCreacion_Id == usuarioActual.Id;
+
+            if (usuarioActual.Permisos.Contains("VER TODAS SOLPS"))
             {
-                throw;
+                filtro = (x => x.FechaBorrado == null);
             }
+
+            string[] pedidos = nombrePedido.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var todasLasSolp = repositorio.Listar<Solp, SolpDto>(x => new SolpDto
+            {
+                UsuarioActual = new UsuarioDto { Mail = x.UsuarioCreacion != null ? x.UsuarioCreacion.Mail : "" },
+                Id = x.Id,
+                NroSolp = x.NroSolp,
+                VerCircular = x.TrabajoYaHecho == null || x.TrabajoYaHecho == false,
+                NombreDeObra = x.Pliego == null ? "" : x.Pliego.NombreObra,
+                FechaCreacion = x.FechaCreacion,
+                EstadoDocumento = new TablaEstadoDto { Descripcion = x.EstadoDocumento == null ? "" : x.EstadoDocumento.Descripcion, Color = x.EstadoDocumento == null ? "" : x.EstadoDocumento.Color, Codigo = x.EstadoDocumento == null ? "" : x.EstadoDocumento.Codigo },
+                EstadoSolpSap_Id = x.NroSolp != null && x.Posiciones.All(p => !p.Estado) ? -1 : (x.EstadoSolpSap != null ? x.EstadoSolpSap_Id : 0),
+                EstadoSolpSap = new TablaSapDto { Descripcion = x.EstadoSolpSap != null ? x.EstadoSolpSap.Descripcion : "", Id = x.EstadoSolpSap != null ? x.EstadoSolpSap.Id : 0 },
+                EstadoSolpDescripcion = x.NroSolp != null && x.Posiciones.All(p => !p.Estado) ? "Borrado en SAP" : (x.EstadoSolpSap != null ? x.EstadoSolpSap.Descripcion : ""),
+                TipoSolp = new TablaGeneralDto { Descripcion = x.TipoSolp != null ? x.TipoSolp.Descripcion : "", Codigo = x.TipoSolp != null ? x.TipoSolp.Codigo : "" },
+                VincularPliego = !x.Pliego_Id.HasValue,
+                TieneCondicionesGenerales = x.Pliego == null ? null : x.Pliego.TieneCondicionesGenerales,
+                RevisadoPor = x.Pliego == null ? "" : x.Pliego.RevisadoPor,
+                TipoSolpSap = x.TipoSolpSap,
+                EstadoPasos = x.EstadoPasos,
+                PosicionesEstado = x.Posiciones.All(p => !p.Estado),
+                ItemPorPagina = paginacion.ItemsPorPagina,
+                Pagina = paginacion.Pagina,
+                TipoPosicionCodigo = x.Posiciones.Select(posiciones => posiciones.TipoPosicion.Codigo).FirstOrDefault(),
+                SolpConAdjuntos = x.Pliego.Archivos.Where(r => r.FileKey == FileKeys.AdjuntoCotizacionesSolp || r.FileKey == FileKeys.AdjuntoCotizacionesSolpCondEsp).Any(),
+                ChatSinLeer = x.ChatInternoCompras.Any(a => !a.Leido && a.Usuario.Roles.Any(r => r.Codigo != rol))
+                             || x.Posiciones.Any(po => po.Peticiones
+                            .SelectMany(se => se.PeticionDeOferta.Usuarios
+                            .SelectMany(re => re.ChatExterno))
+                            .Any(al => !al.Leido && al.Usuario.Roles.Any(r => r.Codigo != rol)))
+                            ,
+                TieneMensajesChatInterno = x.ChatInternoCompras.Count > 0,
+                TienePeticionDeOferta = x.Posiciones.Any(posi => posi.Peticiones.Any()),
+                ClaseDocumento_Id = x.ClaseDocumento_Id,
+                TieneMensajesChatExterno = x.Posiciones.Select(po => po.Peticiones
+                                        .SelectMany(se => se.PeticionDeOferta.Usuarios
+                                        .SelectMany(re => re.ChatExterno)))
+                                        .Where(chatExterno => chatExterno.Any())
+                                        .Any(),
+
+
+            },
+            paginacion,
+            x => x.FechaBorrado == null && (string.IsNullOrEmpty(nroSolp) || x.NroSolp.ToUpper().StartsWith(nroSolp.ToUpper())) &&
+            (!estados.Any() || (x.EstadoSolpSap_Id != null && estados.Contains((int)x.EstadoSolpSap_Id)) || (estados.Any(y => y == -1) && x.NroSolp != null && x.Posiciones.All(p => !p.Estado))) &&
+            (!usuarios.Any() || (x.UsuarioCreacion_Id != null && usuarios.Contains((int)x.UsuarioCreacion_Id))) &&
+            (sap && x.TipoSolpSap == 3 || mantenimiento && x.TipoSolpSap == 2 || repoAutomatica && x.TipoSolpSap == 4 ||
+            (web && (x.TipoSolpSap == null || x.TipoSolpSap == 1)) || (!sap && !mantenimiento && !web && !repoAutomatica)) &&
+            (desde == null || x.FechaCreacion >= desde.Value) && (fechaHasta == null || x.FechaCreacion <= fechaHasta.Value)
+            && (!pedidos.Any() || pedidos.All(p => x.Pliego.NombreObra.ToUpper().Contains(p.ToUpper()))) &&
+            (!contratoMarco || x.Posiciones.Any(p => !string.IsNullOrEmpty(p.NumeroContratoSuperior))) &&
+            (!centros.Any() || x.Posiciones.Any(c => centros.Contains(c.Centro_Id))) && (!grupoDeCompras.Any() || x.Posiciones.Any(gc => grupoDeCompras.Contains((int)gc.GrupoCompras_Id))) &&
+            (!claseDocumento.Any() || claseDocumento.Contains((int)x.ClaseDocumento_Id)) && (!tipoImputacion.Any() || x.Posiciones.Any(c => tipoImputacion.Contains(c.TipoImputacion.Codigo))) &&
+            (!valorTipoImputacion.Any() || x.Posiciones.Any(p => valorTipoImputacion.Contains((int)p.ValorTipoImputacion_Id)) || x.Posiciones.Any(p => p.Subposiciones.Any(sp => valorTipoImputacion.Contains((int)sp.TipoImputacion_Id)))));
+
+
+            if (todasLasSolp.Items != null && todasLasSolp.Items.Any())
+            {
+                todasLasSolp.Items.FirstOrDefault().ItemsTotales = todasLasSolp.ItemsTotales;
+            }
+
+            return todasLasSolp;
+
         }
 
         public List<PeticionDeOfertaDto> ListarPeticionesDeOferta(int solpId)
@@ -7619,7 +7615,7 @@ namespace SustitucionMOAUtils.Services
                 ActualizarDatosSolp(numerosDePedido, cotizacion.PeticionDeOfertaUsuario.PeticionDeOferta);
                 return respuestaGuardarSOLP;
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 if (adjudicacion != null && adjudicacion.Id > 0 && string.IsNullOrEmpty(adjudicacion.NumeroOrdenDeCompra))
                 {
@@ -9948,9 +9944,9 @@ namespace SustitucionMOAUtils.Services
                                 )
                             ) ||
                             (
-                                !sap 
-                                && !mantenimiento 
-                                && !web 
+                                !sap
+                                && !mantenimiento
+                                && !web
                                 && !repoAutomatica
                              )
                     ) &&
@@ -10391,16 +10387,9 @@ namespace SustitucionMOAUtils.Services
 
         public List<SolpDto> ListarSolpCondicionEspecial(FiltroDto filtroDto)
         {
-            try
-            {
-                var filtro = ConvertirAFiltroServiceDto(filtroDto);
-                var todasLasSolp = repositorio.ListarConsulta(new ListarSolpCondicionEspecialConsulta(filtro));
-                return todasLasSolp;
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
+            var filtro = ConvertirAFiltroServiceDto(filtroDto);
+            var todasLasSolp = repositorio.ListarConsulta(new ListarSolpCondicionEspecialConsulta(filtro));
+            return todasLasSolp;
         }
 
         private FiltroServiceDto ConvertirAFiltroServiceDto(FiltroDto filtro)
