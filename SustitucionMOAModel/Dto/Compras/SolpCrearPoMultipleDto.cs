c18227ea82d2cb50c872b5c80ae50e8b4ef30b2a
Federico Seckel <fseckel@baufest.com>
2024-11-05 16:07:15 -0300

    ARMOA003-3079: Modificación del dto

--- SustitucionMOAModel/Dto/Compras/SolpCrearPoMultipleDto.cs
+++ SustitucionMOAModel/Dto/Compras/SolpCrearPoMultipleDto.cs
@@ -1,6 +1,8 @@
 ﻿// Ignore Spelling: Solp Nro
+using iTextSharp.text;
 using SustitucionMOAModel.Attributes;
 using System;
+using System.Collections.Generic;
 
 namespace SustitucionMOAModel.Dto
 {
@@ -17,44 +19,51 @@ namespace SustitucionMOAModel.Dto
 
         [ExcelColumnName("Nombre SOLP/Pliego")]
         [ExcelColumnType(ExcelCellType.Text)]
-        [ExcelColumnOrder(0)]
-        [ExcelColumnWidth(11)]
+        [ExcelColumnOrder(1)]
+        [ExcelColumnWidth(45)]
         public string Nombre { get; set; }
 
         [ExcelColumnName("Fecha Creación")]
         [ExcelColumnType(ExcelCellType.Text)]
-        [ExcelColumnOrder(0)]
-        [ExcelColumnWidth(11)]
+        [ExcelColumnOrder(2)]
+        [ExcelColumnWidth(18)]
         public DateTime FechaCreacion { get; set; }
 
         [ExcelColumnName("Fecha Liberación")]
         [ExcelColumnType(ExcelCellType.Text)]
-        [ExcelColumnOrder(0)]
-        [ExcelColumnWidth(11)]
+        [ExcelColumnOrder(3)]
+        [ExcelColumnWidth(18)]
         public DateTime? FechaLiberacion { get; set; }
 
         [ExcelColumnName("Solicitante")]
         [ExcelColumnType(ExcelCellType.Text)]
-        [ExcelColumnOrder(0)]
-        [ExcelColumnWidth(11)]
+        [ExcelColumnOrder(4)]
+        [ExcelColumnWidth(15)]
         public string Solicitante { get; set; }
 
         [ExcelColumnName("Grupo de compras")]
         [ExcelColumnType(ExcelCellType.Text)]
-        [ExcelColumnOrder(0)]
-        [ExcelColumnWidth(11)]
+        [ExcelColumnOrder(5)]
+        [ExcelColumnWidth(20)]
         public string GrupoDeCompras { get; set; }
 
         [ExcelColumnName("Centro")]
         [ExcelColumnType(ExcelCellType.Text)]
-        [ExcelColumnOrder(0)]
-        [ExcelColumnWidth(11)]
+        [ExcelColumnOrder(6)]
+        [ExcelColumnWidth(20)]
         public string Centro { get; set; }
 
         [ExcelColumnName("Tipo (Sin Doc. / C/Doc. Pliego)")]
         [ExcelColumnType(ExcelCellType.Text)]
-        [ExcelColumnOrder(0)]
-        [ExcelColumnWidth(11)]
+        [ExcelColumnOrder(7)]
+        [ExcelColumnWidth(28)]
         public string Tipo { get; set; }
+
+        [ExcelIgnore]
+        public List<List<string>> Posiciones { get; set; } = new List<List<string>>()
+        {
+            new List<string>() {"f1c1","f1c2"},
+            new List<string>() {"f2c1","f2c2"},
+        };
     }
 }
