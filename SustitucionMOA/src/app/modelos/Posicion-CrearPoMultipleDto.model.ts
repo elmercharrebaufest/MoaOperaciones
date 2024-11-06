5face0b7c237f5a4d24a679671bf5e10fd08c4d6
Federico Seckel <fseckel@baufest.com>
2024-11-06 13:28:19 -0300

    ARMOA003-3079: Modelo subposición FRONT

--- SustitucionMOA/src/app/modelos/Posicion-CrearPoMultipleDto.model.ts
+++ SustitucionMOA/src/app/modelos/Posicion-CrearPoMultipleDto.model.ts
@@ -1,17 +1,16 @@
-﻿export interface PosicionCrearPoMultipleDto {
+﻿import { SubPosicionCrearPoMultipleDto } from "./SubPosicion-CrearPoMultipleDto.model";
+
+export interface PosicionCrearPoMultipleDto {
     Id: number;
-
     NroPosicion?: number;
-
     Descripcion: string;
-
     TipoImputacion: string;
-
     Centro: string;
-
     Almacen: string;
-
     Moneda: string;
-
     ValorTotal?: number;
+
+    SubPosiciones: SubPosicionCrearPoMultipleDto[];
+
+    Expanded: boolean;
 }
---
+++ SustitucionMOA/src/app/modelos/SubPosicion-CrearPoMultipleDto.model.ts
@@ -0,0 +1,12 @@
+﻿export interface SubPosicionCrearPoMultipleDto {
+    Id: number;
+    NroSubPosicion?: number;
+    CodigoServicio: string;
+    Tarea: string;
+    Cantidad: number;
+    UnidadMedida: string;
+    PrecioBruto: number;
+    ValorNeto: number;
+    CuentaMayor: string;
+    Imputación: string;
+}
