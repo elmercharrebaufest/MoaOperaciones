b83c9509c44a5cb7d969de17cb9af0e49d6d3995
Federico Seckel <fseckel@baufest.com>
2024-11-08 14:16:10 -0300

    ARMOA003-3423: Actualizar la base donde tenga TieneVisitaObra a TieneVisitaObraMasiva
    Added `_ScriptBasePreDeployment.sql` to `SustitucionMOADatabase.sqlproj`.
    Included `ARMOA003-3423_TieneVisitaObra_cambiaA_TieneVisitaObraMasiva.sql`
    in the project file. Created the new script to update the `Pliego` table,
    setting `TieneVisitaObraMasiva` to 1 where `TieneVisitaObra` is 1.
    Updated `_ScriptBasePreDeployment.sql` to include the new script using
    SQLCMD syntax.

---
+++ SustitucionMOADatabase/scripts/PreDeployment/ARMOA003-3423_TieneVisitaObra_cambiaA_TieneVisitaObraMasiva.sql
@@ -0,0 +1,7 @@
+﻿/* https://baufest.atlassian.net/browse/ARMOA003-3423
+ * Actualizar la base donde tenga TieneVisitaObra a TieneVisitaObraMasiva
+ */
+
+UPDATE Pliego
+SET TieneVisitaObraMasiva = 1
+WHERE TieneVisitaObra = 1;
