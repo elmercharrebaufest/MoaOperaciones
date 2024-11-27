f3a3804320489c30c02584de0d73496122dd25a3
Eugenio Martin <emartin@baufest.com>
2024-11-27 14:07:22 -0300

    fix mail errores

--- SustitucionMOAUtils/Services/LogTableService.cs
+++ SustitucionMOAUtils/Services/LogTableService.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SustitucionMOAAssets;
-using SustitucionMOAModel.CustomExceptions;
-using SustitucionMOAModel.Dto;
+﻿using SustitucionMOAModel.Dto;
 using SustitucionMOAModel.Entities;
-using SustitucionMOAModel.Models;
-using SustitucionMOAModel.Models.WSMapMOA.CartaPorte.Formulario;
-using SustitucionMOAModel.Models.WSMapMOA.Contrato.Detalle;
-using SustitucionMOAModel.Models.WSMapMOA.Echeq;
 using SustitucionMOARepositorio;
 using SustitucionMOARepositorio.ConsultasEF;
 using SustitucionMOAUtils.Interfaces;
-using SustitucionMOAUtils.Logger;
-using SustitucionMOAWS.WSConsumers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SustitucionMOAUtils.Services
 {
@@ -35,7 +24,16 @@ namespace SustitucionMOAUtils.Services
         {
             //los frontLogger los excluyo por que no nos suman nada por ahora
             var resultado = repositorio.Listar<LogTable, LogTableDto>(
-                l => new LogTableDto(l),
+                l => new LogTableDto
+                {
+                    Id = l.Id,
+                    Level = l.Level,
+                    Logger = l.Logger,
+                    Date = l.Date,
+                    Exception = l.Exception,
+                    Message = l.Message
+
+                },
                 x => (!soloErrores || x.Level == "error") && x.Logger != "frontLogger" && (desde == null || x.Date >= desde));
 
             var agrupado = resultado
@@ -47,7 +45,7 @@ namespace SustitucionMOAUtils.Services
                     Logger = logGroup.Key.Logger,
                     Level = logGroup.Key.Level,
                     Count = logGroup.Count(),
-                    //Errors = logGroup.ToList()
+                    Errors = logGroup.ToList()
                 })
                 .ToList();
             return agrupado;
