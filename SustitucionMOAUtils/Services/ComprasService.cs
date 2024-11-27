c5b6dce32393b3469f3900be9c733c7c9c7dae0c
Federico Seckel <fseckel@baufest.com>
2024-11-27 12:46:43 -0300

    Explicitly define variable types for clarity
    The type of the `solp` variable in the `foreach` loop has been explicitly defined as `Solp`. This change improves code readability and clarity by making the type of `solp` explicit.
    
    The `tienePliego` variable has been changed from `var` to `bool` to explicitly define its type. This change ensures that the type of `tienePliego` is clear and unambiguous.
    
    The `middleFileName` and `pdfFilename` variables have been changed from `var` to `string` to explicitly define their types. This change improves code readability and makes the types of these variables explicit.

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -4798,18 +4798,18 @@ namespace SustitucionMOAUtils.Services
                 esProveedor
                 && (esMultipleSolp && !solps.Any(s => s.Posiciones.Any(p => p.TipoPosicion.Codigo == "SERVICIO"))) /* si es servicio, mostrar aún cuando es múltiple */;
 
-            foreach (var solp in solps)
+            foreach (Solp solp in solps)
             {
                 if (!ocultarArchivosPliego)
                 {
-                    var tienePliego = (solp.TipoSolpSap == (int?)TipoSolpSap.Mantenimiento ||
+                    bool tienePliego = (solp.TipoSolpSap == (int?)TipoSolpSap.Mantenimiento ||
                         solp.TipoSolpSap == (int?)TipoSolpSap.Sap ||
                         solp.TipoSolpSap == (int?)TipoSolpSap.ReposicionAutomatica) && solp.EstadoDocumento.Codigo == "CREADO";
 
                     if (tienePliego || solp.TipoSolp?.Codigo == "CON_PLIEGO")
                     {
-                        var middleFileName = solp.NroSolp ?? solp.Pliego.NombreObra ?? "xxxx";
-                        var pdfFilename = $"Solp-{middleFileName}-pliego-{DateTime.Now:yyyyMMdd}.pdf";
+                        string middleFileName = solp.NroSolp ?? solp.Pliego.NombreObra ?? "xxxx";
+                        string pdfFilename = $"Solp-{middleFileName}-pliego-{DateTime.Now:yyyyMMdd}.pdf";
 
                         //invento registro con id de archivo 0 para bajar el pliego
                         legajo.Add(new LegajoDto
