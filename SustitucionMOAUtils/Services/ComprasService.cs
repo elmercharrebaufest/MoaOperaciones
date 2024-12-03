c56ba623fc357306d666a302e59f220e33b35577
Federico Seckel <fseckel@baufest.com>
2024-12-03 11:28:11 -0300

    Add null check for Solp object in GenerarZipPliego method
    Ensure ArgumentException is thrown if Solp object is null in
    GenerarZipPliego method of ComprasService.cs. This improves
    error handling by providing a clear error message and
    preventing further errors from a null Solp object.

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -1859,7 +1859,7 @@ namespace SustitucionMOAUtils.Services
 
         public string GenerarZipPliego(int idSolp, string pathBase)
         {
-            Solp solp = repositorio.Obtener<Solp>(idSolp);
+            Solp solp = repositorio.Obtener<Solp>(idSolp) ?? throw new ArgumentException("Invalid Solp ID");
             string middleFileName = solp.NroSolp ?? (solp.Pliego.NombreObra ?? "xxxx");
             string pdfFilename = $"Solp-{middleFileName}-pliego-{DateTime.Now:yyyyMMdd}.pdf";
             string pdfFilePath = $"{pathBase}/{pdfFilename}";
