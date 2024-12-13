210077f81231d04dd3c835ca3377ff0619ff1f30
Federico Seckel <fseckel@baufest.com>
2024-12-13 14:36:18 -0300

    Add CampoCosechaProveedor table and update related classes
    - Created CampoCosechaProveedor table with primary key and foreign key constraints, and non-clustered indexes.
    - Updated SustitucionMOADatabase.sqlproj to include CampoCosechaProveedor.sql.
    - Modified CampoCosecha.cs: removed unused directives, renamed Proveedores to CamposProveedor, and added new Proveedores property with InverseProperty attribute.
    - Updated Proveedor.cs: removed unused directives and added CamposCosecha property with InverseProperty attribute.

--- SustitucionMOAModel/Entities/CampoCosecha.cs
+++ SustitucionMOAModel/Entities/CampoCosecha.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SustitucionMOAModel.Entities
 {
@@ -12,16 +8,25 @@ namespace SustitucionMOAModel.Entities
     {
         [Key]
         public int Id { get; set; }
+
         public int CampoSustentable_Id { get; set; }
+
         [ForeignKey("CampoSustentable_Id")]
-        public virtual CampoSustentable Campo {get; set; }
+        public virtual CampoSustentable Campo { get; set; }
+
         public int Cosecha_Id { get; set; }
 
         [ForeignKey("Cosecha_Id")]
         public virtual Cosecha Cosecha { get; set; }
+
         [InverseProperty("CampoCosecha")]
-        public ICollection<CampoProveedor> Proveedores { get; set; }
+        public ICollection<CampoProveedor> CamposProveedor { get; set; }
+
         public double ToneladasAprobadas { get; set; }
+
         public string MotivoRechazo { get; set; }
+
+        [InverseProperty(nameof(Proveedor.CamposCosecha))]
+        public virtual ISet<Proveedor> Proveedores { get; set; }
     }
 }
