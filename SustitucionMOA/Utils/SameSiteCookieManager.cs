b9b8c84ad2187033263b271b4b96174b55faaf51
Eugenio Martin <emartin@baufest.com>
2024-12-16 14:50:23 -0300

    Revert "ARMOA003-3574 - actualizar a .et 4.8"
    This reverts commit 8a9300057d3e265b63188b8bbe3026dc2df9a6d1.
    
    # Conflicts:
    #	SustitucionMOA/Web.config

--- SustitucionMOA/Utils/SameSiteCookieManager.cs
+++ SustitucionMOA/Utils/SameSiteCookieManager.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Owin;
 using Microsoft.Owin.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace SustitucionMOA.Utils
 {
