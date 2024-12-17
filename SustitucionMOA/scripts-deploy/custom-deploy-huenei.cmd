c384a7ca4fe480eaaa0f59f5d1569e2af75d8ada
Eugenio Martin <emartin@baufest.com>
2024-12-17 15:22:03 -0300

    QaHueneiMoaOperaciones custom deploy

--- SustitucionMOA/scripts-deploy/custom-deploy-huenei.cmd
+++ SustitucionMOA/scripts-deploy/custom-deploy-huenei.cmd
@@ -1,16 +1,31 @@
 @echo off
 
-REM Configura la ruta destino
-set DESTINATION_FOLDER=C:\inetpub\wwwroot\QaHueneiMoaOperaciones
+REM Variables
+set SERVER=\\10.12.42.90\C$\inetpub\wwwroot\QaHueneiMoaOperaciones
+set DRIVE=Z:
+set USER=MOLINOSAGRO\UsrSvcMOAOperaciones
+set PASSWORD="FA!rLf-#aC5Spl"
 
-REM Elimina la carpeta destino si existe
-IF EXIST "%DESTINATION_FOLDER%" (
-    echo Eliminando la carpeta destino...
-    rmdir /S /Q "%DESTINATION_FOLDER%"
+REM Conectar la unidad de red
+echo Conectando la unidad de red...
+net use %DRIVE% %SERVER% /user:%USER% %PASSWORD%
+if errorlevel 1 (
+    echo Error al conectar la unidad de red.
+    exit /b 1
 )
 
-REM Crea la carpeta destino
-echo Creando la carpeta destino...
-mkdir "%DESTINATION_FOLDER%"
+REM Eliminar la carpeta
+echo Eliminando la carpeta en %DRIVE%...
+if exist %DRIVE%\ (
+    rmdir /s /q %DRIVE%\
+    echo Carpeta eliminada.
+) else (
+    echo La carpeta no existe.
+)
 
-echo Eliminando completado.
+REM Desconectar la unidad de red
+echo Desconectando la unidad de red...
+net use %DRIVE% /delete /yes
+
+echo Proceso completado.
+exit /b 0
