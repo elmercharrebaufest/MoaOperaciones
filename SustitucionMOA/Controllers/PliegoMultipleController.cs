1db9db2fdefd587daacf7a3bc9ad16bb6e06eb27
Federico Seckel <fseckel@baufest.com>
2024-12-27 14:10:05 -0300

    Add PliegoMultipleController for handling Pliegos Multiples
    Added a new PliegoMultipleController to manage requests related to
    "Pliegos Multiples". This controller inherits from BaseController and
    uses dependency injection to receive an instance of IPliegoMultipleService.
    Implemented a GetPliegosMultiples action method to handle HTTP GET
    requests and return a JSON response with data from IPliegoMultipleService.
    Updated SustitucionMOA.csproj to include the new controller file.

---
+++ SustitucionMOA/Controllers/PliegoMultipleController.cs
@@ -0,0 +1,21 @@
+﻿using SustitucionMOAUtils.Interfaces;
+using System.Web.Mvc;
+
+namespace SustitucionMOA.Controllers
+{
+    public class PliegoMultipleController : BaseController
+    {
+        private readonly IPliegoMultipleService pliegoMultipleService;
+
+        public PliegoMultipleController(IPliegoMultipleService pliegoMultipleService)
+        {
+            this.pliegoMultipleService = pliegoMultipleService;
+        }
+
+        [HttpGet]
+        public JsonResult GetPliegosMultiples(string nombrePliego = "")
+        {
+            return JsonCustom(pliegoMultipleService.GetPliegosMultiples(nombrePliego));
+        }
+    }
+}
