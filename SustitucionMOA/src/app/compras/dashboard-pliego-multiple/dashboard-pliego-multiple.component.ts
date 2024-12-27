6a80d071e21da4e17cb62ed95bfa8a87a5232e76
Federico Seckel <fseckel@baufest.com>
2024-12-27 16:30:35 -0300

    Update dashboard-pliego-multiple component styles and layout
    Updated CSS to add margin-bottom to div.row. Modified HTML to include a new title section, search input, and button for creating new "Pliego". Added a content wrapper with a table for displaying "Pliego" details. Adjusted TypeScript formatting, added a new property, and reformatted ngOnInit method.

--- SustitucionMOA/src/app/compras/dashboard-pliego-multiple/dashboard-pliego-multiple.component.ts
+++ SustitucionMOA/src/app/compras/dashboard-pliego-multiple/dashboard-pliego-multiple.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard-pliego-multiple',
-  templateUrl: './dashboard-pliego-multiple.component.html',
-  styleUrls: ['./dashboard-pliego-multiple.component.css']
+    selector: 'app-dashboard-pliego-multiple',
+    templateUrl: './dashboard-pliego-multiple.component.html',
+    styleUrls: ['./dashboard-pliego-multiple.component.css']
 })
 export class DashboardPliegoMultipleComponent implements OnInit {
 
-  constructor() { }
+    public nombrePliego?: string = null;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
