729c319bedd7920b65c5c177b7f5d2dc0b12e987
Federico Seckel <fseckel@baufest.com>
2024-12-27 14:09:35 -0300

    Add PliegoMultiple service interface and implementation
    Introduce IPliegoMultipleService interface and PliegoMultipleService
    implementation in the SustitucionMOA project. The interface declares
    GetPliegosMultiples method, which retrieves and filters Pliego entities
    based on the provided nombrePliego parameter. The implementation
    depends on IRepositorio to query Pliego entities and converts them to
    PliegoDto objects. Updated SustitucionMOAUtils.csproj to include new
    files.

---
+++ SustitucionMOAUtils/Interfaces/IPliegoMultipleService.cs
@@ -0,0 +1,10 @@
+﻿using SustitucionMOAModel.Dto.PliegoMultiple;
+using System.Collections.Generic;
+
+namespace SustitucionMOAUtils.Interfaces
+{
+    public interface IPliegoMultipleService
+    {
+        List<PliegoDto> GetPliegosMultiples(string nombrePliego);
+    }
+}
