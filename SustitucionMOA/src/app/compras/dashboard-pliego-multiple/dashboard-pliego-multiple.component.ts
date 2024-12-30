0fbed94f7cd571fe25e1c2c82980618ee4f20929
Federico Seckel <fseckel@baufest.com>
2024-12-30 12:21:57 -0300

    Add search functionality and table updates to dashboard
    Updated input field in `dashboard-pliego-multiple.component.html` to include a `(keyup)` event binding for `onSearchkeyPress()`. Added `<ng-template>` block to `p-table` for displaying `pliego` properties.
    
    In `dashboard-pliego-multiple.component.ts`, added imports for `OnDestroy`, services, and RxJS operators. Extended `DashboardPliegoMultipleComponent` to `ListBaseComponent` and implemented `OnInit` and `OnDestroy`. Updated constructor to include service dependencies.
    
    Added `pliegos` array to store `PliegoDto` objects. Introduced `debouncer` subject and `debouncerSubscription` for debounced search input. Subscribed to `debouncer` in `ngOnInit` with 500ms debounce time and called `getPliegos()` for initial data. Unsubscribed from `debouncerSubscription` in `ngOnDestroy`. Implemented `onSearchkeyPress` to trigger `debouncer` and `getPliegos` to fetch `PliegoDto` objects based on `nombrePliego` search term.

--- SustitucionMOA/src/app/compras/dashboard-pliego-multiple/dashboard-pliego-multiple.component.ts
+++ SustitucionMOA/src/app/compras/dashboard-pliego-multiple/dashboard-pliego-multiple.component.ts
@@ -1,17 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PliegoDto } from './pliegoDto.interface';
+import { ListBaseComponent } from '../../common/base-components/list-base-component';
+import { PliegoMultipleService } from '../pliegoMultiple.service';
+import { NavService } from '../../common/services/NavService';
+import { SessionDataService } from '../../common/services/SessionDataService';
+import { SecurityService } from '../../common/services/SecurityService';
+import { FloatMsgService } from '../../common/services/FloatMsgService';
+import { ModalService } from '../../common/services/ModalService';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'app-dashboard-pliego-multiple',
     templateUrl: './dashboard-pliego-multiple.component.html',
     styleUrls: ['./dashboard-pliego-multiple.component.css']
 })
-export class DashboardPliegoMultipleComponent implements OnInit {
+export class DashboardPliegoMultipleComponent
+    extends ListBaseComponent
+    implements OnInit, OnDestroy {
 
     public nombrePliego?: string = null;
 
-    constructor() { }
+    public pliegos: PliegoDto[];
 
-    ngOnInit() {
+    private debouncer: Subject<void> = new Subject<void>();
+    private debouncerSubscription?: Subscription;
+
+    constructor(protected service: PliegoMultipleService, protected navService: NavService, protected sessionDataService: SessionDataService,
+        protected securityService: SecurityService, protected floatMsgService: FloatMsgService, protected modalService: ModalService) {
+        super(service, navService, sessionDataService, securityService, floatMsgService, modalService);
     }
 
+    ngOnInit() {
+        this.debouncerSubscription = this.debouncer
+            .pipe(
+                debounceTime(500)
+            )
+            .subscribe(value => {
+                this.getPliegos();
+            });
+        this.getPliegos();
+    }
+
+    ngOnDestroy() {
+        if (this.debouncerSubscription) { this.debouncerSubscription.unsubscribe(); }
+    }
+
+    onSearchkeyPress() {
+        this.debouncer.next();
+    }
+
+
+    public getPliegos() {
+        this.service.getPliegoMultiple(this.nombrePliego).subscribe((pliegos: PliegoDto[]) => {
+            this.pliegos = pliegos;
+        });
+    }
 }
