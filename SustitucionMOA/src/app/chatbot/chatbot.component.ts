b0812bb3175622c475e18515d81cd15f0b13f8b5
Eugenio Martin <emartin@baufest.com>
2025-01-06 17:28:28 -0300

    ARMOA003-3599  Refactor chatbot component for better visibility control
    Updated CSS to add .hiddenChatBot and .visibleChatBot classes for toggling chatbot visibility. Replaced onclick attributes with Angular's (click) event binding in HTML. Used ngClass directive for conditional class application. Extended ChatbotComponent from BaseComponent and added logging in toggleChatbot method.

--- SustitucionMOA/src/app/chatbot/chatbot.component.ts
+++ SustitucionMOA/src/app/chatbot/chatbot.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../common/base-components/base-component';
 
 @Component({
     selector: 'app-chatbot',
     templateUrl: './chatbot.component.html',
     styleUrls: ['./chatbot.component.css']
 })
-export class ChatbotComponent implements OnInit {
+export class ChatbotComponent extends BaseComponent implements OnInit {
 
     isChatbotVisible = false;
-    constructor() { }
 
     ngOnInit() {
         this.isChatbotVisible = false;
     }
 
-
     toggleChatbot(): void {
         this.isChatbotVisible = !this.isChatbotVisible;
+        console.log("this.isChatbotVisible ", this.isChatbotVisible);
     }
 }
