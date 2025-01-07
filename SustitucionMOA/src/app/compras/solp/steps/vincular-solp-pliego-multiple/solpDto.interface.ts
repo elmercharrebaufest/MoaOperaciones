b6f26e1a2bbecc10a9e6a4b944866c42b546b431
Federico Seckel <fseckel@baufest.com>
2025-01-07 16:57:51 -0300

    Add SolpDto interface to project
    Added a new TypeScript file `solpDto.interface.ts` to the `SustitucionMOA.csproj` project file. This file defines the `SolpDto` interface with properties: `Id`, `TipoSolpSap`, `NumeroSolp`, `NombreSolp`, `FechaCreacion`, `Creador`, `Fiscal`, and `Estado`.

---
+++ SustitucionMOA/src/app/compras/solp/steps/vincular-solp-pliego-multiple/solpDto.interface.ts
@@ -0,0 +1,10 @@
+export interface SolpDto {
+    Id: number;
+    TipoSolpSap: string;
+    NumeroSolp: string;
+    NombreSolp: string;
+    FechaCreacion: Date;
+    Creador: string;
+    Fiscal: string;
+    Estado: string;
+}
