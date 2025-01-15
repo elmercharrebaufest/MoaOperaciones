a4f41459c0cee2ca8838d8690ae38696bf33523c
Federico Seckel <fseckel@baufest.com>
2025-01-15 13:22:12 -0300

    Refactor PliegoMultipleController and update dependencies
    Updated using directives to include SustitucionMOASecurity and SustitucionMOAUtils.Interfaces, removed System.Web and System.Web.Mvc. Added IUsuarioService dependency to PliegoMultipleController. Modified constructor to accept IUsuarioService. Changed CrearPliegoMultiple method to HttpPost with ValidateInput(false). Updated CrearPliegoMultiple parameters to accept string solpsAsociar. Deserialized solpsAsociar into List<int> and set UsuarioActual in pliegoDataDto using new ObtenerUsuarioActual method. Added ObtenerUsuarioActual method to retrieve current user info via usuarioService.

--- SustitucionMOA/Controllers/PliegoMultipleController.cs
+++ SustitucionMOA/Controllers/PliegoMultipleController.cs
@@ -1,10 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿// Ignore Spelling: Sustitucion solps
+
+using Newtonsoft.Json;
 using SustitucionMOASecurity;
 using SustitucionMOAUtils.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using ComprasDto = SustitucionMOAModel.Dto;
 
@@ -14,10 +15,13 @@ namespace SustitucionMOA.Controllers
     public class PliegoMultipleController : BaseController
     {
         private readonly IPliegoMultipleService pliegoMultipleService;
+        private readonly IUsuarioService usuarioService;
 
-        public PliegoMultipleController(IPliegoMultipleService pliegoMultipleService)
+        public PliegoMultipleController(IPliegoMultipleService pliegoMultipleService,
+            IUsuarioService usuarioService)
         {
             this.pliegoMultipleService = pliegoMultipleService;
+            this.usuarioService = usuarioService;
         }
 
         [HttpGet]
@@ -46,14 +50,25 @@ namespace SustitucionMOA.Controllers
             return JsonCustom(pliegoMultipleService.GetSolpDisponiblesPliegosMultiple(numeroSolp, fechaInicio, fechaFin, creadorList, fiscalList, sap, mantenimiento));
         }
 
-        [HttpGet]
-        public ActionResult CrearPliegoMultiple(string pliegoData, HttpFileCollectionBase adjuntos, IEnumerable<int> solpsAsociar)
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult CrearPliegoMultiple(string pliegoData, string solpsAsociar)
         {
             ComprasDto.SolpDto pliegoDataDto = JsonConvert.DeserializeObject<ComprasDto.SolpDto>(pliegoData);
 
-            pliegoMultipleService.CrearPliegoMultiple(pliegoDataDto, adjuntos, solpsAsociar);
+            List<int> solpsAsociarList = JsonConvert.DeserializeObject<List<int>>(solpsAsociar);
+
+            pliegoDataDto.UsuarioActual = ObtenerUsuarioActual();
+
+            pliegoMultipleService.CrearPliegoMultiple(pliegoDataDto, Request.Files, solpsAsociarList);
 
             return JsonCustom(new { });
+        }
+
+        private ComprasDto.UsuarioDto ObtenerUsuarioActual()
+        {
+            string userMail = SessionPersister.getUsername();
+            return usuarioService.GetUsuario(userMail);
         }
     }
 }
