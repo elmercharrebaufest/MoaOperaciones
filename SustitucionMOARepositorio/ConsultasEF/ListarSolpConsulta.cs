069b1f39ffe66eccd987ead719a9c926f1b2ac6c
Federico Seckel <fseckel@baufest.com>
2025-01-23 15:26:14 -0300

    Remove unused date variables in ListarSolpConsulta.cs
    Removed the `hoy` and `ayer` variables from the `Ejecutar` method
    in the `SustitucionMOARepositorio.ConsultasEF` namespace as they
    are no longer needed.

--- SustitucionMOARepositorio/ConsultasEF/ListarSolpConsulta.cs
+++ SustitucionMOARepositorio/ConsultasEF/ListarSolpConsulta.cs
@@ -83,9 +83,6 @@ namespace SustitucionMOARepositorio.ConsultasEF
         }
         public ListaPaginada<SolpDto> Ejecutar(DbContext contexto)
         {
-            var hoy = DateTime.Now;
-            var ayer = hoy.AddDays(-1);
-
             try
             {
                 ((IObjectContextAdapter)contexto).ObjectContext.CommandTimeout = 180;
