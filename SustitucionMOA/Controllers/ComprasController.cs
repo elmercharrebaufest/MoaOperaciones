b428b2ddc93718efffd6fe969e31c2ae60b26d4d
Federico Seckel <fseckel@baufest.com>
2025-01-30 14:21:32 -0300

    Add tipoPliego parameter to ComprasController method
    Introduce a new parameter `tipoPliego` to the method in `ComprasController.cs` within the `SustitucionMOA.Controllers` namespace. Initialize a new local variable `tipoPliegoEnum` of type `TipoPliego` to `TipoPliego.All`. Check if `tipoPliego` is not null or whitespace, split it by commas, parse each segment to an integer, and aggregate them using a bitwise OR operation to set `tipoPliegoEnum`. Update the `JsonCustom` method call and the method signature to include the new `tipoPliego` parameter. Update the call to `service.ListarPosicionesPOMultipleServicio` to pass `tipoPliegoEnum`.

--- SustitucionMOA/Controllers/ComprasController.cs
+++ SustitucionMOA/Controllers/ComprasController.cs
@@ -891,8 +891,17 @@ namespace SustitucionMOA.Controllers
                                                        string tipoImputacion = null,
                                                        string valorTipoImputacion = null,
                                                        int? numeroPo = null,
-                                                       string nombrePliego = null)
+                                                       string nombrePliego = null,
+                                                       string tipoPliego = null)
         {
+
+            TipoPliego tipoPliegoEnum = TipoPliego.All;
+            if (!string.IsNullOrWhiteSpace(tipoPliego))
+            {
+                IEnumerable<int> tipoPliegoList = tipoPliego.Split(',').Select(x => int.Parse(x));
+                tipoPliegoEnum = (TipoPliego)tipoPliegoList.Aggregate((x, y) => x | y);
+            }
+
             return JsonCustom(new
             {
                 data = service.ListarPosicionesPOMultipleServicio(fechaDesde,
@@ -909,7 +918,8 @@ namespace SustitucionMOA.Controllers
                                                           !string.IsNullOrEmpty(tipoImputacion) ? tipoImputacion.Split(',').ToList() : new List<string>(),
                                                           !string.IsNullOrEmpty(valorTipoImputacion) ? valorTipoImputacion.Split(',').Select(x => int.Parse(x)).ToList() : new List<int>(),
                                                           numeroPo,
-                                                          nombrePliego)
+                                                          nombrePliego,
+                                                          tipoPliegoEnum)
             });
         }
 
