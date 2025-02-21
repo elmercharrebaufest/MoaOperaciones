ecbef64c238f18c9689b73a281e2160b1906d7c3
Luis Fernando Nestares <lnestares@baufest.com>
2025-02-21 13:27:05 -0500

    feat(Pliego Multiple): Expand filtering condition in ComprasService.cs

--- SustitucionMOAUtils/Services/ComprasService.cs
+++ SustitucionMOAUtils/Services/ComprasService.cs
@@ -8061,7 +8061,7 @@ namespace SustitucionMOAUtils.Services
                             Tipo = solp.TipoSolp.Descripcion,
                             MultipleFinalizado = solp.Pliego.MultipleFinalizado,
                         },
-                    solp => solpIds.Contains(solp.Id) && (solp.Pliego.MultipleFinalizado == true)
+                    solp => solpIds.Contains(solp.Id) && (solp.Pliego.MultipleFinalizado == true || solp.Pliego.Multiple == false)
                 );
 
                 return posicionMaterial;
