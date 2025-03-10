a7186b1a9f539984ab40137c35e817e2a5b5fe7f
Diego Hidalgo <dhidalgo@baufest.com>
2025-03-10 17:34:39 -0300

    MOA-568 Agregar texto en racional de compras según si admite certificaciones parciales

--- SustitucionMOA/src/app/compras/dashboard-comprador/ver-ofertas/ver-ofertas.component.ts
+++ SustitucionMOA/src/app/compras/dashboard-comprador/ver-ofertas/ver-ofertas.component.ts
@@ -451,7 +451,6 @@ export class VerOfertasComponent extends ListBaseComponent implements OnInit {
                         } else {
                             this.verificarCertificacionesParciales();
                         }
-
                     }
                 },
                 (error) => {
@@ -606,8 +605,10 @@ export class VerOfertasComponent extends ListBaseComponent implements OnInit {
     }
 
     guardarAdjudicacionTextos() {
+        const txtAdmiteCertifParc = this.adjudicacion.AdmiteCertificacionesParciales ? "Admite certificaciones parciales. " : "No admite certificaciones parciales. ";
+
         this.adjudicacion.CondicionesDeEntrega = this.modalTexto.adjudicacion.CondicionesDeEntrega;
-        this.adjudicacion.CondicionesDePago = this.modalTexto.adjudicacion.CondicionesDePago;
+        this.adjudicacion.CondicionesDePago = txtAdmiteCertifParc + this.modalTexto.adjudicacion.CondicionesDePago;
         this.adjudicacion.Garantias = this.modalTexto.adjudicacion.Garantias;
         this.adjudicacion.TextoDeCabecera = this.modalTexto.adjudicacion.TextoDeCabecera;
     }
