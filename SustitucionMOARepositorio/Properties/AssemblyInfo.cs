59c17ae7b6aa8a25e9824fec88c0bfdfed2e9422
Eugenio Martin <emartin@baufest.com>
2025-03-20 11:28:52 -0300

    Update assembly metadata and project references

--- SustitucionMOARepositorio/Properties/AssemblyInfo.cs
+++ SustitucionMOARepositorio/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SustitucionMOA")]
+[assembly: AssemblyTitle("SustitucionMOARepositorio.Properties")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SustitucionMOA")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("SustitucionMOARepositorio.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b951631c-6e73-433b-b0db-72eeb5990f7f")]
+[assembly: Guid("aa28b462-4832-457a-9205-f1f93cedd35c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,7 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
