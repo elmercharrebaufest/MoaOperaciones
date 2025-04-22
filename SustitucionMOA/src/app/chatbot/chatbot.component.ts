085e97e088131d71a3cd12a80f5539dae0a5b43f
Eugenio Martin <emartin@baufest.com>
2025-04-22 16:46:01 -0300

    chat bot solo visible para adminsitracion.

--- SustitucionMOA/src/app/chatbot/chatbot.component.ts
+++ SustitucionMOA/src/app/chatbot/chatbot.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '../common/base-components/base-component';
+import { SecurityService } from '../common/services/SecurityService';
+import { NavService } from '../common/services/NavService';
+import { AduanaService } from '../aduana/aduana.service';
+import { FloatMsgService } from '../common/services/FloatMsgService';
+import { ModalService } from '../common/services/ModalService';
 
 @Component({
     selector: 'app-chatbot',
@@ -7,6 +12,9 @@ import { BaseComponent } from '../common/base-components/base-component';
     styleUrls: ['./chatbot.component.css']
 })
 export class ChatbotComponent extends BaseComponent implements OnInit {
+    constructor(protected navService: NavService, protected service: AduanaService, protected securityService: SecurityService, protected floatMsgService: FloatMsgService, protected modalService: ModalService) {
+        super(navService, securityService, floatMsgService, modalService);
+    }
 
     isChatbotVisible = false;
 
@@ -18,4 +26,13 @@ export class ChatbotComponent extends BaseComponent implements OnInit {
         this.isChatbotVisible = !this.isChatbotVisible;
         console.log("this.isChatbotVisible ", this.isChatbotVisible);
     }
+
+    checkPermisos() {
+        let tienepermiso = this.securityService.tienePermiso("HANGFIREDASHBOARD");
+        // se usa este permiso para no crear uno nuevo y es el unico que tiene solo adminsitracion.
+        //borrar cuando se pase a produccion todo la logica de permisos para ver le chat.
+        console.log("tienepermiso", tienepermiso);
+        return tienepermiso;
+    }
+
 }
