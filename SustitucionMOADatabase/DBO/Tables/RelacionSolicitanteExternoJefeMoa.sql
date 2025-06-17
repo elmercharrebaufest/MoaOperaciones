dfb4427c8b8edb14a80e8b34b8e24d8d810cb4b9
Luis Fernando Nestares <lnestares@baufest.com>
2025-06-17 10:28:07 -0500

    feat: Implement external requester relationship and role management for external requesters

---
+++ SustitucionMOADatabase/DBO/Tables/RelacionSolicitanteExternoJefeMoa.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[RelacionSolicitanteExternoJefeMoa]
+(
+	[Id] INT NOT NULL IDENTITY (1, 1), 
+	[Usuario_Id] INT NOT NULL,
+	[JefeMoa_Id] INT NOT NULL,
+	CONSTRAINT [PK_RelacionSolicitanteExternoJefeMoa] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_RelacionSolicitanteExternoJefeMoa_Usuario] FOREIGN KEY ([Usuario_Id]) REFERENCES [Usuario]([Id]),
+	CONSTRAINT [FK_RelacionSolicitanteExternoJefeMoa_JefeMoa] FOREIGN KEY ([JefeMoa_Id]) REFERENCES [Usuario]([Id]),
+)
