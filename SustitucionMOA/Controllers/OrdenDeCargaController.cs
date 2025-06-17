2fa4195e26ecac712f086dd9125e31105199bfb7
Evangelino Miranda <emiranda@baufest.com>
2025-06-17 11:15:22 -0500

    modificacion orden carga

--- SustitucionMOA/Controllers/OrdenDeCargaController.cs
+++ SustitucionMOA/Controllers/OrdenDeCargaController.cs
@@ -14,7 +14,7 @@ using System.Web.Mvc;
 
 namespace SustitucionMOA.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class OrdenDeCargaController : BaseController
     {
         readonly IOrdenDeCargaService ordenDeCargaService;
--- SustitucionMOA/src/app/ordenes-de-carga/ordenes-de-carga.service.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/ordenes-de-carga.service.ts
@@ -79,7 +79,7 @@ export class OrdenesDeCargaService extends OrdenesBaseService {
             .append('fechaFin', fechaFin);
 
         return this.http
-            .get<any>('/api/OrdenDeCarga/GetListado', { params: params })
+            .get<OrdenDeCarga[]>('/api/ordendecarga/getlistado', { params: params })
             .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde"))));
     }
 
