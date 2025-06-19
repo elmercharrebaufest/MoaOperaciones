994bea2b074aa01c458bbc26af8de67876efe56b
Evangelino Miranda <emiranda@baufest.com>
2025-06-19 14:59:26 -0500

    Correccion metodo GetListado

--- SustitucionMOA/src/app/ordenes-de-carga/ordenes-de-carga.service.ts
+++ SustitucionMOA/src/app/ordenes-de-carga/ordenes-de-carga.service.ts
@@ -79,7 +79,7 @@ export class OrdenesDeCargaService extends OrdenesBaseService {
             .append('fechaFin', fechaFin);
 
         return this.http
-            .get('/api/ordendecarga/getlistado', { params: params })
+            .get<OrdenDeCarga[]>('/api/OrdenDeCarga/GetListado', { params: params })
             .pipe(timeoutWith(360000, observableThrowError(new Error("Se excedió el tiempo de espera, por favor inténtelo más tarde"))));
     }
 
