5ad985c106cbe9205bd2063fefa1d29d50ce3240
Diego Hidalgo <dhidalgo@baufest.com>
2025-06-25 13:06:01 -0300

    MOA-832 Fix No se ven las certificaciones aprobadas

--- SustitucionMOAUtils/Services/EntradaServicioService.cs
+++ SustitucionMOAUtils/Services/EntradaServicioService.cs
@@ -904,7 +904,6 @@ namespace SustitucionMOAUtils.Services
             entradasServicioCabeceraSap = entradasServicioCabeceraSap
                 .Where(x =>
                     x.OrdenCompra.StartsWith("412") && //Se filtran por las OC tomando las que empiezan con 412
-                    x.Ingresante.Contains("@") &&
                     (!debeFiltrarPorFecha || !x.FechaCreacionDateTime.HasValue || x.FechaCreacionDateTime <= fechaHasta)
                 ).ToList();
 
@@ -987,7 +986,7 @@ namespace SustitucionMOAUtils.Services
                 entradasServicioResponse.Add(documento);
             }
 
-            return entradasServicioResponse;
+            return entradasServicioResponse.Where(x => x.Ingresante.Contains("@")).ToList();
         }
 
         /// <summary>
