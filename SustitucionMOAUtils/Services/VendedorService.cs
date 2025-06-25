337a10927f7ccab97c49f2121d19e12ce4249b06
Evangelino Miranda <emiranda@baufest.com>
2025-06-25 15:21:18 -0500

    Remover logs de seguimiento

--- SustitucionMOAUtils/Services/VendedorService.cs
+++ SustitucionMOAUtils/Services/VendedorService.cs
@@ -354,35 +354,20 @@ namespace SustitucionMOAUtils.Services
         }
         private List<ProveedorDto> GetVendedoresInternal(string mailUsuario, Func<Proveedor, bool> filtro = null)
         {
-
             var usuario = repositorio.Obtener<Entities.Usuario>(u => u.Mail == mailUsuario);
-
-            Log.Info($"GetVendedoresInternal usuario");
-
             var listadoProveedores = new List<ProveedorDto>();
 
             if (usuario.EsAdmin() || usuario.TienePermiso(PermisoEnum.ElegirTodosVendedores))
             {
-                Log.Info($"GetVendedoresInternal usuario");
-                Log.Info($"GetVendedoresInternal usuario.EsAdmin()");
-
                 listadoProveedores = repositorio
                         .Listar<Proveedor>(p => p.EstadoAprobacion == EstadoAprobacion.Aprobado)
                         .Where(filtro)
                         .Select(proveedor => new ProveedorDto(proveedor, false)).ToList();
-                Log.Info($"GetVendedoresInternal listadoProveedores 1");
-                Log.Info(listadoProveedores.ToXml());
             }
             else
             {
-                Log.Info($"GetVendedoresInternal proveedores1 00");
                 var proveedores1 = usuario.ObtenerProveedor();
-                Log.Info($"GetVendedoresInternal proveedores1 00");
-                Log.Info(proveedores1.ToXml());
-
                 var proveedores = repositorio.Listar<Proveedor>(p => p.CUIT == usuario.CUITRegistro).ToList();
-                Log.Info($"GetVendedoresInternal listadoProveedores 0");
-                Log.Info(proveedores.ToXml());
 
                 //var proveedores = usuario.Proveedores.ToList();
 
@@ -390,16 +375,9 @@ namespace SustitucionMOAUtils.Services
                 {
                     proveedores = proveedores.Where(filtro).ToList();
                 }
-                Log.Info($"GetVendedoresInternal proveedores 0-1");
-                Log.Info(proveedores.ToXml());
 
                 listadoProveedores.AddRange(proveedores.Select(proveedor => new ProveedorDto(proveedor, false)).ToList());
-                Log.Info($"GetVendedoresInternal listadoProveedores 2");
-                Log.Info(listadoProveedores.ToXml());
             }
-
-            Log.Info($"GetVendedoresInternal listadoProveedores 3");
-            Log.Info(listadoProveedores.ToXml());
 
             foreach (var item in listadoProveedores.Where(a => a.CUIT == null || a.CUIT == ""))
             {
