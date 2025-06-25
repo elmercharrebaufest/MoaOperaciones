0583610d61f1f5f651f90b485d3d335da26a33e2
Evangelino Miranda <emiranda@baufest.com>
2025-06-25 14:42:54 -0500

    Correccion RFC Echeq Anular Apertura

--- SustitucionMOAWS/WSConsumers/EcheqAnularAperturaChequeConsumerMOA.cs
+++ SustitucionMOAWS/WSConsumers/EcheqAnularAperturaChequeConsumerMOA.cs
@@ -29,7 +29,8 @@ namespace SustitucionMOAWS.WSConsumers
                     var agent = new Z_WS_MOAOP_DIRECTClient();
                     agent.ClientCredentials.UserName.UserName = UserSap;
                     agent.ClientCredentials.UserName.Password = PassSap;
-                    IM_USUARIO = string.IsNullOrEmpty(IM_USUARIO) ? "moaoperaciones" : IM_USUARIO;
+                    IM_USUARIO = string.IsNullOrEmpty(IM_USUARIO) ? "moaoperacion" : IM_USUARIO;
+                    IM_USUARIO = IM_USUARIO.Length > 12 ? IM_USUARIO.Substring(0, 12) : IM_USUARIO;
 
                     var request = new Z_MPRFC_ANULAR_APERTURA_CHEQUE(){
                         IM_CHEQUE = IM_CHEQUE,
