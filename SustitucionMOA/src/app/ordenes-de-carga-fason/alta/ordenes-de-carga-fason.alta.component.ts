59c5b3c90bc2e4205cd8adf5b6abe4ac9d561ee7
Eugenio Martin <emartin@baufest.com>
2025-07-16 16:32:38 -0300

    MOA-898

--- SustitucionMOA/src/app/ordenes-de-carga-fason/alta/ordenes-de-carga-fason.alta.component.ts
+++ SustitucionMOA/src/app/ordenes-de-carga-fason/alta/ordenes-de-carga-fason.alta.component.ts
@@ -157,6 +157,7 @@ export class OrdenesDeCargaFasonAltaComponent
             this.obtenerOrdenDeCarga();
         } else if (this.esAdmin) {
             this.obtenerCorredores();
+            this.cargarClientes('');
         }
     }
 
@@ -429,6 +430,7 @@ export class OrdenesDeCargaFasonAltaComponent
                             this.obtenerCorredores();
                         } else {
                             this.obtenerCorredores();
+                            this.cargarClientes('');
                         }
 
                         const producto = this.listaProductos.find(producto => producto.MaterialId == this.ordenDeCargaFason.Producto_Id)
