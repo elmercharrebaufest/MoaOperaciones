bb38d955cef7d56d334e4aeabcbc699928956def
Eugenio Martin <emartin@baufest.com>
2025-07-23 12:33:36 -0300

    MOA-677
    Enhance DataAgroServices with new DTOs and methods
    
    Updated Web.config for binding configuration and security settings.
    Added EstadoProveedorDto and implemented ObtenerEstadoProveedores
    in IDataAgroService. Modified DataAgroService and VendedorService
    to utilize new methods. Updated WSDL and XSD files to reflect
    new operations and data structures. Adjusted Reference.cs and
    svcmap for new service definitions. Enhanced DataAgroConsumer
    to handle exceptions more generically.

---
+++ SustitucionMOAModel/Dto/EstadoProveedorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SustitucionMOAModel.Dto
+{
+    public class EstadoProveedorDto
+    {
+        public string CUIT { get; set; }
+        public string EstadoHomeDescripcion { get; set; }
+
+    }
+}
