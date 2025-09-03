cbcc5061bc9e8a4fda17aa0d01cd1be0019bdc4b
Eugenio Martin <emartin@baufest.com>
2025-09-03 16:07:36 -0300

    huenei configs

--- SustitucionMOAWS/WSConsumers/AgregarRegistroInfoConsumerMOA.cs
+++ SustitucionMOAWS/WSConsumers/AgregarRegistroInfoConsumerMOA.cs
@@ -276,6 +276,10 @@ namespace SustitucionMOAWS.WSConsumers
             foreach (var registro in registros)
             {
                 string unidadMedidaCodigo = unidadesDeMedia.First(x => x.Comercial == registro.Unidad).UM;
+                registro.FechaVigencia = "2025-01-01";
+                registro.FechaVigenciaFormateada = new DateTime(2025, 01, 01);
+
+
                 var registroInfoSAP = new RegistroInfoSAPSinPI
                 {
                     MEWIEINA = new WS_GAQ_sin_PI_DIRECT_MOAOP.MEWIEINA
