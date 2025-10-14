271c24e8d2c05b13fcc29f3af3545eb97994046d
Eugenio Martin <emartin@baufest.com>
2025-10-14 16:14:07 -0300

    MOA-1227 OperaConMATBA no habilitado

--- SustitucionMOAWS/WSConsumers/DataAgroConsumer.cs
+++ SustitucionMOAWS/WSConsumers/DataAgroConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using SustitucionMOAWS.CredentialService;
 using SustitucionMOAWS.DataAgroServices;
 using System;
+using System.Linq;
 
 namespace SustitucionMOAWS.WSConsumers
 {
@@ -70,8 +71,12 @@ namespace SustitucionMOAWS.WSConsumers
 
         public ResultEstadoProveedores ObtenerEstadoProveedores(string[] cuits)
         {
-
-            return service.ObtenerEstadoProveedores(cuits);
+            var result = service.ObtenerEstadoProveedores(cuits);
+            foreach (var item in result.Contactos.Where(a => a.OperaConMATBA))
+            {
+                item.EstadoHomeDescripcion = "No Habilitado";
+            }
+            return result;
 
         }
 
