a563affd36af40632fa05c58098160b67ccbdcb1
Eugenio Martin <emartin@baufest.com>
2025-10-24 08:32:58 -0300

    MOA-1237

--- SustitucionMOAWS/Logger/SapLogHelper.cs
+++ SustitucionMOAWS/Logger/SapLogHelper.cs
@@ -1,22 +1,20 @@
-﻿using SustitucionMOAWS.Util;
-using System;
+﻿using System;
 using System.Configuration;
 
 namespace SustitucionMOAWS.Logger
 {
     public static class SapLogHelper
     {
-        public static void LogResponse(object response, string nombreMetodo)
+        public static void LogResponse(string response, string nombreMetodo)
         {
             try
             {
-                string xml = response.ToXml();
-                int size = System.Text.Encoding.UTF8.GetByteCount(xml);
+                int size = System.Text.Encoding.UTF8.GetByteCount(response);
                 bool enableBigLogs = ConfigurationManager.AppSettings["SAPEnableBigLogs"] == "1";
 
                 Log.Info($"SAP sin PI {nombreMetodo} response");
                 if (enableBigLogs || size < 1048576)
-                    Log.Info(xml);
+                    Log.Info(response);
             }
             catch (Exception ex)
             {
