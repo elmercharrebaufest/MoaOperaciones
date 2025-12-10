c17eb51edec46d172988801193ceec1d3e86c274
Diego Hidalgo <dhidalgo@baufest.com>
2025-12-10 15:26:45 -0300

    MOA-1371 Borro código comentado

--- SustitucionMOAUtils/Services/AltaEmpresaNoGranosService.cs
+++ SustitucionMOAUtils/Services/AltaEmpresaNoGranosService.cs
@@ -349,56 +349,6 @@ namespace SustitucionMOAUtils.Services
 
                 var formularioAltaNoGranos = respuestaArchivo.Contenido;
                 return formularioAltaNoGranos;
-
-                //var url = string.Concat(DataAgroURL, "/FormularioAltaNoGranos/Generar");
-                //var urlReporte = string.Concat(DataAgroURL, "/Download/Reporte");
-
-                //string userName = DataAgroWSCredential.getUserName();
-                //string password = DataAgroWSCredential.getPassword();
-                //string dominio = DataAgroWSCredential.getDominio();
-
-                //var httpClientHandler = new HttpClientHandler()
-                //{
-                //    Credentials = new NetworkCredential(userName, password, dominio),
-                //};
-
-                //string downloadKey = "";
-
-                //var content = JsonConvert.SerializeObject(proveedorDto); //myDetails is my class object.
-                //var buffer = Encoding.UTF8.GetBytes(content);
-                //var byteContent = new ByteArrayContent(buffer);
-                //byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-                //using (var client = new HttpClient(httpClientHandler, false))
-                //{
-                //    var task = client.PostAsync(url, byteContent);
-
-                //    task.Wait();
-
-                //    var response = task.Result;
-
-                //    var stringContent = response.Content.ReadAsStringAsync();
-
-                //    dynamic jsonResult = JObject.Parse(stringContent.Result);
-
-                //    if (bool.Parse(jsonResult.HayErrores.ToString()))
-                //    {
-                //        throw new InfoCustomException(jsonResult.Errores[0].Message);
-                //    }
-
-                //    downloadKey = jsonResult.DownloadKey;
-
-
-                //    urlReporte = string.Concat(urlReporte, "?key=", downloadKey);
-
-                //    using (WebClient clienteDescarga = new WebClient())
-                //    {
-                //        clienteDescarga.Credentials = new NetworkCredential(userName, password, dominio);
-                //        byte[] formularioAltaNoGranos = clienteDescarga.DownloadData(urlReporte);
-                //        return formularioAltaNoGranos;
-                //    }
-
-                //}
             }
             catch (InfoCustomException)
             {
