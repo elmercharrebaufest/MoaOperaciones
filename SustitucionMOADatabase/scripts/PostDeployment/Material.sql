b55dd3bd791dde0c98e90d8b13391398577e75cc
Diego Hidalgo <dhidalgo@baufest.com>
2026-04-08 14:59:06 -0300

    MOA-1583 Agregar material Biodiesel en FAS

--- SustitucionMOADatabase/scripts/PostDeployment/Material.sql
+++ SustitucionMOADatabase/scripts/PostDeployment/Material.sql
@@ -15,12 +15,16 @@ VALUES
 	('99704 - PELLET DE CASCARA DE SOJA A GRANEL',	'99704',	1,			1,				'P. Cáscara',		0),
 	('99710 - PELLET DE GIRASOL',					'99710',	1,			0,				'P. Girasol',		0),
 	('99709 - PELLET DE GIRASOL INTEGRAL',			'99709',	1,			1,				'P. Girasol Int',	0),
+	('99131 - BIODIESEL (METILESTER DE SOJA) A GRANEL',	'99131',1,			0,				'BIO',				0),
 
 	('94705 - ACEITE DE SOJA CRUDO A GRANEL',		'94705',	2,			1,				'Ac. Soja',			1), 
 	('94687 - ACEITE GIRASOL CRUDO',				'94687',	2,			1,				'Ac. Girasol',		1),
 	('50866 - HARINA DE SOJA HIPRO',				'50866',	2,			0,				'Harina hipro',		0), 
 	('99704 - PELLET DE CASCARA DE SOJA A GRANEL',	'99704',	2,			1,				'P. Cáscara',		1), 
 	('99709 - PELLET DE GIRASOL INTEGRAL',			'99709',	2,			1,				'P. Girasol Int',	1),
+	('99131 - BIODIESEL (METILESTER DE SOJA) A GRANEL',	'99131',2,			0,				'BIO',				0),
+	('98855 - ACEITE DE SOJA NEUTRALIZADO',			'98855',	2,			0,				'Ac. Neutro',		0),
+	('99098 - ACEITE METILADO DE SOJA',				'99098',	2,			0,				'Ac. Metilado',		0),
 
 	( '64195 - CHATARRA',							'64195',	3,			0,				'Chatarra',			0),
 	( '64207 - GOMA EXTRACCION',					'64207',	3,			0,				'Goma extracc.',	0),
@@ -29,10 +33,8 @@ VALUES
 	( '64194 - RESIDUOS ORGANICOS',					'64194',	3,			1,				'Residuos org.',	0),
 	( '64199 - RESIDUOS PELIGROSOS',				'64199',	3,			0,				'Resid. peligr.',	0),
 	( '64196 - RESIDUOS SOLIDOS URBANOS',			'64196',	3,			0,				'Resid. sól. urb',	0),
-	('172789 - SUELO SELECCIONADO',					'172789',	3,			0,				'Suelo selecc.',	0),
-	('99131 - BIODIESEL (METILESTER DE SOJA) A GRANEL',	'99131',2,			0,				'BIO',				0),
-	('98855 - ACEITE DE SOJA NEUTRALIZADO',			'98855',	2,			0,				'Ac. Neutro',		0),
-	('99098 - ACEITE METILADO DE SOJA',				'99098',	2,			0,				'Ac. Metilado',		0)
+	('172789 - SUELO SELECCIONADO',					'172789',	3,			0,				'Suelo selecc.',	0)
+	
 
 -- Actualización de materiales existentes
 UPDATE Material
